f7b9ea95999e9aee27f6744ccae06189e7990984
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2011-09-22 18:19:54 +0000

    added button test program

--- firmware/openos/drivers/telosb/leds.c
+++ firmware/openos/drivers/telosb/leds.c
@@ -8,34 +8,45 @@
 
 void leds_init() {
    P5DIR  |=  0x70;                              // P5DIR = 0bx111xxxx for LEDs
-   P5OUT  &= ~0x70;                              // P2OUT = 0bx000xxxx, all LEDs off
+   P5OUT  |=  0x70;                              // P2OUT = 0bx111xxxx, all LEDs off
 }
 
 //shifts the LEDs by one position
 void leds_circular_shift() {
-  uint8_t temp_leds;
-  if ((P5OUT & 0x70)==0) {                       // if no LEDs on, switch on first one
-    P5OUT |= 0x10;
-    return;
-  }
-  temp_leds = P5OUT & 0x70;                      // retrieve current status of LEDs
-  temp_leds >>= 4;                               // bring back to position 0, 
-  temp_leds <<= 1;                               // shift by one position
-  if ((temp_leds & 0x08)!=0) {
-    temp_leds |= 1;                              // handle overflow
-  }
-  temp_leds <<= 4;                               // send back to position 4
-  P5OUT |= temp_leds;                            // switch on the leds marked '1' in temp_leds
-  P5OUT &= ~(~temp_leds & 0x70);                 // switch off the leds marked '0' in temp_leds
+   uint8_t leds_on;
+   // get LED state
+   leds_on  = (~P5OUT & 0x70) >> 4;
+   // modify LED state
+   if (leds_on==0) {                             // if no LEDs on, switch on one
+      leds_on = 0x01;
+   } else {
+      leds_on <<= 1;                             // shift by one position
+      if ((leds_on & 0x08)!=0) {
+         leds_on &= ~0x08;
+         leds_on |=  0x01;                       // handle overflow
+      }
+   }
+   // apply updated LED state
+   leds_on <<= 4;                                // send back to position 4
+   P5OUT |=  (~leds_on & 0x70);                  // switch on the leds marked '1' in leds_on
+   P5OUT &= ~( leds_on & 0x70);                  // switch off the leds marked '0' in leds_on
 }
 
 //increments the binary number displayed on the LEDs
 void leds_increment() {
-   uint8_t led_counter;
-   led_counter  = (P5OUT & 0x70) >> 4;
-   led_counter += 1;
-   P5OUT &= ~0x070; //all LEDs off
-   P5OUT |=  (led_counter<<4) & 0x70; //LEDs on again
+   uint8_t leds_on;
+   // get LED state
+   leds_on  = (~P5OUT & 0x70) >> 4;
+   // modify LED state
+   if (leds_on==0) {                             // if no LEDs on, switch on one
+      leds_on = 0x01;
+   } else {
+      leds_on += 1;
+   }
+   // apply updated LED state
+   leds_on <<= 4;                                // send back to position 4
+   P5OUT |=  (~leds_on & 0x70);                  // switch on the leds marked '1' in leds_on
+   P5OUT &= ~( leds_on & 0x70);                  // switch off the leds marked '0' in leds_on
 }
 
 //=========================== private =========================================
--- firmware/openos/iar_projects/telosb/test_button.c
+++ firmware/openos/iar_projects/telosb/test_button.c
@@ -1,40 +1,41 @@
 /**
-\brief This is a standalone test program for the button of the GINA2.2b/c
+\brief This is a standalone test program for the button of the TelosB
 board.
 
-Download the program to a GINA board, run it, and when you press the AUX
+Download the program to a TelosB board, run it, and when you press the USER
 button, the LEDs should shift circularly. Note that the RESET button is non
 programmable, and always resets the MSP430.
 
 The digital outputs are:
-  - P2.0: red LED
-  - P2.1: green LED
-  - P2.2: blue LED
-  - P2.3: red LED
+   - P5.4: red LED
+   - P5.5: green LED
+   - P5.6: blue LED
  
- The digital inputs are:
+The digital inputs are:
    - P2.7: button
  
- \author Thomas Watteyne <watteyne@eecs.berkeley.edu>, August 2010
- */
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, September 2011
+*/
 
-#include "gina.h"
-#include "openwsn.h"
 #include "test_button.h"
 #include "leds.h"
 #include "button.h"
-#include "scheduler.h"
 
 int main(void) {
-   gina_init();
-   scheduler_init();
+   WDTCTL  = WDTPW + WDTHOLD;                    // disable watchdog timer
+   
+   DCOCTL  = DCO0 | DCO1 | DCO2;                 // MCLK at 8MHz
+   BCSCTL1 = RSEL0 | RSEL1 | RSEL2;              // MCLK at 8MHz
+   
    leds_init();
    button_init();
    
-   scheduler_start();
-   return 0;
+   __bis_SR_register(GIE+LPM4_bits);             // sleep
 }
 
-void isr_button() {
-   leds_circular_shift();                        // circular-shift the LEDs
+// Port2 interrupt service routine
+#pragma vector=PORT2_VECTOR
+__interrupt void Port2_ISR (void) {
+   P2IFG &= ~0x80;                               // clear interrupt flag
+   leds_circular_shift();                        // wiggle LEDs
 }
