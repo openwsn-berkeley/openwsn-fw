886bc225c667db84ab7f2ab1fd5e197c23f3b3c4
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-02-18 06:08:54 +0000

    fixed leds_xtal project

--- firmware/openos/projects/telosb/00-minimal/leds_xtal.c
+++ firmware/openos/projects/telosb/00-minimal/leds_xtal.c
@@ -21,7 +21,6 @@ The debug pins are:
 
 #include "msp430f1611.h"
 #include "stdint.h"
-#include "leds.h"
 
 int main(void)
 {
@@ -30,7 +29,8 @@ int main(void)
    DCOCTL  = DCO0 | DCO1 | DCO2;                 // MCLK at 8MHz
    BCSCTL1 = RSEL0 | RSEL1 | RSEL2;              // MCLK at 8MHz
    
-   leds_init();
+   P5DIR  |=  0x70;                              // P5DIR = 0bx111xxxx for LEDs
+   P5OUT  |=  0x70;                              // P2OUT = 0bx111xxxx, all LEDs off
    
    P6DIR  |=  0x40;                              // P4DIR = 0bx1xxxxxx for debug
 
@@ -44,6 +44,21 @@ int main(void)
 // TimerA interrupt service routine
 #pragma vector=TIMERA0_VECTOR
 __interrupt void Timer_A_ISR (void) {
+   uint8_t leds_on;
+   
+   // get LED state
+   leds_on  = (~P5OUT & 0x70) >> 4;
+   
+   // modify LED state
+   if (leds_on==0) {                             // if no LEDs on, switch on one
+      leds_on = 0x01;
+   } else {
+      leds_on += 1;
+   }
+   // apply updated LED state
+   leds_on <<= 4;                                // send back to position 4
+   P5OUT |=  (~leds_on & 0x70);                  // switch on the leds marked '1' in leds_on
+   P5OUT &= ~( leds_on & 0x70);                  // switch off the leds marked '0' in leds_on
+   
    P6OUT  ^=  0x40;                              // toggle P6.6 for debug
-   leds_increment();
 }
