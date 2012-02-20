6d46d80c76649c4ab05499038e12d2df8cb7a1ca
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-02-20 17:03:11 +0000

    standalone functions for TelosB

--- firmware/openos/projects/telosb/00-standalone/clocks.c
+++ firmware/openos/projects/telosb/00-standalone/clocks.c
@@ -1,33 +1,33 @@
 /**
 \brief This is a standalone test program for the clocks on the TelosB
-board.
-
-This is a minimal project, i.e. it is fully self-contained and does not
-rely on a external BSP, drivers or OS-related files.
+       board.
 
 Download the program to a TelosB board, run it. It will output its
 clocks on the pins below. Note that these pins are connected to the
-LEDs, so they will blink (so fast you won't see it). Use a scope probe
-to see the clock signals and measure their frequency.
+LEDs, so they will blink (so fast they will appear simply on to your slow eyes).
+Use a scope probe to see the clock signals and measure their frequency.
 
 The digital outputs are:
    - P5.4: MCLK  (red LED or pad 48 on the back)
-   - P5.5: SMCLK (green LED  or pad 49 on the back)
+   - P5.5: SMCLK (green LED or pad 49 on the back)
    - P5.6: ACLK  (blue LED)
 
 We measure an DCO frequency of 4.8MHz. Very low, indeed.
  
-\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, September 2011
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, February 2012
 */
 
 #include "msp430f1611.h"
 #include "stdint.h"
 
+/**
+\brief The program starts executing here.
+*/
 int main(void) {
    WDTCTL  = WDTPW + WDTHOLD;                    // disable watchdog timer
    
-   DCOCTL  = DCO0 | DCO1 | DCO2;                 // MCLK at ~8MHz
-   BCSCTL1 = RSEL0 | RSEL1 | RSEL2;              // MCLK at ~8MHz
+   DCOCTL  = DCO0 | DCO1 | DCO2;                 // MCLK at 8MHz
+   BCSCTL1 = RSEL0 | RSEL1 | RSEL2;              // MCLK at 8MHz
                                                  // by default, ACLK from 32kHz XTAL which is running
 
    P5OUT  |= 0x70;                               // P5OUT = 0xx111xxxx -> P5.4-6 output
