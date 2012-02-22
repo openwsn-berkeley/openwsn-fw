55ef439a67ad345106adb364a386be673af56222
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-02-22 16:12:34 +0000

    updated radio register definition

--- firmware/openos/bsp/telosb/cc2420.h
+++ firmware/openos/bsp/telosb/cc2420.h
@@ -7,9 +7,21 @@
 #ifndef __CC2420_H
 #define __CC2420_H
 
-//=========================== define ==========================================
+//=========================== status registers ================================
 
-// strobes
+typedef struct {
+   uint8_t res1:1;
+   uint8_t xosc16m_stable:1;
+   uint8_t tx_underflow:1;
+   uint8_t enc_busy:1;
+   uint8_t tx_active:1;
+   uint8_t lock:1;
+   uint8_t rssi_valid:1;
+   uint8_t res2:1;
+} cc2420_status_reg_t;
+
+//=========================== strobes =========================================
+
 #define CC2420_SNOP          0x00 // [S  ] No Operation
 #define CC2420_SXOSCON       0x01 // [S  ] Turn on the crystal oscillator
 #define CC2420_STXCAL        0x02 // [S  ] Enable and calibrate frequency synthesizer for TX
@@ -26,48 +38,120 @@
 #define CC2420_STXENC        0x0d // [S  ] Start TXFIFO in-line encryption / authentication
 #define CC2420_SAES          0x0e // [S  ] AES Stand alone encryption strobe
 
-// registers
-#define CC2420_MAIN          0x10 // [R/W] Main Control Register
-#define CC2420_MDMCTRL0      0x11 // [R/W] Modem Control Register 0
-#define CC2420_MDMCTRL1      0x12 // [R/W] Modem Control Register 1
-#define CC2420_RSSI          0x13 // [R/W] RSSI and CCA Status and Control register
-#define CC2420_SYNCWORD      0x14 // [R/W] Synchronisation word control register
-#define CC2420_TXCTRL        0x15 // [R/W] Transmit Control Register
-#define CC2420_RXCTRL0       0x16 // [R/W] Receive Control Register 0
-#define CC2420_RXCTRL1       0x17 // [R/W] Receive Control Register 1
-#define CC2420_FSCTRL        0x18 // [R/W] Frequency Synthesizer Control and Status Register
-#define CC2420_SECCTRL0      0x19 // [R/W] Security Control Register 0
-#define CC2420_SECCTRL1      0x1a // [R/W] Security Control Register 1
-#define CC2420_BATTMON       0x1b // [R/W] Battery Monitor Control and Status Register
-#define CC2420_IOCFG0        0x1c // [R/W] Input / Output Control Register 0
-#define CC2420_IOCFG1        0x1d // [R/W] Input / Output Control Register 1
-#define CC2420_MANFIDL       0x1e // [R/W] Manufacturer ID, Low 16 bits
-#define CC2420_MANFIDH       0x1f // [R/W] Manufacturer ID, High 16 bits
-#define CC2420_FSMTC         0x20 // [R/W] Finite State Machine Time Constants
-#define CC2420_MANAND        0x21 // [R/W] Manual signal AND override register
-#define CC2420_MANOR         0x22 // [R/W] Manual signal OR override register
-#define CC2420_AGCCTRL       0x23 // [R/W] AGC Control Register
-#define CC2420_AGCTST0       0x24 // [R/W] AGC Test Register 0
-#define CC2420_AGCTST1       0x25 // [R/W] AGC Test Register 1
-#define CC2420_AGCTST2       0x26 // [R/W] AGC Test Register 2
-#define CC2420_FSTST0        0x27 // [R/W] Frequency Synthesizer Test Register 0
-#define CC2420_FSTST1        0x28 // [R/W] Frequency Synthesizer Test Register 1
-#define CC2420_FSTST2        0x29 // [R/W] Frequency Synthesizer Test Register 2
-#define CC2420_FSTST3        0x2a // [R/W] Frequency Synthesizer Test Register 3
-#define CC2420_RXBPFTST      0x2b // [R/W] Receiver Bandpass Filter Test Register
-#define CC2420_FSMSTATE      0x2c // [R  ] Finite State Machine State Status Register
-#define CC2420_ADCTST        0x2c // [R/W] ADC Test Register
-#define CC2420_DACTST        0x2e // [R/W] DAC Test Register
-#define CC2420_TOPTST        0x2f // [R/W] Top Level Test Register
+//=========================== registers =======================================
 
-// buffer
-#define CC2420_TXFIFO        0x3e // [  W] Transmit FIFO Byte Register
-#define CC2420_RXFIFO        0x3f // [R/W] Receiver FIFO Byte Register
+//===== 'functions'
 
-//=========================== typedef =========================================
 
-//=========================== variables =======================================
 
-//=========================== prototypes ======================================
+//===== address and structure
+
+/// [R/W] Main Control Register
+#define CC2420_MAIN          0x10
+
+/// [R/W] Modem Control Register 0
+#define CC2420_MDMCTRL0      0x11
+
+/// [R/W] Modem Control Register 1
+#define CC2420_MDMCTRL1      0x12
+
+// [R/W] RSSI and CCA Status and Control register
+#define CC2420_RSSI          0x13
+
+/// [R/W] Synchronisation word control register
+#define CC2420_SYNCWORD      0x14
+
+/// [R/W] Transmit Control Register
+#define CC2420_TXCTRL        0x15
+
+/// [R/W] Receive Control Register 0
+#define CC2420_RXCTRL0       0x16
+
+/// [R/W] Receive Control Register 1
+#define CC2420_RXCTRL1       0x17
+
+/// [R/W] Frequency Synthesizer Control and Status Register
+#define CC2420_FSCTRL        0x18
+
+/// [R/W] Security Control Register 0
+#define CC2420_SECCTRL0      0x19
+
+/// [R/W] Security Control Register 1
+#define CC2420_SECCTRL1      0x1a
+
+/// [R/W] Battery Monitor Control and Status Register
+#define CC2420_BATTMON       0x1b
+
+/// [R/W] Input / Output Control Register 0
+#define CC2420_IOCFG0        0x1c
+
+/// [R/W] Input / Output Control Register 1
+#define CC2420_IOCFG1        0x1d
+
+/// [R/W] Manufacturer ID, Low 16 bits
+#define CC2420_MANFIDL       0x1e
+typedef struct {
+   uint16_t mandif:12;
+   uint16_t partnum:4;
+} cc2420_manfidl_reg_t;
+
+/// [R/W] Manufacturer ID, High 16 bits
+#define CC2420_MANFIDH       0x1f
+
+/// [R/W] Finite State Machine Time Constants
+#define CC2420_FSMTC         0x20
+
+/// [R/W] Manual signal AND override register
+#define CC2420_MANAND        0x21
+
+/// [R/W] Manual signal OR override register
+#define CC2420_MANOR         0x22
+
+/// [R/W] AGC Control Register
+#define CC2420_AGCCTRL       0x23
+
+/// [R/W] AGC Test Register 0
+#define CC2420_AGCTST0       0x24
+
+/// [R/W] AGC Test Register 1
+#define CC2420_AGCTST1       0x25
+
+/// [R/W] AGC Test Register 2
+#define CC2420_AGCTST2       0x26
+
+/// [R/W] Frequency Synthesizer Test Register 0
+#define CC2420_FSTST0        0x27
+
+/// [R/W] Frequency Synthesizer Test Register 1
+#define CC2420_FSTST1        0x28
+
+/// [R/W] Frequency Synthesizer Test Register 2
+#define CC2420_FSTST2        0x29
+
+/// [R/W] Frequency Synthesizer Test Register 3
+#define CC2420_FSTST3        0x2a
+
+/// [R/W] Receiver Bandpass Filter Test Register
+#define CC2420_RXBPFTST      0x2b
+
+/// [R  ] Finite State Machine State Status Register
+#define CC2420_FSMSTATE      0x2c
+
+/// [R/W] ADC Test Register
+#define CC2420_ADCTST        0x2c
+
+/// [R/W] DAC Test Register
+#define CC2420_DACTST        0x2e
+
+/// [R/W] Top Level Test Register
+#define CC2420_TOPTST        0x2f
+
+//=========================== buffer ==========================================
+
+/// [  W] Transmit FIFO Byte Register
+#define CC2420_TXFIFO        0x3e
+
+/// [R/W] Receiver FIFO Byte Register
+#define CC2420_RXFIFO        0x3f
 
 #endif
--- firmware/openos/projects/telosb/01-bsp/01bsp_spi.c
+++ firmware/openos/projects/telosb/01-bsp/01bsp_spi.c
@@ -9,14 +9,18 @@
 #include "stdint.h"
 #include "board.h"
 #include "spi.h"
+#include "cc2420.h"
 
 /**
 \brief The program starts executing here.
 */
 int main(void)
 {  
-   uint8_t spi_tx_buffer[3];
-   uint8_t spi_rx_buffer[3];
+   uint8_t              spi_tx_buffer[3];
+   uint8_t              spi_rx_buffer[3];
+   cc2420_status_reg_t  cc2420_status_reg;
+   cc2420_manfidl_reg_t cc2420_manfidl_reg;
+   uint16_t             temp_reg;
    
    // initialize
    board_init();
@@ -30,6 +34,14 @@ int main(void)
    
    // retrieve radio manufacturer ID over SPI
    spi_txrx(&(spi_tx_buffer[0]),sizeof(spi_tx_buffer),&(spi_rx_buffer[0]));
+   
+   temp_reg = spi_rx_buffer[1]<<8 | spi_rx_buffer[2];
+   
+   cc2420_status_reg  = *((cc2420_status_reg_t*)&spi_rx_buffer[0]);
+   cc2420_manfidl_reg = *((cc2420_manfidl_reg_t*)&temp_reg);
+   
+   cc2420_manfidl_reg = cc2420_manfidl_reg;
+   temp_reg = temp_reg;
    
    // go back to sleep
    board_sleep();
