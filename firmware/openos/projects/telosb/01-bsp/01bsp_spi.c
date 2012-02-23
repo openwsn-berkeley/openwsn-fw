8e3dd4a7f9d5b71b9554c8c699f42eda6cd90bc1
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-02-23 06:52:55 +0000

    updating bsp spi project

--- firmware/openos/projects/telosb/01-bsp/01bsp_spi.c
+++ firmware/openos/projects/telosb/01-bsp/01bsp_spi.c
@@ -1,15 +1,12 @@
 /**
 \brief This program shows the use of the "spi" bsp module.
 
-
-
 \author Thomas Watteyne <watteyne@eecs.berkeley.edu>, February 2012
 */
 
 #include "stdint.h"
 #include "board.h"
 #include "spi.h"
-#include "cc2420.h"
 
 /**
 \brief The program starts executing here.
@@ -18,9 +15,6 @@ int main(void)
 {  
    uint8_t              spi_tx_buffer[3];
    uint8_t              spi_rx_buffer[3];
-   cc2420_status_t      cc2420_status;
-   cc2420_MANFIDL_reg_t cc2420_MANFIDL_reg;
-   uint16_t             temp_reg;
    
    // initialize
    board_init();
@@ -36,13 +30,6 @@ int main(void)
    spi_txrx(&(spi_tx_buffer[0]),sizeof(spi_tx_buffer),&(spi_rx_buffer[0]));
    
    temp_reg             = spi_rx_buffer[1]<<8 | spi_rx_buffer[2];
-   
-   cc2420_status        = *((cc2420_status_t*)&spi_rx_buffer[0]);
-   cc2420_MANFIDL_reg   = *((cc2420_MANFIDL_reg_t*)&temp_reg);
-   
-   cc2420_MANFIDL_reg   = cc2420_MANFIDL_reg;
-   temp_reg             = temp_reg;
-   cc2420_status        = cc2420_status;
    
    // go back to sleep
    board_sleep();
