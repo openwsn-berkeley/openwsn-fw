678f34d7ca810cdfdcbec817de908557dd51d54e
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-02-27 00:02:51 +0000

    board_info.h file for TelosB

---
+++ firmware/openos/bsp/telosb/board_info.h
@@ -0,0 +1,20 @@
+/**
+\brief GINA-specific definition of the "board" bsp module.
+
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, February 2012.
+*/
+
+#include "stdint.h"
+
+//=========================== variables =======================================
+
+static const uint8_t rreg_uriquery[] = "h=ucb";
+static const uint8_t infoBoardname[] = "TelosB";
+static const uint8_t infouCName[]    = "MSP430f1611";
+static const uint8_t infoRadioName[] = "CC2420";
+
+//=========================== prototypes ======================================
+
+//=========================== public ==========================================
+
+//=========================== private =========================================
