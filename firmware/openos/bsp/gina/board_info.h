96a9ad50d374ff14a3538d0fa7245bbaeda5c47f
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-02-27 00:01:05 +0000

    created board_info.h file for GINA

---
+++ firmware/openos/bsp/gina/board_info.h
@@ -0,0 +1,20 @@
+/**
+\brief GINA-specific definition of the "board" bsp module.
+
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, February 2012.
+*/
+
+#include "stdint.h"
+
+//=========================== variables =======================================
+
+static const uint8_t rreg_uriquery[] = "h=ucb";
+static const uint8_t infoBoardname[] = "GINA";
+static const uint8_t infouCName[]    = "MSP430f2618";
+static const uint8_t infoRadioName[] = "AT86RF231";
+
+//=========================== prototypes ======================================
+
+//=========================== public ==========================================
+
+//=========================== private =========================================
