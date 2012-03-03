61ddc3b8c3dbd0d9c96fa4d3225547e43050d2cd
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-03-03 07:03:16 +0000

    Creating OpenWSN project (WIP)

---
+++ firmware/openos/projects/common/03-app/openwsn_proj.c
@@ -0,0 +1,26 @@
+/**
+\brief This project runs the full OpenWSN stack on the GINA2.2b/c and GINA
+       basestations platforms.
+
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, August 2010
+*/
+
+#include "openwsn.h"
+//board
+#include "board.h"
+#include "leds.h"
+//openwsn
+#include "openwsn.h"
+#include "scheduler.h"
+#include "packetfunctions.h"
+#include "openqueue.h"
+#include "IEEE802154E.h"
+#include "idmanager.h"
+#include "openserial.h"
+
+void main(void) {
+   board_init();
+   scheduler_init();
+   openwsn_init();
+   scheduler_start();
+}
