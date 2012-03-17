06e7e54bbb3e2ee74f98e6a253c02cc449ea83af
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-03-17 05:07:04 +0000

    updating xpressohack 00std_spi example

--- firmware/openos/projects/xpressohack/00std_spi/src/main.c
+++ firmware/openos/projects/xpressohack/00std_spi/src/main.c
@@ -12,17 +12,8 @@
 #include <cr_section_macros.h>
 #include <NXP/crp.h>
 
-// Variable to store CRP value in. Will be placed automatically
-// by the linker when "Enable Code Read Protect" selected.
-// See crp.h header for more information
-__CRP const unsigned int CRP_WORD = CRP_NO_CRP ;
-
-// TODO: insert other include files here
-
-// TODO: insert other definitions and declarations here
-
 int main(void) {
-	uint8_t bit;
+	uint8_t byte;
 	
     // power SPI
 	LPC_SC->PCONP           |= (1<<10);
@@ -35,13 +26,10 @@ int main(void) {
 
 	// pin configuration
 	LPC_PINCON->PINSEL0     |= (1<<30)|(1<<31);  // SCK0  = P0.15
-	LPC_PINCON->PINSEL1     |= (1<<0 )|(1<<1 );  // SSEL0 = P0.16
+	//LPC_PINCON->PINSEL1     |= (1<<0 )|(1<<1 );
 	LPC_PINCON->PINSEL1     |= (1<<2 )|(1<<3 );  // MISO0 = P0.17
 	LPC_PINCON->PINSEL1     |= (1<<4 )|(1<<5 );  // MOSI0 = P0.18
-
-	LPC_GPIO0->FIODIR       |= (1<<16);
-	LPC_GPIO2->FIODIR       |= (1<<6 );
-
+	LPC_GPIO2->FIODIR       |= (1<<6 );          // SSEL0 = P2.6
 
 	// SPI in master mode
 	LPC_SPI->SPCR           |= (1<<5);           //  master mode
@@ -49,27 +37,20 @@ int main(void) {
 	// enable interrupt
 	// NVIC_EnableIRQ(SPI_IRQn);
 
-	bit = 0;
-	while(bit<0xff) {
-        LPC_GPIO0->FIOCLR   |= (1<<16);
+	byte = 0;
+	while(byte<0xff) {
+        // CS low
         LPC_GPIO2->FIOCLR   |= (1<<6);
 
-		LPC_SPI->SPDR        = bit;
+        // send byte
+		LPC_SPI->SPDR        = byte;
 
+		// wait for byte being sent
 		while(!(LPC_SPI->SPSR & (1<<7)));
 
+		// CS high
 		LPC_GPIO2->FIOSET   |= (1<<6);
-		LPC_GPIO0->FIOSET   |= (1<<16);
 
-        bit++;
+		byte++;
 	}
-
-	// TODO: insert code here
-
-	// Enter an infinite loop, just incrementing a counter
-	volatile static int i = 0 ;
-	while(1) {
-		i++ ;
-	}
-	return 0 ;
 }
