0827ad16ff2808e2398b0b2dfe7f7cc70376db84
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-03-18 19:20:22 +0000

    indentation

--- firmware/openos/projects/xpressohack/00std_spi/src/main.c
+++ firmware/openos/projects/xpressohack/00std_spi/src/main.c
@@ -13,44 +13,43 @@
 #include <NXP/crp.h>
 
 int main(void) {
-	uint8_t byte;
-	
-    // power SPI
-	LPC_SC->PCONP           |= (1<<10);
+   uint8_t byte;
+   
+   // power SPI
+   LPC_SC->PCONP            |= (1<<10);
 
-    // clock SPI
-	LPC_SC->PCLKSEL0        |= (1<<16)|(1<<17);
+   // clock SPI
+   LPC_SC->PCLKSEL0         |= (1<<16)|(1<<17);
 
-	// SPI0 Clock counter setting
-	LPC_SPI->SPCCR           = 8;
+   // SPI0 Clock counter setting
+   LPC_SPI->SPCCR            = 8;
 
-	// pin configuration
-	LPC_PINCON->PINSEL0     |= (1<<30)|(1<<31);  // SCK0  = P0.15
-	//LPC_PINCON->PINSEL1     |= (1<<0 )|(1<<1 );
-	LPC_PINCON->PINSEL1     |= (1<<2 )|(1<<3 );  // MISO0 = P0.17
-	LPC_PINCON->PINSEL1     |= (1<<4 )|(1<<5 );  // MOSI0 = P0.18
-	LPC_GPIO2->FIODIR       |= (1<<6 );          // SSEL0 = P2.6
+   // pin configuration
+   LPC_PINCON->PINSEL0      |= (1<<30)|(1<<31);  // [P0.15] SCK0
+   LPC_PINCON->PINSEL1      |= (1<<2 )|(1<<3 );  // [P0.17] MISO0
+   LPC_PINCON->PINSEL1      |= (1<<4 )|(1<<5 );  // [P0.18] MOSI0
+   LPC_GPIO2->FIODIR        |= (1<<6 );          // [P2.6]  SSEL0
 
-	// SPI in master mode
-	LPC_SPI->SPCR           |= (1<<5);           //  master mode
+   // SPI in master mode
+   LPC_SPI->SPCR            |= (1<<5);
 
-	// enable interrupt
-	// NVIC_EnableIRQ(SPI_IRQn);
+   // enable interrupt
+   // NVIC_EnableIRQ(SPI_IRQn);
 
-	byte = 0;
-	while(byte<0xff) {
-        // CS low
-        LPC_GPIO2->FIOCLR   |= (1<<6);
+   byte = 0;
+   while(byte<0xff) {
+      // CS low
+      LPC_GPIO2->FIOCLR     |= (1<<6);
 
-        // send byte
-		LPC_SPI->SPDR        = byte;
+      // send byte
+      LPC_SPI->SPDR          = byte;
 
-		// wait for byte being sent
-		while(!(LPC_SPI->SPSR & (1<<7)));
+      // wait for byte being sent
+      while(!(LPC_SPI->SPSR & (1<<7)));
 
-		// CS high
-		LPC_GPIO2->FIOSET   |= (1<<6);
+      // CS high
+      LPC_GPIO2->FIOSET     |= (1<<6);
 
-		byte++;
-	}
+      byte++;
+   }
 }
