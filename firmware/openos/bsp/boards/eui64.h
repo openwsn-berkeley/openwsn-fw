82706047b5c6b5529077a08158c0ffbf1404297c
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-03-21 06:18:23 +0000

    adding dummy eui64 bsp modules

---
+++ firmware/openos/bsp/boards/eui64.h
@@ -0,0 +1,22 @@
+/**
+\brief Cross-platform declaration "eui64" bsp module.
+
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, March 2012.
+*/
+
+#ifndef __EUI64_H
+#define __EUI64_H
+
+#include "stdint.h"
+ 
+//=========================== define ==========================================
+
+//=========================== typedef =========================================
+
+//=========================== variables =======================================
+
+//=========================== prototypes ======================================
+
+void eui64_get(uint8_t* addressToWrite);
+
+#endif
---
+++ firmware/openos/bsp/boards/telosb/eui64.c
@@ -0,0 +1,22 @@
+/**
+\brief TelosB-specific definition of the "eui64" bsp module.
+
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, March 2012.
+*/
+
+#include "string.h"
+#include "eui64.h"
+
+//=========================== defines =========================================
+
+//=========================== variables =======================================
+
+//=========================== prototypes ======================================
+
+//=========================== public ==========================================
+
+void eui64_get(uint8_t* addressToWrite) {
+   memset(addressToWrite,0,8);
+}
+
+//=========================== private =========================================
