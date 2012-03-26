c5bacbecc1a51a0a2a600a175f559ca8bcbadc51
Fabien Chraim <fabien.chraim@gmail.com>
2012-03-26 17:17:14 +0000

    added code to turn off sensors on the gina platform in board initialization.

--- firmware/openos/bsp/boards/gina/board.c
+++ firmware/openos/bsp/boards/gina/board.c
@@ -15,6 +15,12 @@
 #include "bsp_timer.h"
 #include "radio.h"
 #include "radiotimer.h"
+//sensors
+#include "gyro.h"
+#include "large_range_accel.h"
+#include "magnetometer.h"
+#include "sensitive_accel_temperature.h"
+
 
 //=========================== variables =======================================
 
@@ -45,9 +51,16 @@ void board_init() {
    leds_init();
    uart_init();
    spi_init();
+   i2c_init();
    bsp_timer_init();
    radio_init();
    radiotimer_init();
+   
+   //turn sensors off
+   gyro_disable();
+   large_range_accel_disable();
+   magnetometer_disable();
+   sensitive_accel_temperature_disable();
    
    // enable interrupts
    __bis_SR_register(GIE);
