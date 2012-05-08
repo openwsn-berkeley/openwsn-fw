8ac30a58472e158d0178966108515da00a1eea05
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-05-08 06:53:36 +0000

    updated 01bsp_timer project

--- firmware/openos/projects/common/01bsp_timer/01bsp_timer.c
+++ firmware/openos/projects/common/01bsp_timer/01bsp_timer.c
@@ -10,8 +10,6 @@ can use this project with any platform.
 #include "stdint.h"
 #include "string.h"
 #include "board.h"
-#include "leds.h"
-#include "bsp_timer.h"
 #include "debugpins.h"
 #include "radiotimer.h"
 
@@ -20,24 +18,19 @@ can use this project with any platform.
 //=========================== variables =======================================
 
 typedef struct {
-   uint8_t radio_busy;
-   uint8_t timer_busy;
-   uint8_t num_overflow;
-   uint8_t num_compare;
-   uint8_t num_startFrame;
-   uint8_t num_endFrame;
+   uint16_t num_overflow;
+   uint16_t num_compare;
 } app_vars_t;
 
 app_vars_t app_vars;
 
 //=========================== prototypes ======================================
 
-void cb_timer_0();
-void cb_timer_1();
-void cb_timer_2();
+void cb_overflow();
+void cb_compare();
 
 //=========================== main ============================================
-uint32_t count=0;
+
 /**
 \brief The program starts executing here.
 */
@@ -46,30 +39,11 @@ int mote_main(void)
    // initialize board
    board_init();
    
-   radiotimer_setOverflowCb(cb_timer_0);
-   radiotimer_setCompareCb(cb_timer_1);
-   //radiotimer_setPeriod(327);
-   radiotimer_start(327);
-   radiotimer_schedule(33);
-
-   // start timer_0
-  /* timers_start(0,
-                1,
-                TIMER_PERIODIC,
-                cb_timer_0);*/
+   radiotimer_setOverflowCb(cb_overflow);
+   radiotimer_setCompareCb(cb_compare);
+   radiotimer_start(0xffff);      // 0xffff @32kHz = 2s
+   radiotimer_schedule(0x7fff);   // 0x7fff @32kHz = 1s
    
-//   // start timer_1
-//   timers_start(1,
-//                5000,
-//                TIMER_PERIODIC,
-//                cb_timer_1);
-//
-//   // start timer_2
-//   timers_start(2,
-//                7000,
-//                TIMER_PERIODIC,
-//                cb_timer_2);
-//
    while (1) {
       board_sleep();
    }
@@ -77,20 +51,18 @@ int mote_main(void)
 
 //=========================== callbacks =======================================
 
-void cb_timer_0() {
-   //leds_error_toggle();
-	debugpins_frame_toggle();
-
+void cb_overflow() {
+   // toggle pin
+   debugpins_frame_toggle();
+   
+   // increment counter
+   app_vars.num_overflow++;
 }
 
-void cb_timer_1() {
-  // leds_radio_toggle();
-	debugpins_fsm_toggle();
-	count++;
-	if (count%2==0)	 radiotimer_schedule(66);
-	else radiotimer_schedule(11);
-}
-
-void cb_timer_2() {
-   leds_sync_toggle();
+void cb_compare() {
+   // toggle pin
+   debugpins_fsm_toggle();
+   
+   // increment counter
+   app_vars.num_compare++;
 }
