dcdbc4253e5967c60e3a5070eadc0cc3357b4cf7
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-05-14 02:13:58 +0000

    removed duplicate 01bsp_debugpins.c file

--- firmware/openos/projects/common/01bsp_debugpins/01bsp_debugpins.c
+++ firmware/openos/projects/common/01bsp_debugpins/01bsp_debugpins.c
@@ -1,7 +1,8 @@
 /**
-\brief This is a program which shows how to use the bsp modules for the board  and debugpins.
+\brief This is a program to verify the correct functionality of the "debugpins"
+       bsp module.
 
-\author Chuang Qian <cqian@berkeley.edu>, Mayy 2012
+\author Chuang Qian <cqian@berkeley.edu>, May 2012.
 */
 
 #include "stdint.h"
@@ -18,17 +19,13 @@ int mote_main(void) {
    
    board_init();
    
-   debugpins_init();
-
-
-      while (1) {
-        for (delay=0xffff;delay>0;delay--);
-        debugpins_frame_toggle();
-        debugpins_slot_toggle();
-        debugpins_fsm_toggle();
-        debugpins_task_toggle();
-        debugpins_isr_toggle();
-        debugpins_radio_toggle();
-      
+   while (1) {
+      for (delay=0xffff;delay>0;delay--);
+      debugpins_frame_toggle();
+      debugpins_slot_toggle();
+      debugpins_fsm_toggle();
+      debugpins_task_toggle();
+      debugpins_isr_toggle();
+      debugpins_radio_toggle();
    }
 }
