f984bb10315476905958e7352f4a3d56e6a2c221
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2012-08-30 03:55:53 +0000

    bumping TelosB's UART baudrate to 115200

--- firmware/openos/bsp/boards/telosb/uart.c
+++ firmware/openos/bsp/boards/telosb/uart.c
@@ -31,11 +31,20 @@ void uart_init() {
    UCTL1                     =  SWRST;           // hold UART1 module in reset
    UCTL1                    |=  CHAR;            // 8-bit character
    
+   /*
    //9600 baud, clocked from 32kHz ACLK
    UTCTL1                   |=  SSEL0;           // clocking from ACLK
    UBR01                     =  0x03;            // 32768/9600 = 3.41
    UBR11                     =  0x00;            //
    UMCTL1                    =  0x4A;            // modulation
+   */
+   
+   //115200 baud, clocked from 4.8MHz SMCLK
+   UTCTL1                   |=  SSEL1;           // clocking from SMCLK
+   UBR01                     =  41;              // 4.8MHz/115200 - 41.66
+   UBR11                     =  0x00;            //
+   UMCTL1                    =  0x4A;            // modulation
+   
    
    ME2                      |=  UTXE1 + URXE1;   // enable UART1 TX/RX
    UCTL1                    &= ~SWRST;           // clear UART1 reset bit
