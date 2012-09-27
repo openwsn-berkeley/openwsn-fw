0a63b8ccb358d587ac8496ebe36e969514c6a3e6
Xavier Vilajosana <xvilajosana@eecs.berkeley.edu>
2012-09-27 21:28:08 +0000

    fine tunning telosb state machine timming

--- firmware/openos/bsp/boards/telosb/board_info.h
+++ firmware/openos/bsp/boards/telosb/board_info.h
@@ -52,10 +52,10 @@ to return the board's description.
 // time-slot related
 #define PORT_TsSlotDuration                 491   // counter counts one extra count, see datasheet
 // execution speed related
-#define PORT_maxTxDataPrepare               95    //  2899us (measured 2420us)
+#define PORT_maxTxDataPrepare               100    //  2899us (measured 2420us)
 #define PORT_maxRxAckPrepare                20    //   610us (measured  474us)
 #define PORT_maxRxDataPrepare               33    //  1000us (measured  477us)
-#define PORT_maxTxAckPrepare                26    //   792us (measured  746us)
+#define PORT_maxTxAckPrepare                40    //   792us (measured  746us)- cannot be bigger than 28.. is the limit for telosb as actvitiy_rt5 is executed almost there.
 // radio speed related
 #define PORT_delayTx                        12    //   366us (measured  352us)
 #define PORT_delayRx                        0     //     0us (can not measure)
