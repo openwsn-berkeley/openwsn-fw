57a7cf4d1c5410ea0e0701babcf74e0b56da3ed5
Thomas Watteyne <twatteyne@gmail.com>
2012-11-27 07:36:26 -0800

    Fixing while(1) loop which caused compilation warning. Part of FW-7.

--- firmware/openos/projects/common/01bsp_radio_range_tx/01bsp_radio_tx.c
+++ firmware/openos/projects/common/01bsp_radio_range_tx/01bsp_radio_tx.c
@@ -50,7 +50,7 @@ typedef struct {
    uint8_t     packet[LENGTH_PACKET];
    uint8_t     packet_len;
    uint8_t     packet_num;
-    int8_t     rxpk_rssi;
+   int8_t     rxpk_rssi;
    uint8_t     rxpk_lqi;
    uint8_t     rxpk_crc;
    uint8_t     lock;//semaphore
@@ -73,60 +73,70 @@ void     cb_timer();
 \brief The program starts executing here.
 */
 int mote_main(void) {
-   uint8_t i;
+   uint8_t  i;
    uint16_t j;
+   
    // clear local variables
    memset(&app_vars,0,sizeof(app_vars_t));
    
    // initialize board
    board_init();
    leds_error_on(); 
-   // add callback functions radio
+   
+   // add radio callback functions
    radio_setOverflowCb(cb_radioTimerOverflows);
    radio_setCompareCb(cb_radioTimerCompare);
    radio_setStartFrameCb(cb_startFrame);
    radio_setEndFrameCb(cb_endFrame);
    
-    // prepare packet
+   // prepare packet
    app_vars.packet_len = sizeof(app_vars.packet);
    for (i=0;i<app_vars.packet_len;i++) {
-        app_vars.packet[i] = i;
+      app_vars.packet[i] = i;
    }
    
    bsp_timer_set_callback(cb_timer);
+   
    // prepare radio
    radio_rfOn();
    radio_setFrequency(CHANNEL); 
    radio_rfOff();
-               
+   
    // start transmitting packet
    leds_radio_off();
    leds_sync_on();
-   //for (i=0;i<255;i++) {
-   while(1){
+   
+   for (i=0;i<255;i++) {
+      
       i=i%255;
-      app_vars.packet_num=i;
-      app_vars.packet[0]=i;//set the packet number.
+      
+      app_vars.packet_num = i;
+      app_vars.packet[0]  = i; //set the packet number.
+      
       radio_loadPacket(app_vars.packet,app_vars.packet_len);
       radio_txEnable();
       radio_txNow();
+      
       app_vars.lock=1;//get semaphore unitl tx done
-      while ( app_vars.lock);
+      while (app_vars.lock);
+      
       for (j=0;j<0xFFFF;j++); //small wait
       leds_circular_shift();
-     
-//      app_vars.lock=1;//get semaphore
-//      bsp_timer_scheduleIn(327);//1ms
-//    
-//      while(app_vars.lock);//delay 1ms aprox
+      
+      //      app_vars.lock=1;//get semaphore
+      //      bsp_timer_scheduleIn(327);//1ms
+      //    
+      //      while(app_vars.lock);//delay 1ms aprox
       leds_radio_toggle();
-    }//end send.
+   } //end send.
+   
    leds_sync_off();
    leds_radio_on();
    
    app_vars.state = APP_STATE_TX;
+   
    while (1){
-     board_sleep();
+      board_sleep();
    }
 }
 
@@ -153,8 +163,8 @@ void cb_endFrame(uint16_t timestamp) {
    // update debug stats
    app_dbg.num_endFrame++;
    
-    app_vars.lock=0;//release semaphore
-     
+   app_vars.lock=0;//release semaphore
+   
 }
 
 void cb_timer() {
