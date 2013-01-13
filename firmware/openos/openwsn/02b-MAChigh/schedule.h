6451cd02026383fadfa09a414b28032c5862c605
Thomas Watteyne <twatteyne@gmail.com>
2013-01-13 02:35:51 -0800

    Committing missing file.

--- firmware/openos/openwsn/02b-MAChigh/schedule.h
+++ firmware/openos/openwsn/02b-MAChigh/schedule.h
@@ -12,11 +12,46 @@
 
 //=========================== define ==========================================
 
-#define MAXACTIVESLOTS  8    // the maximum number of active slots
-#define MINBE           2    // min backoff exponent, used in shared TX slots
-#define MAXBE           4    // max backoff exponent, used in shared TX slots
+/**
+\brief The length of the superframe, in slots.
 
- 
+The superframe repears over time and can be arbitrarly long.
+*/
+#define SUPERFRAME_LENGTH    9
+
+#define NUMADVSLOTS          1
+#define NUMSHAREDTXRX        4
+#define NUMSERIALRX          3
+
+/**
+\brief Maximum number of active slots in a superframe.
+
+Note that this is merely used to allocate RAM memory for the schedule. The
+schedule is represented, in RAM, by a table. There is one row per active slot
+in that table; a slot is "active" when it is not of type CELLTYPE_OFF.
+
+Set this number to the exact number of active slots you are planning on having
+in your schedule, so not to waste RAM.
+*/
+#define MAXACTIVESLOTS       (NUMADVSLOTS+NUMSHAREDTXRX+NUMSERIALRX)
+
+/**
+\brief Minimum backoff exponent.
+
+Backoff is used only in slots that are marked as shared in the schedule. When
+not shared, the mote assumes that schedule is collision-free, and therefore
+does not use any backoff mechanism when a transmission fails.
+*/
+#define MINBE                2
+
+/**
+\brief Maximum backoff exponent.
+
+See MINBE for an explanation of backoff.
+*/
+#define MAXBE                4
+
+
 //=========================== typedef =========================================
 
 typedef uint8_t    channelOffset_t;
@@ -66,34 +101,41 @@ typedef struct {
    scheduleEntry_t scheduleEntry;
 } debugScheduleEntry_t;
 PRAGMA(pack());
+
 //=========================== variables =======================================
 
 //=========================== prototypes ======================================
 
 // admin
-          void            schedule_init();
-          bool            debugPrint_schedule();
+void               schedule_init();
+bool               debugPrint_schedule();
 // from uRES
-          void            schedule_setFrameLength(frameLength_t newFrameLength);
-          void            schedule_addActiveSlot(slotOffset_t    slotOffset,
-                                                 cellType_t      type,
-                                                 bool            shared,
-                                                 uint8_t         channelOffset,
-                                                 open_addr_t*    neighbor);
+void               schedule_setFrameLength(frameLength_t newFrameLength);
+void               schedule_addActiveSlot(
+                        slotOffset_t   slotOffset,
+                        cellType_t     type,
+                        bool           shared,
+                        uint8_t        channelOffset,
+                        open_addr_t*   neighbor
+                   );
 // from IEEE802154E
- void            schedule_syncSlotOffset(slotOffset_t targetSlotOffset);
- void            schedule_advanceSlot();
- slotOffset_t    schedule_getNextActiveSlotOffset();
- frameLength_t   schedule_getFrameLength();
- cellType_t      schedule_getType();
- void            schedule_getNeighbor(open_addr_t* addrToWrite);
- channelOffset_t schedule_getChannelOffset();
- bool            schedule_getOkToSend();
- void            schedule_indicateRx(asn_t*   asnTimestamp);
- void            schedule_indicateTx(asn_t*   asnTimestamp,
-                                              bool     succesfullTx);
- //void            scheduleBuf_getAll(scheduleEntry_t *blist);//deprecated
- void            schedule_getNetDebugInfo(netDebugScheduleEntry_t *schlist,uint8_t maxbytes);
+ void              schedule_syncSlotOffset(slotOffset_t targetSlotOffset);
+ void              schedule_advanceSlot();
+ slotOffset_t      schedule_getNextActiveSlotOffset();
+ frameLength_t     schedule_getFrameLength();
+ cellType_t        schedule_getType();
+ void              schedule_getNeighbor(open_addr_t* addrToWrite);
+ channelOffset_t   schedule_getChannelOffset();
+ bool              schedule_getOkToSend();
+ void              schedule_indicateRx(asn_t*   asnTimestamp);
+ void              schedule_indicateTx(
+                        asn_t*    asnTimestamp,
+                        bool      succesfullTx
+                   );
+ void              schedule_getNetDebugInfo(
+                        netDebugScheduleEntry_t* schlist,
+                        uint8_t                  maxbytes
+                   );
 
 /**
 \}
