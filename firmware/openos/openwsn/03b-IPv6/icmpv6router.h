0fef95a50f95902c20c5c9ce984e7f3ce143e79e
Thomas Watteyne <twatteyne@gmail.com>
2013-01-27 14:41:17 -0800

    remove not unused trigger for icmpv6router.

--- firmware/openos/openwsn/03b-IPv6/icmpv6router.h
+++ firmware/openos/openwsn/03b-IPv6/icmpv6router.h
@@ -17,7 +17,6 @@
 //=========================== prototypes ======================================
 
 void icmpv6router_init();
-void icmpv6router_trigger();
 void icmpv6router_sendDone(OpenQueueEntry_t* msg, error_t error);
 void icmpv6router_receive(OpenQueueEntry_t* msg);
 
