3df648f1b2a7ddff55a96cf414b605f3a0d6cdf3
Thomas Watteyne <twatteyne@gmail.com>
2013-05-12 13:02:06 -0700

    SW-34. Build openwsn module from SCons.

--- test_openwn.py
+++ test_openwn.py
@@ -3,3 +3,11 @@ import os
 if __name__=='__main__':
     here = sys.path[0]
     sys.path.insert(0, os.path.join(here, 'build', 'lib.win32-2.7'))# contains openwsn module
+
+import openwsn
+
+mote = openwsn.OpenMote()
+print str(mote)
+print mote.bsp_timer_isr()
+
+raw_input('poi')
