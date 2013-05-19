c792a8939ed368949585ee8f1c22ff6c40f9916e
Thomas Watteyne <twatteyne@gmail.com>
2013-05-19 01:20:11 -0700

    SW-34. Fixing implementation of EUI64 BSP module for Python "board".

--- firmware/openos/bsp/boards/python/eui64_obj.c
+++ firmware/openos/bsp/boards/python/eui64_obj.c
@@ -15,9 +15,7 @@
 //=========================== public ==========================================
 
 void eui64_get(OpenMote* self, uint8_t* addressToWrite) {
-   printf("eui64_get\r\n");
    PyObject*  result;
-   PyObject*  euiList;
    PyObject*  item;
    uint8_t    i;
    
@@ -31,21 +29,19 @@ void eui64_get(OpenMote* self, uint8_t* addressToWrite) {
       printf("[CRITICAL] eui64_get() returned NULL\r\n");
    }
    
-   // verify 
-   PyArg_ParseTuple(result, "O", &euiList);
-   if (!PySequence_Check(euiList)) {
+   // verify
+   if (!PySequence_Check(result)) {
       printf("[CRITICAL] eui64_get() did not return a list\r\n");
    }
-   if (PyList_Size(euiList)!=8) {
+   if (PyList_Size(result)!=8) {
       printf("[CRITICAL] eui64_get() did not return a list of exactly 8 elements\r\n");
    }
    
    for (i=0;i<8;i++) {
-      item = PyList_GetItem(euiList, i);
+      item = PyList_GetItem(result, i);
       addressToWrite[i] = (uint8_t)PyInt_AsLong(item);
    }
    Py_DECREF(result);
-   Py_DECREF(euiList);
 }
 
 //=========================== private =========================================
--- test_openwn.py
+++ test_openwn.py
@@ -4,25 +4,46 @@ if __name__=='__main__':
     here = sys.path[0]
     sys.path.insert(0, os.path.join(here, 'firmware','openos','projects','common'))# contains openwsn module
 
-try:
+import re
 
-    import oos_openwsn
+#============================ get notification IDs ============================
 
-    def callback(notifId):
-       print "callback {0} called".format(notifId)
-       raw_input("press Enter for next")
+f = open(os.path.join('firmware','openos','bsp','boards','python','openwsnmodule_obj.h'))
+lines = f.readlines()
+f.close()
 
-    # create instance
-    mote = oos_openwsn.OpenMote()
-    print str(mote)
-    
-    # install some callbacks
-    for i in range(81):
-       temp_lambda = lambda notifId=i:callback(notifId)
-       mote.set_callback(i,temp_lambda)
-    
-    # call other methods
-    mote.supply_on()
+notifString = []
 
-except Exception as err:
-    raw_input('ERROR: {0}'.format(err))
+for line in lines:
+    m = re.search('MOTE_NOTIF_(\w+)',line)
+    if m:
+        if m.group(1) not in notifString:
+            notifString += [m.group(1)]
+
+def notifId(s):
+    assert s in notifString
+    return notifString.index(s)
+
+import oos_openwsn
+
+def default_callback(id):
+   print "P: {0}() (callback {1})".format(notifString[id],id)
+   raw_input("press Enter for next")
+
+def eui64_get():
+   return range(8)
+
+# create instance
+mote = oos_openwsn.OpenMote()
+print str(mote)
+
+# install default callback
+for i in range(len(notifString)-1):
+    temp_lambda = lambda id=i:default_callback(id)
+    mote.set_callback(i,temp_lambda)
+
+# overwrite some callbacks
+mote.set_callback(notifId('eui64_get'),eui64_get)
+
+# call other methods
+mote.supply_on()
