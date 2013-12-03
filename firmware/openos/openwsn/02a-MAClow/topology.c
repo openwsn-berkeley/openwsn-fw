48844e9099886dfedbf45d7e596b4240ce519218
Thomas Watteyne <twatteyne@gmail.com>
2013-12-03 01:02:28 -0800

    SW-172. Fix.

--- firmware/openos/openwsn/02a-MAClow/topology.c
+++ firmware/openos/openwsn/02a-MAClow/topology.c
@@ -1,21 +1,24 @@
 #include "openwsn.h"
 #include "topology.h"
-#include "idmanager.h"
 
 //=========================== defines =========================================
 
-#define TOPOLOGY_MOTE1 0x01
-#define TOPOLOGY_MOTE2 0x02
-#define TOPOLOGY_MOTE3 0x03
-#define TOPOLOGY_MOTE4 0x04
 //=========================== variables =======================================
 
 //=========================== prototypes ======================================
 
 //=========================== public ==========================================
 
-bool topology_isAcceptablePacket(ieee802154_header_iht* ieee802514_header) {
-   bool returnVal;
+/**
+\brief Force a topology.
+
+This function is used to force a certain topology, by hard-coding the list of
+acceptable neighbors for a given mote. This function is invoke each time a
+packet is received. If it returns FALSE, the packet is silently dropped, as if
+it were never received.
+
+An example body for this function is
+
    switch (idmanager_getMyID(ADDR_64B)->addr_64b[7]) {
       case TOPOLOGY_MOTE1:
          if (ieee802514_header->src.addr_64b[7]==TOPOLOGY_MOTE2) {
@@ -32,25 +35,18 @@ bool topology_isAcceptablePacket(ieee802154_header_iht* ieee802514_header) {
             returnVal=FALSE;
          }
          break;
-       case TOPOLOGY_MOTE3:
-         if (ieee802514_header->src.addr_64b[7]==TOPOLOGY_MOTE2 ||
-             ieee802514_header->src.addr_64b[7]==TOPOLOGY_MOTE4) {
-            returnVal=TRUE;
-         } else {
-            returnVal=FALSE;
-         }
-         break;
-       case TOPOLOGY_MOTE4:
-         if (ieee802514_header->src.addr_64b[7]==TOPOLOGY_MOTE3) {
-            returnVal=TRUE;
-         } else {
-            returnVal=FALSE;
-         }
-         break;
       default:
          returnVal=TRUE;
    }
    return returnVal;
+
+\param[in] ieee802514_header The parsed IEEE802.15.4 MAC header.
+
+\return TRUE if the packet can be received.
+\return FALSE if the packet should be silently dropped.
+*/
+bool topology_isAcceptablePacket(ieee802154_header_iht* ieee802514_header) {
+   return TRUE;
 }
 
 //=========================== private =========================================
