88301a67067cab058804e347c74609e002c6ffa3
Pere Tuset <peretuset@gmail.com>
2014-01-28 14:56:22 +0100

    Changes in the CC2538 BSP to fix a bug and add comments to the code.

--- firmware/openos/bsp/boards/cc2538/debugpins.c
+++ firmware/openos/bsp/boards/cc2538/debugpins.c
@@ -14,8 +14,7 @@
 //=========================== defines =========================================
 // Board dbPINS defines
 #define BSP_PINA_BASE           GPIO_A_BASE
-#define BSP_PIND_BASE           GPIO_C_BASE
-
+#define BSP_PIND_BASE           GPIO_D_BASE
 
 #define BSP_PINA_4              GPIO_PIN_4      //!< PA4 -- frame -RF1.5
 #define BSP_PINA_5              GPIO_PIN_5      //!< PA5 -- isr   -RF1.11
@@ -25,43 +24,36 @@
 #define BSP_PIND_1              GPIO_PIN_1      //!< PD1 -- task  -RF1.10
 #define BSP_PIND_0              GPIO_PIN_0      //!< PD0 -- radio -RF1-12
 
-
 //=========================== variables =======================================
 
 //=========================== prototypes ======================================
+
 port_INLINE void bspDBpinToggle(uint32_t base,uint8_t ui8Pin);
 
 //=========================== public ==========================================
 
 void debugpins_init() {
+   GPIOPinTypeGPIOOutput(BSP_PINA_BASE, BSP_PINA_4 | BSP_PINA_5);
+   GPIOPinTypeGPIOOutput(BSP_PIND_BASE, BSP_PIND_3 | BSP_PIND_2 | BSP_PIND_1 | BSP_PIND_0);
 
-
-  // GPIOPinTypeGPIOOutput(BSP_PINA_BASE, BSP_PINA_4|BSP_PINA_5);
-   GPIOPinTypeGPIOOutput(BSP_PIND_BASE, BSP_PIND_3|BSP_PIND_2|BSP_PIND_1|BSP_PIND_0);
-   
-   debugpins_frame_clr();
-   debugpins_slot_clr();
-   debugpins_fsm_clr();
-   debugpins_task_clr();
-   debugpins_isr_clr();
-   debugpins_radio_clr();
+   GPIOPinWrite(BSP_PINA_BASE, (BSP_PINA_4 | BSP_PINA_5), 0x00);
+   GPIOPinWrite(BSP_PIND_BASE, (BSP_PIND_3 | BSP_PIND_2 | BSP_PIND_1 | BSP_PIND_0), 0);
 }
 
 // PA4
 void debugpins_frame_toggle() {
-	//bspDBpinToggle(BSP_PINA_BASE,BSP_PINA_4);
+	bspDBpinToggle(BSP_PINA_BASE, BSP_PINA_4);
 }
 void debugpins_frame_clr() {
-    //GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_4, 0);
+    GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_4, 0);
 }
-
 void debugpins_frame_set() {
-   // GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_4, BSP_PINA_4);
+   GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_4, BSP_PINA_4);
 }
 
 // PD3
 void debugpins_slot_toggle() {
-	bspDBpinToggle(BSP_PIND_BASE,BSP_PIND_3);
+	bspDBpinToggle(BSP_PIND_BASE, BSP_PIND_3);
 }
 void debugpins_slot_clr() {
 	GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_3, 0);
@@ -72,7 +64,7 @@ void debugpins_slot_set() {
 
 // PD2
 void debugpins_fsm_toggle() {
-	bspDBpinToggle(BSP_PIND_BASE,BSP_PIND_2);
+	bspDBpinToggle(BSP_PIND_BASE, BSP_PIND_2);
 }
 void debugpins_fsm_clr() {
 	GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_2, 0);
@@ -94,18 +86,18 @@ void debugpins_task_set() {
 
 // PA5
 void debugpins_isr_toggle() {
-	//bspDBpinToggle(BSP_PINA_BASE,BSP_PINA_5);
+	bspDBpinToggle(BSP_PINA_BASE, BSP_PINA_5);
 }
 void debugpins_isr_clr() {
-	//GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_5, 0);
+	GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_5, 0);
 }
 void debugpins_isr_set() {
-	//GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_5, BSP_PINA_5);
+	GPIOPinWrite(BSP_PINA_BASE, BSP_PINA_5, BSP_PINA_5);
 }
 
 // PD0
 void debugpins_radio_toggle() {
-	bspDBpinToggle(BSP_PIND_BASE,BSP_PIND_0);
+	bspDBpinToggle(BSP_PIND_BASE, BSP_PIND_0);
 }
 void debugpins_radio_clr() {
 	GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_0, 0);
@@ -114,13 +106,9 @@ void debugpins_radio_set() {
 	GPIOPinWrite(BSP_PIND_BASE, BSP_PIND_0, BSP_PIND_0);
 }
 
-
-
 //------------ private ------------//
 
-
-
-port_INLINE void bspDBpinToggle(uint32_t base,uint8_t ui8Pin)
+port_INLINE void bspDBpinToggle(uint32_t base, uint8_t ui8Pin)
 {
     //
     // Get current pin values of selected bits
@@ -137,5 +125,3 @@ port_INLINE void bspDBpinToggle(uint32_t base,uint8_t ui8Pin)
     //
     GPIOPinWrite(base, ui8Pin, ui32Toggle);
 }
-
-
