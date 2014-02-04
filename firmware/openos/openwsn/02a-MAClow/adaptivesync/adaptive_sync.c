07ca15d1c6a40b633a6289085687d402ff2ab242
changtengfei <tengfei.chang@gmail.com>
2014-02-03 19:15:29 -0800

    FW-186. Add adaptivesync/adaptive_sync.c and adaptivesync/adaptive_sync.h

---
+++ firmware/openos/openwsn/02a-MAClow/adaptivesync/adaptive_sync.c
@@ -0,0 +1,147 @@
+/**
+\brief this file is used for the time synchronizatino between different hardware platform
+
+\auther Tengfei Chang <tengfei.chang@gmail.com>, Janauray ,2014.
+*/
+#include "adaptive_sync.h"
+#include "IEEE802154E.h"
+#include "radio.h"
+#include "openserial.h"
+#include "leds.h"
+#include "neighbors.h"
+#include "debugpins.h"
+
+//=========================== define ==========================================
+
+//=========================== type ============================================
+
+typedef struct {
+  open_addr_t* neighborID;
+  uint32_t     compensationSlots; // compensation interval, counted by slots 
+}compensationInfo_t;
+
+typedef struct {
+  adaptive_sync_state_t clockState;
+  PORT_RADIOTIMER_WIDTH lapsedSlots;        // since last synchronizatino, this number of slots have  lapsed.
+  uint16_t              compensationTimeout;// decrease one every slot, when it reach zero, adjust currectly slot length by 2 tick(60us). 
+  uint16_t              compensateTicks;    // record how many ticks  are compensated 
+  asn_t                 oldASN;             // the asn when synchronized previous time
+  compensationInfo_t    compensationInfo_vars[9]; //keep each time soures' compensation information
+} adaptive_sync_t;
+
+//=========================== variables =======================================
+
+adaptive_sync_t adaptive_sync_vars;
+
+//=========================== prototypes ======================================
+
+//=========================== prototypes ======================================
+
+/**
+\brief initial this module
+*/
+void adaptive_sync_init(){
+  memset(&adaptive_sync_vars,0,sizeof(adaptive_sync_t));
+}
+
+/**
+\brief calculated how many slots have lapsed since last synchronized.
+
+\param[in] address The address of neighbor.
+
+\returns the number of slots
+*/
+void adaptive_sync_recordLastASN(int16_t timeCorrection, uint8_t syncMethod){
+    uint8_t array[5];
+    ieee154e_getAsn(array);
+    // check whether I am synchronized?
+    if(ieee154e_isSynch())
+    {
+      // if I am synchronized, only calculated compensation interval when timeCorrection excess LIMITLARGETIMECORRECTION
+      if(timeCorrection > LIMITLARGETIMECORRECTION || timeCorrection < -LIMITLARGETIMECORRECTION)
+        adaptive_sync_calculateCompensatedSlots(timeCorrection, syncMethod);
+    }
+    // update oldASN variable by currect asn
+    memcpy(&(adaptive_sync_vars.oldASN.bytes0and1), &array[0], sizeof(uint16_t));
+    memcpy(&(adaptive_sync_vars.oldASN.bytes2and3), &array[2], sizeof(uint16_t));
+    memcpy(&(adaptive_sync_vars.oldASN.byte4), &array[4], sizeof(uint8_t));
+}
+
+/**
+\brief calcluated the compensation interval, counted by number of slots.
+
+\para[in] address The address of neighbor.
+
+\returns compensationSlots the number of slots. 
+*/
+void adaptive_sync_calculateCompensatedSlots(int16_t timeCorrection, uint8_t syncMethod){
+    uint16_t TC;
+    adaptive_sync_vars.lapsedSlots = ieee154e_asnDiff(&adaptive_sync_vars.oldASN);
+    // determine the clock's state: faster or slower?
+    switch(syncMethod)
+    {
+    case S_PACKET_SYNC:
+      if(timeCorrection > 0)
+      {
+        if(adaptive_sync_vars.clockState == S_NONE)
+          adaptive_sync_vars.clockState = S_FASTER;
+      }
+      else
+      {
+        TC = -timeCorrection;
+        if(adaptive_sync_vars.clockState == S_NONE)
+          adaptive_sync_vars.clockState = S_SLOWER;
+      }
+      break;
+    case S_ACK_SYNC:
+      if(timeCorrection > 0)
+      {
+        if(adaptive_sync_vars.clockState == S_NONE)
+          adaptive_sync_vars.clockState = S_SLOWER;
+      }
+      else
+      {
+        TC = -timeCorrection;
+        if(adaptive_sync_vars.clockState == S_NONE)
+          adaptive_sync_vars.clockState = S_FASTER;
+      }
+      break;
+    default:
+      while(1); // should not reach here
+    }// calculate the compensation interval, uint: slots/x ticks. 2ticks is only the case of openmotestm platform, usually it should be one
+    adaptive_sync_vars.compensationInfo_vars[0].compensationSlots = COMPENSATE_ACCURACY * adaptive_sync_vars.lapsedSlots / (TC + adaptive_sync_vars.compensateTicks);
+    adaptive_sync_vars.compensateTicks = 0;
+    adaptive_sync_vars.compensationTimeout = adaptive_sync_vars.compensationInfo_vars[0].compensationSlots;
+}
+/**
+\brief minus compensationTimeout at each slot
+*/
+void adaptive_sync_countCompensationTimeout() {
+  // if clockState is not set yet, don't compensate.
+  if(adaptive_sync_vars.clockState == S_NONE) return;
+
+    adaptive_sync_vars.compensationTimeout--;
+    // when compensationTimeout, adjust current slot length
+    if(adaptive_sync_vars.compensationTimeout == 0)
+    {
+      switch(adaptive_sync_vars.clockState)
+      {
+      case S_SLOWER:
+        radio_setTimerPeriod(TsSlotDuration - COMPENSATE_ACCURACY);
+        adaptive_sync_vars.compensateTicks += COMPENSATE_ACCURACY;
+        break;
+      case S_FASTER:
+        radio_setTimerPeriod(TsSlotDuration + COMPENSATE_ACCURACY);
+        adaptive_sync_vars.compensateTicks += COMPENSATE_ACCURACY;
+        break;
+      case S_NONE:
+        while(1);
+      default:
+        while(1);
+      }
+      // tf: for debugging
+//      GPIOC->ODR ^= 0X1000;
+      // reload compensationTimeout
+      adaptive_sync_vars.compensationTimeout = adaptive_sync_vars.compensationInfo_vars[0].compensationSlots;
+    }
+}
---
+++ firmware/openos/openwsn/02a-MAClow/adaptivesync/adaptive_sync.h
@@ -0,0 +1,48 @@
+#ifndef __ADAPTIVE_SYNC_H
+#define __ADAPTIVE_SYNC_H
+
+#include "board_info.h"
+
+/**
+\addtogroup MAClow
+\{
+\addtogroup adpativesync
+\{
+
+\brief adaptive sync module
+
+\author Tengfei Chang <tengfei.chang@eecs.berkeley.edu>, Janaury 2014.
+*/
+
+//=========================== define ==========================================
+
+typedef enum {
+  S_NONE   = 0x00,
+  S_FASTER = 0x01,
+  S_SLOWER = 0x02,
+}adaptive_sync_state_t;
+
+typedef enum {
+  S_PACKET_SYNC = 0x00,
+  S_ACK_SYNC    = 0x01,
+}adaptive_sync_methods_t;
+
+//=========================== typedef =========================================
+
+//=========================== variables =======================================
+
+//=========================== prototypes ======================================
+
+//=========================== prototypes ======================================
+
+void adaptive_sync_init();
+void adaptive_sync_recordLastASN(int16_t timeCorrection, uint8_t syncMethod);
+void adaptive_sync_calculateCompensatedSlots(int16_t timeCorrection, uint8_t syncMethod);
+
+void adaptive_sync_countCompensationTimeout();
+
+/**
+\}
+\}
+*/
+#endif
