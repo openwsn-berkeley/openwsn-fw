77a55b6a4698e397e28bb793aa57d3da50e7d396
ssciancalepore <savio.sciancalepore@poliba.it>
2014-07-02 10:35:20 +0200

    Update board_info.h

--- firmware/openos/bsp/boards/wsn430v14/board_info.h
+++ firmware/openos/bsp/boards/wsn430v14/board_info.h
@@ -27,16 +27,16 @@ to this board.
 
 #if defined(__GNUC__) && (__GNUC__==4)  && (__GNUC_MINOR__<=5) && defined(__MSP430__)
    // mspgcc <4.5.x
-#define INTERRUPT_DECLARATION()   unsigned short s;
-#define DISABLE_INTERRUPTS()      s = READ_SR&0x0008; \
-                                  __disable_interrupt();
-#define ENABLE_INTERRUPTS()       __asm__("bis %0,r2" : : "ir" ((uint16_t) s));
+   #define INTERRUPT_DECLARATION()          unsigned short s;
+   #define DISABLE_INTERRUPTS()             s = READ_SR&0x0008; \
+                                            __disable_interrupt();
+   #define ENABLE_INTERRUPTS()              __asm__("bis %0,r2" : : "ir" ((uint16_t) s));
 #else
    // other
-#define INTERRUPT_DECLARATION()   __istate_t s;
-#define DISABLE_INTERRUPTS()      s = __get_interrupt_state(); \
-                                   __disable_interrupt();
-#define ENABLE_INTERRUPTS()       __set_interrupt_state(s);
+   #define INTERRUPT_DECLARATION()          __istate_t s;
+   #define DISABLE_INTERRUPTS()             s = __get_interrupt_state(); \
+                                            __disable_interrupt();
+   #define ENABLE_INTERRUPTS()              __set_interrupt_state(s);
 #endif
 
 //===== timer
