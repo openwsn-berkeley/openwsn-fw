afd542e2a317443dbab3579ed924b246e9195235
Thomas Watteyne <twatteyne@gmail.com>
2014-07-01 22:41:56 -0700

    FW-241. indentation.

--- firmware/openos/openwsn/07-App/rex/rex.c
+++ firmware/openos/openwsn/07-App/rex/rex.c
@@ -77,79 +77,81 @@ void rex_timer_cb(){
 }
 
 void rex_task_cb() {
-   OpenQueueEntry_t* pkt;
-   owerror_t           outcome;
-   uint8_t           numOptions;
-   uint8_t           i;
+   OpenQueueEntry_t*    pkt;
+   owerror_t            outcome;
+   uint8_t              numOptions;
+   uint8_t              i;
    
-   uint16_t       x_int       = 0;
-   //uint16_t*      p_x_int     = &x_int;
-   uint16_t       sum         = 0;
-   uint16_t       avg         = 0;
-   uint8_t        N_avg       = 10;
+   uint16_t             x_int       = 0;
+   uint16_t             sum         = 0;
+   uint16_t             avg         = 0;
+   uint8_t              N_avg       = 10;
    
    // don't run if not synch
    if (ieee154e_isSynch() == FALSE) return;
    
-       // don't run on dagroot
+   // don't run on dagroot
    if (idmanager_getIsDAGroot()) {
-       opentimers_stop(rex_vars.timerId);
-       return;
+      opentimers_stop(rex_vars.timerId);
+      return;
    }
    
-   
    for (i = 0; i < N_avg; i++) {
-      //ADC_getvoltage(p_x_int);
       sum += x_int;
    }
    avg = sum/N_avg;
    
-   
    // create a CoAP RD packet
    pkt = openqueue_getFreePacketBuffer(COMPONENT_REX);
    if (pkt==NULL) {
-      openserial_printError(COMPONENT_REX,ERR_NO_FREE_PACKET_BUFFER,
-                            (errorparameter_t)0,
-                            (errorparameter_t)0);
+      openserial_printError(
+         COMPONENT_REX,
+         ERR_NO_FREE_PACKET_BUFFER,
+         (errorparameter_t)0,
+         (errorparameter_t)0
+      );
       openqueue_freePacketBuffer(pkt);
       return;
    }
    // take ownership over that packet
-   pkt->creator    = COMPONENT_REX;
-   pkt->owner      = COMPONENT_REX;
+   pkt->creator                   = COMPONENT_REX;
+   pkt->owner                     = COMPONENT_REX;
    // CoAP payload
    packetfunctions_reserveHeaderSize(pkt,PAYLOADLEN);
    for (i=0;i<PAYLOADLEN;i++) {
-      pkt->payload[i] = i;
+      pkt->payload[i]             = i;
    }
    avg = openrandom_get16b();
-   pkt->payload[0] = (avg>>8)&0xff;
-   pkt->payload[1] = (avg>>0)&0xff;
+   pkt->payload[0]                = (avg>>8)&0xff;
+   pkt->payload[1]                = (avg>>0)&0xff;
    
    numOptions = 0;
    // location-path option
    packetfunctions_reserveHeaderSize(pkt,sizeof(rex_path0)-1);
    memcpy(&pkt->payload[0],&rex_path0,sizeof(rex_path0)-1);
    packetfunctions_reserveHeaderSize(pkt,1);
-   pkt->payload[0]                  = (COAP_OPTION_NUM_URIPATH) << 4 |
-      sizeof(rex_path0)-1;
+   pkt->payload[0]                = (COAP_OPTION_NUM_URIPATH) << 4 | sizeof(rex_path0)-1;
    numOptions++;
    // content-type option
    packetfunctions_reserveHeaderSize(pkt,2);
-   pkt->payload[0]                  = COAP_OPTION_NUM_CONTENTFORMAT << 4 |
-      1;
-   pkt->payload[1]                  = COAP_MEDTYPE_APPOCTETSTREAM;
+   pkt->payload[0]                = COAP_OPTION_NUM_CONTENTFORMAT << 4 | 1;
+   pkt->payload[1]                = COAP_MEDTYPE_APPOCTETSTREAM;
    numOptions++;
+   
    // metadata
-   pkt->l4_destination_port         = WKP_UDP_COAP;
-   pkt->l3_destinationAdd.type = ADDR_128B;
+   pkt->l4_destination_port       = WKP_UDP_COAP;
+   pkt->l3_destinationAdd.type    = ADDR_128B;
    memcpy(&pkt->l3_destinationAdd.addr_128b[0],&ipAddr_motesEecs,16);
+   
    // send
-   outcome = opencoap_send(pkt,
-                           COAP_TYPE_NON,
-                           COAP_CODE_REQ_PUT,
-                           numOptions,
-                           &rex_vars.desc);
+   outcome = opencoap_send(
+      pkt,
+      COAP_TYPE_NON,
+      COAP_CODE_REQ_PUT,
+      numOptions,
+      &rex_vars.desc
+   );
+   
    // avoid overflowing the queue if fails
    if (outcome==E_FAIL) {
       openqueue_freePacketBuffer(pkt);
