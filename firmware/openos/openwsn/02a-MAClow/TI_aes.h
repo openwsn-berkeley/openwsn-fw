517e1a99fdc6626daa91d7564f0042534e54da89
ssciancalepore <savio.sciancalepore@poliba.it>
2014-07-02 09:06:05 +0200

    Create TI_aes.h

---
+++ firmware/openos/openwsn/02a-MAClow/TI_aes.h
@@ -0,0 +1,13 @@
+/*
+ * AES module by Texas Instruments
+ */
+
+#ifndef TI_AES
+#define TI_AES
+
+uint8_t Key[64];
+
+void aes_encrypt(unsigned char *input,unsigned char *state, unsigned char *key);
+//void aes_decrypt(unsigned char *state, unsigned char *key);
+
+#endif
