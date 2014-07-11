69c34666788724e3f05d8d2999c6bed1ecebdea0
Thomas Watteyne <twatteyne@gmail.com>
2014-07-11 01:02:16 -0700

    FW-251. udpstorm.c/h changes for simulator.

--- firmware/openos/openwsn/07-App/udpstorm/udpstorm.h
+++ firmware/openos/openwsn/07-App/udpstorm/udpstorm.h
@@ -14,6 +14,12 @@
 
 //=========================== variables =======================================
 
+typedef struct {
+   coap_resource_desc_t desc;
+   opentimer_id_t       timerId;
+   uint16_t             udp_storm_period;   ///< inter-packet period (in ms)
+} udpstorm_vars_t;
+
 //=========================== prototypes ======================================
 
 void udpstorm_init(void);
