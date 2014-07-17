96e590c8041943595407e48af8ebeca4c7b0010f
Thomas Watteyne <twatteyne@gmail.com>
2014-07-16 22:41:49 -0700

    FW-261. Forcing topology.

--- firmware/openos/openwsn/02a-MAClow/topology.c
+++ firmware/openos/openwsn/02a-MAClow/topology.c
@@ -17,7 +17,7 @@ acceptable neighbors for a given mote. This function is invoked each time a
 packet is received. If it returns FALSE, the packet is silently dropped, as if
 it were never received. If it returns TRUE, the packet is accepted.
 
-Typically, filtering packets is done by analysing the IEEE802.15.4 header. An
+Typically, filtering packets is done by analyzing the IEEE802.15.4 header. An
 example body for this function which forces a topology is:
 
    switch (idmanager_getMyID(ADDR_64B)->addr_64b[7]) {
@@ -50,7 +50,102 @@ topology.
 \return FALSE if the packet should be silently dropped.
 */
 bool topology_isAcceptablePacket(ieee802154_header_iht* ieee802514_header) {
+#ifdef PLUGFEST
+   bool returnVal;
+   
+   returnVal=FALSE;
+   switch (idmanager_getMyID(ADDR_64B)->addr_64b[7]) {
+      case 0x4c:
+         if (
+               ieee802514_header->src.addr_64b[7]==0x00 ||
+               ieee802514_header->src.addr_64b[7]==0x60
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+      case 0x60:
+         if (
+               ieee802514_header->src.addr_64b[7]==0x4c ||
+               ieee802514_header->src.addr_64b[7]==0x97 ||
+               ieee802514_header->src.addr_64b[7]==0xc8
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+      case 0xc8:
+         if (
+               ieee802514_header->src.addr_64b[7]==0x60 ||
+               ieee802514_header->src.addr_64b[7]==0x6f ||
+               ieee802514_header->src.addr_64b[7]==0x50
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+      case 0x50:
+         if (
+               ieee802514_header->src.addr_64b[7]==0xc8
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+      case 0x6f:
+         if (
+               ieee802514_header->src.addr_64b[7]==0x85 ||
+               ieee802514_header->src.addr_64b[7]==0x97 ||
+               ieee802514_header->src.addr_64b[7]==0xc8
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+      case 0x85:
+         if (
+               ieee802514_header->src.addr_64b[7]==0x5c ||
+               ieee802514_header->src.addr_64b[7]==0x97 ||
+               ieee802514_header->src.addr_64b[7]==0x6f
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+      case 0xa8:
+         if (
+               ieee802514_header->src.addr_64b[7]==0x5c ||
+               ieee802514_header->src.addr_64b[7]==0x97 ||
+               ieee802514_header->src.addr_64b[7]==0x00
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+      case 0x00:
+         if (
+               ieee802514_header->src.addr_64b[7]==0x4c ||
+               ieee802514_header->src.addr_64b[7]==0xa8
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+      case 0x97:
+         if (
+               ieee802514_header->src.addr_64b[7]==0xa8 ||
+               ieee802514_header->src.addr_64b[7]==0x85 ||
+               ieee802514_header->src.addr_64b[7]==0x6f ||
+               ieee802514_header->src.addr_64b[7]==0x60
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+      case 0x5c:
+         if (
+               ieee802514_header->src.addr_64b[7]==0x85 ||
+               ieee802514_header->src.addr_64b[7]==0xa8
+            ) {
+            returnVal=TRUE;
+         }
+         break;
+   }
+   return returnVal;
+#else
    return TRUE;
+#endif
 }
 
 //=========================== private =========================================
