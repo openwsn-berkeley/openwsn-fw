709f2a88f6d40485c5ec19caf32d273ed9eb4ccf
root <savio.sciancalepore@poliba.it>
2014-07-18 09:25:11 +0200

    modifying security.c to find error on objective_c compiling

--- firmware/openos/openwsn/02a-MAClow/security.c
+++ firmware/openos/openwsn/02a-MAClow/security.c
@@ -628,14 +628,14 @@ uint8_t deviceDescriptorLookup(open_addr_t* Address,
 							   m_keyDescriptor* keydescr){
 
 	uint8_t i;
-//
-//	for(i=0; i<MAXNUMNEIGHBORS; i++){
+
+	for(i=0; i<MAXNUMNEIGHBORS; i++){
 //
 //		if((packetfunctions_sameAddress(Address,keydescr->DeviceTable->DeviceDescriptorEntry[i].deviceAddress)== TRUE)
 //			&& (packetfunctions_sameAddress(PANId, MacKeyTable.KeyDescriptorElement[i].KeyIdLookupList.PANId))){
 //			return i;
 //		}
-//	}
+	}
 //
 	return 25;
 }
