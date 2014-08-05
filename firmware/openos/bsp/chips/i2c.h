b6cd6f9145ab1aca079363c19c3ed9e73596598a
Thomas Watteyne <twatteyne@gmail.com>
2014-08-05 00:13:48 -0700

    FW-268. Cleaning up I2C.

--- firmware/openos/bsp/chips/i2c.h
+++ firmware/openos/bsp/chips/i2c.h
@@ -30,11 +30,11 @@ void i2c_read_registers(uint8_t bus_num,uint8_t slave_addr,
                              uint8_t* spaceToWrite);
 void i2c_write_register(uint8_t bus_num, uint8_t slave_addr, uint8_t length, uint8_t* data);
 
-unsigned char i2c_slave_present(int bus_num,unsigned char slave_address);
+uint8_t i2c_slave_present(uint8_t bus_num,uint8_t slave_address);
 
 // interrupt handlers
-void isr_i2c_tx(int bus_num);
-void isr_i2c_rx(int bus_num);
+void isr_i2c_tx(uint8_t bus_num);
+void isr_i2c_rx(uint8_t bus_num);
 
 /**
 \}
