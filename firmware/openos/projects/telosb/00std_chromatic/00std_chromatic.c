67b920005d5b4a8e433bcb236bd1a806c135500a
Thomas Watteyne <twatteyne@gmail.com>
2014-08-10 15:21:13 -0700

    FW-270. Skeleton for 00std_chromatic sample application.

---
+++ firmware/openos/projects/telosb/00std_chromatic/00std_chromatic.c
@@ -0,0 +1,20 @@
+/**
+\brief <TODO, 1-2 lines>
+
+<TODO, what to buy>
+
+<TODO, how to connect the TelosB to the buzzer>
+
+<TODO, how to build the binary>
+
+<TODO, what to expect when it runs>
+ 
+\author <TODO, name> <TODO, e-mail>, <TODO date>
+*/
+
+#include "msp430f1611.h"
+#include "stdint.h"
+
+int main(void) {
+   // TODO
+}
