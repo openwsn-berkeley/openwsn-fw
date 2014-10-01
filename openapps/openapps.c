ff0773a76e36d2219c7dac83c37ab0d5b5c09c91
Thomas Watteyne <twatteyne@gmail.com>
2014-09-30 23:20:37 -0700

    FW-277. Default applications.

--- openapps/openapps.c
+++ openapps/openapps.c
@@ -1,45 +1,28 @@
 /**
-\brief General OpenWSN definitions
+\brief Applications running on top of the OpenWSN stack.
 
-\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, September 2012
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, September 2014.
 */
 
 #include "openwsn.h"
-//+++++ TCP
-//- debug
+
+//=== default applications
+// TCP
 #include "tcpecho.h"
 #include "tcpinject.h"
 #include "tcpprint.h"
-//- common
-#include "ohlone.h"
-//- board-specific
-//++++ UDP
-//- debug
+// UDP
 #include "udpecho.h"
 #include "udpinject.h"
 #include "udpprint.h"
-//- common
-//#include "udprand.h"
-//#include "udplatency.h"
-#include "udpstorm.h"
-//- board-specific
-//#include "imu.h"
-//+++++ CoAP
-//- debug
-//- common
+// CoAP
 #include "rinfo.h"
 #include "rleds.h"
 #include "rwellknown.h"
 #include "r6t.h"
-//#include "rrt.h"
-//#include "rex.h"
-//#include "rrube.h"
-//- board-specific
-//#include "rheli.h"
-//#include "rt.h"
-//#include "rxl1.h"
-//#include "heli.h"
-//#include "imu.h"
+
+//=== user applications
+// MARKER_USER_APPS_INCLUDES_HERE (don't remove this line)
 
 //=========================== variables =======================================
 
@@ -51,41 +34,23 @@
 
 void openapps_init(void) {
    
-   //===== applications
-   //+++++ TCP
-   //- debug
+   //=== default applications
+   // TCP
    tcpecho_init();
    tcpinject_init();
    tcpprint_init();
-   //- common
-   ohlone_init();
-   //- board-specific
-   //+++++ UDP
-   //- debug
+   // UDP
    udpecho_init();
    udpinject_init();
    udpprint_init();
-   //- common
-   //udprand_init();
-   //udplatency_init();
-   udpstorm_init();
-   //- board-specific
-   //imu_init();
-   //+++++ CoAP
-   //- debug
-   //- core
-   //- common
+   // CoAP
+   r6t_init();
    rinfo_init();
-   //rrt_init();
    rleds__init();
    rwellknown_init();
-   r6t_init();
-   //rreg_init();
-   //rex_init();
-   //rrube_init();
-   //layerdebug_init();
-   //- board-specific
-   //rheli_init();
-   //rt_init();
-   //rxl1_init();
+   
+   //=== user applications
+   // These applications are added on-the-fly by the SCons build system by 
+   // using the 'apps=udprand,ohlone' switch
+   // MARKER_USER_APPS_INITS_HERE (don't remove this line)
 }
