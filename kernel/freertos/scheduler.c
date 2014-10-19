c8294e57ea5963d2ee4fab7f54c7c9750bb1e4d3
Xavier Vilajosana <xvilajosana@eecs.berkeley.edu>
2014-10-19 15:54:39 +0200

    FW-16 - scheduler implementation.

--- kernel/freertos/scheduler.c
+++ kernel/freertos/scheduler.c
@@ -19,8 +19,12 @@
 #include "portable.h"
 #include "queue.h"
 
-#define STACK_SIZE 256
+#define STACK_SIZE 50
 #define tskIDLE_PRIORITY 2
+
+#define SCHEDULER_APP_PRIO_BOUNDARY TASKPRIO_MAX //TODO REVISE THIS PRIOS
+#define SCHEDULER_STACK_PRIO_BOUNDARY 4
+#define SCHEDULER_SENDDONETIMER_PRIO_BOUNDARY 8
 
 #define INTERRUPT_DECLARATION()  (xGlobalMutex != NULL ?: ( xGlobalMutex = xSemaphoreCreateMutex()))
 #define DISABLE_INTERRUPTS() xSemaphoreTake(xGlobalMutex, portMAX_DELAY )
@@ -44,7 +48,6 @@ SemaphoreHandle_t xSchedulerAppStackSemaphore;
 SemaphoreHandle_t xSchedulerStackRxSemaphore;
 SemaphoreHandle_t xSchedulerSendDoneAndTimerSemaphore;
 
-
 SemaphoreHandle_t xSchedulerMutex; //for mutual exclusion
 
 //=========================== prototypes ======================================
@@ -53,6 +56,10 @@ void vSendDoneAndTimerTask(void * pvParameters);
 void vStackRxTask(void * pvParameters);
 
 void initializeTaskSemaphores(SemaphoreHandle_t * sem);
+void internal_scheduler_push_task(task_cbt cb, task_prio_t prio);
+bool internal_find_next_task(task_prio_t minprio, task_prio_t maxprio,
+		taskList_item_t * pThisTask);
+void executeTask(taskList_item_t* pThisTask);
 //=========================== public ==========================================
 
 void scheduler_init() {
@@ -60,7 +67,7 @@ void scheduler_init() {
 	xGlobalMutex = xSemaphoreCreateMutex();
 	if (xGlobalMutex == NULL) {
 		//fail
-	    return;
+		return;
 	}
 	//give it
 	if (xSemaphoreGive(xGlobalMutex) != pdTRUE) {
@@ -74,19 +81,13 @@ void scheduler_init() {
 
 	//create the tasks
 	xTaskCreate(vStackRxTask, "StackRX", STACK_SIZE, NULL, tskIDLE_PRIORITY,
-			&xStackRxHandle);
-	configASSERT(xStackRxHandle);
-
+			&xStackRxHandle);configASSERT(xStackRxHandle);
 
 	xTaskCreate(vStackRxTask, "StackApp", STACK_SIZE, NULL, tskIDLE_PRIORITY,
-			&xAppStackHandle);
-	configASSERT(xAppStackHandle);
-
+			&xAppStackHandle);configASSERT(xAppStackHandle);
 
 	xTaskCreate(vSendDoneAndTimerTask, "StackTimer", STACK_SIZE, NULL,
-			tskIDLE_PRIORITY, &xSendDoneAndTimerHandle);
-	configASSERT(xSendDoneAndTimerHandle);
-
+			tskIDLE_PRIORITY, &xSendDoneAndTimerHandle);configASSERT(xSendDoneAndTimerHandle);
 
 }
 
@@ -95,64 +96,207 @@ void scheduler_start() {
 	vTaskStartScheduler();
 
 	/* If all is well we will never reach here as the scheduler will now be
-		running.  If we do reach here then it is likely that there was insufficient
-		heap available for the idle task to be created. */
-	for( ;; );
+	 running.  If we do reach here then it is likely that there was insufficient
+	 heap available for the idle task to be created. */
+	for (;;)
+		;
 }
 
 void scheduler_push_task(task_cbt cb, task_prio_t prio) {
-	// TODO: fill in as part of FW-16.
+	//1-insert the task into the task list
+	internal_scheduler_push_task(cb, prio);
+	//2-toggle the appropiate sempahore so the corresponding handler takes care of it.
+	if (prio <= SCHEDULER_STACK_PRIO_BOUNDARY) {
+		if (xSemaphoreGive(xSchedulerStackRxSemaphore) != pdTRUE) {
+			//TODO handle failure
+			return;
+		}
+
+	} else if (prio > SCHEDULER_STACK_PRIO_BOUNDARY
+			&& prio <= SCHEDULER_SENDDONETIMER_PRIO_BOUNDARY) {
+		if (xSemaphoreGive(xSchedulerSendDoneAndTimerSemaphore) != pdTRUE) {
+			//TODO handle failure
+			return;
+		}
+	} else if (prio > SCHEDULER_SENDDONETIMER_PRIO_BOUNDARY
+			&& prio <= SCHEDULER_APP_PRIO_BOUNDARY) {
+		if (xSemaphoreGive(xSchedulerAppStackSemaphore) != pdTRUE) {
+			//TODO handle failure
+			return;
+		}
+	} else {
+		//THIS SHOULD NEVER HAPPEN
+		while (1)
+			;
+	}
+	/*We will need 3 different queues to split tasks into the 3 different tasks.*/
 }
 
 //=========================== private =========================================
 
 /* Task to handle reception of packets up to the stack. */
 void vStackRxTask(void * pvParameters) {
-	uint16_t i=0;
+	bool found = false;
+	taskList_item_t * pThisTask = NULL;
 	while (1) {
+		xSemaphoreTake(xSchedulerStackRxSemaphore, portMAX_DELAY);
+		found = internal_find_next_task(0,
+				SCHEDULER_STACK_PRIO_BOUNDARY, pThisTask);
+		if (found) {
+			// execute the current task
+			executeTask(pThisTask);
+		}
 
 		leds_debug_toggle();
-		for (i = 0; i < 0xffff; i++)
-			;	      //delay
-
 	}
 }
 
 /* Task to handle app packets downstream the stack up to be queued at MAC. */
 void vAppStackTask(void * pvParameters) {
-	uint16_t i=0;
+	bool found = false;
+	taskList_item_t * pThisTask = NULL;
 	while (1) {
-
+		xSemaphoreTake(xSchedulerAppStackSemaphore, portMAX_DELAY);
+		found = internal_find_next_task(SCHEDULER_SENDDONETIMER_PRIO_BOUNDARY,
+				SCHEDULER_APP_PRIO_BOUNDARY, pThisTask);
+		if (found) {
+			// execute the current task
+			executeTask(pThisTask);
+		}
 		leds_sync_toggle();
-		for ( i = 0; i < 0x3fff; i++)
-			;	      //delay
 
 	}
 }
 
 /* Task to handle senddone notification and app level pushed tasks (from timers). */
 void vSendDoneAndTimerTask(void * pvParameters) {
-	uint16_t i=0;
+	bool found;
+	taskList_item_t * pThisTask = NULL;
 	while (1) {
-
+		xSemaphoreTake(xSchedulerSendDoneAndTimerSemaphore, portMAX_DELAY);
+		found = internal_find_next_task(SCHEDULER_STACK_PRIO_BOUNDARY,
+				SCHEDULER_SENDDONETIMER_PRIO_BOUNDARY, pThisTask);
+		if (found) {
+			// execute the current task
+			executeTask(pThisTask);
+		}
 		leds_radio_toggle();
-		for ( i = 0; i < 0x0fff; i++)
-			;	      //delay
-
 	}
 }
 
 /* helper functions */
-void initializeTaskSemaphores(SemaphoreHandle_t * sem ){
+void initializeTaskSemaphores(SemaphoreHandle_t * sem) {
 
 	*sem = xSemaphoreCreateBinary();
 	if (*sem == NULL) {
 		//fail
-	    return;
+		return;
 	}
 	//give it
 	if (xSemaphoreGive(*sem) != pdTRUE) {
 		//TODO handle failure
 		return;
 	}
+}
+
+/* insert task into the list according to prio */
+void internal_scheduler_push_task(task_cbt cb, task_prio_t prio) {
+	taskList_item_t* taskContainer;
+	taskList_item_t** taskListWalker;
+	INTERRUPT_DECLARATION();
+
+	DISABLE_INTERRUPTS();
+
+	// find an empty task container
+	taskContainer = &scheduler_vars.taskBuf[0];
+	while (taskContainer->cb != NULL
+			&& taskContainer <= &scheduler_vars.taskBuf[TASK_LIST_DEPTH - 1]) {
+		taskContainer++;
+	}
+	if (taskContainer > &scheduler_vars.taskBuf[TASK_LIST_DEPTH - 1]) {
+		// task list has overflown. This should never happpen!
+
+		// we can not print from within the kernel. Instead:
+		// blink the error LED
+		leds_error_blink();
+		// reset the board
+		board_reset();
+	}
+	// fill that task container with this task
+	taskContainer->cb = cb;
+	taskContainer->prio = prio;
+
+	// find position in queue
+	taskListWalker = &scheduler_vars.task_list;
+	while (*taskListWalker != NULL
+			&& (*taskListWalker)->prio < taskContainer->prio) {
+		taskListWalker = (taskList_item_t**) &((*taskListWalker)->next);
+	}
+	// insert at that position
+	taskContainer->next = *taskListWalker;
+	*taskListWalker = taskContainer;
+	// maintain debug stats
+	scheduler_dbg.numTasksCur++;
+	if (scheduler_dbg.numTasksCur > scheduler_dbg.numTasksMax) {
+		scheduler_dbg.numTasksMax = scheduler_dbg.numTasksCur;
+	}
+
+	ENABLE_INTERRUPTS();
+}
+
+/*
+ * finds the next task to execute. out param the taskList item to return.
+ *
+ * */
+bool internal_find_next_task(task_prio_t minprio, task_prio_t maxprio,
+		taskList_item_t * pThisTask) {
+	//to shift
+	taskList_item_t ** prevTask;
+	if (scheduler_vars.task_list != NULL) {
+		// start searching by the task at the head of the queue
+		prevTask = &scheduler_vars.task_list;
+
+		//check first element is not the one we want.
+		if ((*prevTask)->prio >= minprio && (*prevTask)->prio < maxprio) {
+			pThisTask = (*prevTask);
+			scheduler_vars.task_list = pThisTask->next;
+			return true;
+		}
+
+		if ((*prevTask)->next != NULL) {
+			pThisTask = (*prevTask)->next;
+		} else {
+			return false;
+		}
+
+		while (!(pThisTask->prio >= minprio && pThisTask->prio < maxprio)
+				&& pThisTask != NULL) {
+			//advance both prev and thistask
+			prevTask = (taskList_item_t**) &((*prevTask)->next);
+			pThisTask = (*prevTask)->next;
+		}
+
+		if (pThisTask == NULL) {
+			//not found
+			return false;
+		} else {
+			//found
+			//link the list again and remove the selected task
+			(*prevTask)->next = pThisTask->next;
+			return true;
+		}
+	}
+	return false;
+
+}
+
+/* Executes a task */
+void executeTask(taskList_item_t* pThisTask) {
+	// execute the current task
+	pThisTask->cb();
+	// free up this task container
+	pThisTask->cb = NULL;
+	pThisTask->prio = TASKPRIO_NONE;
+	pThisTask->next = NULL;
+	scheduler_dbg.numTasksCur--;
 }
