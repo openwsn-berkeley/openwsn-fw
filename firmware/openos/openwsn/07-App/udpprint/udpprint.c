4513416bc1f9549e4ab9f8e018a04c193218ce48
Alex Budilovsky <alexbudy@gmail.com>
2014-10-21 20:34:44 -0700

    udpprint.c reverted, changes not needed

--- firmware/openos/openwsn/07-App/udpprint/udpprint.c
+++ firmware/openos/openwsn/07-App/udpprint/udpprint.c
@@ -13,33 +13,18 @@ void udpprint_init() {
 }
 
 void udpprint_receive(OpenQueueEntry_t* msg) {
-//   openserial_printData((uint8_t*)(msg->payload),msg->length);
-    owerror_t error;
-   error = openqueue_freePacketBuffer(msg);
-   if (error == E_FAIL) {
-       openserial_printError(
-          COMPONENT_UDPPRINT,
-          ERR_UNEXPECTED_SENDDONE,
-          (errorparameter_t)0,
-          (errorparameter_t)0
-       );
-   }
+   openserial_printData((uint8_t*)(msg->payload),msg->length);
+   openqueue_freePacketBuffer(msg);
 }
 
 void udpprint_sendDone(OpenQueueEntry_t* msg, owerror_t error) {
-    owerror_t error2;
-   error2 = openqueue_freePacketBuffer(msg);
-
-   if (error2 == E_FAIL) {
    openserial_printError(
       COMPONENT_UDPPRINT,
       ERR_UNEXPECTED_SENDDONE,
       (errorparameter_t)0,
       (errorparameter_t)0
    );
-
-   }
-
+   openqueue_freePacketBuffer(msg);
 }
 
 bool udpprint_debugPrint() {
