36b012b3036fe30e65d5ad374f46c57efba52b28
Thomas Watteyne <twatteyne@gmail.com>
2014-10-27 01:26:03 -0700

    FW-297. FW-299. 01bsp_eui64 common application.

--- projects/common/01bsp_eui64/01bsp_eui64.c
+++ projects/common/01bsp_eui64/01bsp_eui64.c
@@ -1,5 +1,6 @@
 /**
-\brief This is a program which shows how to use the bsp modules for the board
+\brief This is a program which shows how to use the bsp modules to read the
+   EUI64
        and leds.
 
 \note: Since the bsp modules for different platforms have the same declaration,
@@ -14,64 +15,21 @@ Load this program on your boards. The LEDs should start blinking furiously.
 #include "stdio.h"
 // bsp modules required
 #include "board.h"
-#include "leds.h"
-
-void some_delay(void);
+#include "eui64.h"
 
 /**
 \brief The program starts executing here.
 */
 int mote_main(void) {uint8_t i;
+   uint8_t eui[8];
    
    board_init();
    
-   // error LED functions
-   leds_error_on();          some_delay();
-   leds_error_off();         some_delay();
-   leds_error_toggle();      some_delay();
-   leds_error_blink();       some_delay();
+   memset(eui,0,8);
    
-   // radio LED functions
-   leds_radio_on();          some_delay();
-   leds_radio_off();         some_delay();
-   leds_radio_toggle();      some_delay();
+   eui64_get(eui);
    
-   // sync LED functions
-   leds_sync_on();           some_delay();
-   leds_sync_off();          some_delay();
-   leds_sync_toggle();       some_delay();
-   
-   // debug LED functions
-   leds_debug_on();          some_delay();
-   leds_debug_off();         some_delay();
-   leds_debug_toggle();      some_delay();
-   
-   // all LED functions
-   leds_all_off();           some_delay();
-   leds_all_on();            some_delay();
-   leds_all_off();           some_delay();
-   leds_all_toggle();        some_delay();
-   
-   // LED increment function
-   leds_all_off();           some_delay();
-   for (i=0;i<9;i++) {
-      leds_increment();      some_delay();
-   }
-   
-   // LED circular shift function
-   leds_all_off();           some_delay();
-   leds_error_on();          some_delay();
-   for (i=0;i<9;i++) {
-      leds_circular_shift(); some_delay();
-   }
-   
-   // reset the board, so the program starts running again
-   board_reset();
+   board_sleep();
    
    return 0;
-}
-
-void some_delay(void) {
-   volatile uint16_t delay;
-   for (delay=0xffff;delay>0;delay--);
 }
