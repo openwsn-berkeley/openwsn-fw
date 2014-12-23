8a0b122eadda7f18183a434a62c5f0a35c87daab
changtengfei <tengfei.chang@gmail.com>
2014-12-23 14:37:34 +0800

    update led and uart driver for new openmotestm board.

--- bsp/boards/openmotestm/leds.c
+++ bsp/boards/openmotestm/leds.c
@@ -18,40 +18,33 @@ void Delay(void);
 
 void leds_init()
 {
-    // Enable GPIOC, GPIOB clock 
+    // Enable GPIOC clock
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC , ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB , ENABLE);
 
     GPIO_InitTypeDef GPIO_InitStructure;
-    // Configure PC.2 and PC.3 as Output push-pull 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_10 | GPIO_Pin_11;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
-    // Configure PB.08 and PB.09 as Output push-pull 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
 // red
 void leds_error_on() 
 {
-  GPIOC->ODR |= 0X0008;
+  GPIOC->ODR |= 0X0040;
 }
 void leds_error_off() 
 {
-   GPIOC->ODR &= ~0X0008;
+   GPIOC->ODR &= ~0X0040;
 }
 void leds_error_toggle() 
 {
-   GPIOC->ODR ^= 0X0008;
+   GPIOC->ODR ^= 0X0040;
 }
 uint8_t leds_error_isOn()
 {
   u8 bitstatus = 0x00;
-  if ((GPIOC->ODR & 0X0008) != (u32)0)
+  if ((GPIOC->ODR & 0X0040) != (u32)0)
   {
     bitstatus = 0x01;
   }
@@ -74,20 +67,20 @@ void    leds_error_blink()
 // green
 void    leds_radio_on() 
 {
-   GPIOC->ODR |= 0X0004;
+   GPIOC->ODR |= 0X0080;
 }
 void leds_radio_off() 
 {
-   GPIOC->ODR &= ~0X0004;
+   GPIOC->ODR &= ~0X0080;
 }
 void leds_radio_toggle() 
 {
-   GPIOC->ODR ^= 0X0004;
+   GPIOC->ODR ^= 0X0080;
 }
 uint8_t leds_radio_isOn() 
 {
   u8 bitstatus = 0x00;
-  if ((GPIOC->ODR & 0X0004) != (u32)0)
+  if ((GPIOC->ODR & 0X0080) != (u32)0)
   {
     bitstatus = 0x01;
   }
@@ -101,20 +94,20 @@ uint8_t leds_radio_isOn()
 // blue
 void leds_sync_on() 
 {
-   GPIOB->ODR |= 0X0200;
+   GPIOC->ODR |= 0X0400;
 }
 void leds_sync_off() 
 {
-   GPIOB->ODR &= ~0X0200;
+   GPIOC->ODR &= ~0X0400;
 }
 void leds_sync_toggle()
 {
-   GPIOB->ODR ^= 0X0200;
+   GPIOC->ODR ^= 0X0400;
 }
 uint8_t leds_sync_isOn() 
 {
   u8 bitstatus = 0x00;
-  if ((GPIOA->ODR & 0X0200) != (u32)0)
+  if ((GPIOC->ODR & 0X0400) != (u32)0)
   {
     bitstatus = 0x01;
   }
@@ -127,23 +120,23 @@ uint8_t leds_sync_isOn()
 // yellow
 void    leds_debug_on()
 {
-   GPIOB->ODR |= 0X0100;  
+   GPIOC->ODR |= 0X0800;  
 }
 
 void    leds_debug_off()
 {
-   GPIOB->ODR &= ~0X0100;  
+   GPIOC->ODR &= ~0X0800;  
 }
 
 void    leds_debug_toggle()
 {
-   GPIOB->ODR ^= 0X0100;  
+   GPIOC->ODR ^= 0X0800;  
 }
 
 uint8_t leds_debug_isOn()
 {
   u8 bitstatus = 0x00;
-  if ((GPIOB->ODR & 0X0100) != (u32)0)
+  if ((GPIOC->ODR & 0X0800) != (u32)0)
   {
     bitstatus = 0x01;
   }
@@ -156,37 +149,34 @@ uint8_t leds_debug_isOn()
 
 void leds_all_on() 
 {
-   GPIOC->ODR |= 0X000C;
-   GPIOB->ODR |= 0X0300;
+   GPIOC->ODR |= 0X0CC0;
 }
 void leds_all_off() 
 {
-   GPIOC->ODR &= ~0X000C;
-   GPIOB->ODR &= ~0X0300;
+   GPIOC->ODR &= ~0X0CC0;
 }
 void leds_all_toggle() 
 {
-   GPIOC->ODR ^= 0X000C;
-   GPIOB->ODR ^= 0X0300;
+   GPIOC->ODR ^= 0X0CC0;
 }
 
 void leds_circular_shift() 
 {
-     GPIOC->ODR ^= 0X0008;
+     GPIOC->ODR ^= 0X0040;
      Delay();
-     GPIOC->ODR ^= 0X0008;
+     GPIOC->ODR ^= 0X0040;
      Delay();
-     GPIOC->ODR ^= 0X0004;
+     GPIOC->ODR ^= 0X0080;
      Delay();
-     GPIOC->ODR ^= 0X0004;
+     GPIOC->ODR ^= 0X0080;
      Delay();
-     GPIOB->ODR ^= 0X0200;
+     GPIOC->ODR ^= 0X0400;
      Delay();
-     GPIOB->ODR ^= 0X0200;
+     GPIOC->ODR ^= 0X0400;
      Delay();
-     GPIOB->ODR ^= 0X0100;
+     GPIOC->ODR ^= 0X0800;
      Delay();
-     GPIOB->ODR ^= 0X0100;
+     GPIOC->ODR ^= 0X0800;
      Delay();
 }
 
