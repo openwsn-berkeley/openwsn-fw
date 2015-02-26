364a5bb904159ad7ecf5c472a6a4303b24520608
Nicola Accettura <nick.accettura@gmail.com>
2015-02-26 00:18:50 -0800

    FW-335. Added opensensors driver

---
+++ drivers/common/opensensors.c
@@ -0,0 +1,25 @@
+#include "opendefs.h"
+#include "opensensors.h"
+
+opensensors_vars_t opensensors_vars;
+
+void opensensors_init(void) {
+   memset(&opensensors_vars,0,sizeof(opensensors_vars_t));
+}
+
+void opensensors_register(uint8_t path1len,
+                      uint8_t* path1val,
+                      callbackRead_cbt callbackRead,
+                      callbackConvert_cbt callbackConvert) {
+   opensensors_vars.opensensors_resource[opensensors_vars.numSensors].path1len        = path1len;
+   opensensors_vars.opensensors_resource[opensensors_vars.numSensors].path1val        = path1val;
+   opensensors_vars.opensensors_resource[opensensors_vars.numSensors].callbackRead    = callbackRead;
+   opensensors_vars.opensensors_resource[opensensors_vars.numSensors].callbackConvert = callbackConvert;
+   opensensors_vars.numSensors++;
+}
+
+void opensensors_read(void) {
+}
+}
+
+
