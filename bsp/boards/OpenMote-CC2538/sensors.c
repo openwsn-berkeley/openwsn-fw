bf468d143def33e364ffb3975902689a5c2750c2
Nicola Accettura <nick.accettura@gmail.com>
2015-02-26 14:16:42 -0800

    FW-337. Commenting sensor drivers call (to be uncommented after OpenMote-CC2538 bug fixes); changing SConscript.env to include drivers linking into bsp/boards

--- bsp/boards/OpenMote-CC2538/sensors.c
+++ bsp/boards/OpenMote-CC2538/sensors.c
@@ -15,51 +15,51 @@ const uint8_t cputemp[] = "c";
 
 void sensors_init(void) {
 
-   if (sht21_is_present()==1) {
-      sht21_init();
-      opensensors_register(
-         sizeof(temperature)-1,
-         (uint8_t*)(&temperature),
-         &sht21_read_temperature,
-         &sht21_convert_temperature
-      );
-      opensensors_register(
-         sizeof(humidity)-1,
-         (uint8_t*)(&humidity),
-         &sht21_read_humidity,
-         &sht21_convert_humidity
-      );
-   }
-   if (max44009_is_present()==1) {
-      max44009_init();
-      opensensors_register(
-         sizeof(light)-1,
-         (uint8_t*)(&light),
-         &max44009_read_light,
-         &max44009_convert_light
-      );
-   }
-   if (adxl346_is_present()==1) {
-      adxl346_init();
-      opensensors_register(
-         sizeof(x)-1,
-         (uint8_t*)(&x),
-         &adxl346_read_x,
-         NULL
-      );
-      opensensors_register(
-         sizeof(y)-1,
-         (uint8_t*)(&y),
-         &adxl346_read_y,
-         NULL
-      );
-      opensensors_register(
-         sizeof(z)-1,
-         (uint8_t*)(&z),
-         &adxl346_read_z,
-         NULL
-      );
-   }
+   // if (sht21_is_present()==1) {
+      // sht21_init();
+      // opensensors_register(
+         // sizeof(temperature)-1,
+         // (uint8_t*)(&temperature),
+         // &sht21_read_temperature,
+         // &sht21_convert_temperature
+      // );
+      // opensensors_register(
+         // sizeof(humidity)-1,
+         // (uint8_t*)(&humidity),
+         // &sht21_read_humidity,
+         // &sht21_convert_humidity
+      // );
+   // }
+   // if (max44009_is_present()==1) {
+      // max44009_init();
+      // opensensors_register(
+         // sizeof(light)-1,
+         // (uint8_t*)(&light),
+         // &max44009_read_light,
+         // &max44009_convert_light
+      // );
+   // }
+   // if (adxl346_is_present()==1) {
+      // adxl346_init();
+      // opensensors_register(
+         // sizeof(x)-1,
+         // (uint8_t*)(&x),
+         // &adxl346_read_x,
+         // NULL
+      // );
+      // opensensors_register(
+         // sizeof(y)-1,
+         // (uint8_t*)(&y),
+         // &adxl346_read_y,
+         // NULL
+      // );
+      // opensensors_register(
+         // sizeof(z)-1,
+         // (uint8_t*)(&z),
+         // &adxl346_read_z,
+         // NULL
+      // );
+   // }
 
    adc_sensor_init();
    opensensors_register(
