a2ca020f8e95d923e67ab93dec50edcb6a4bdc4b
Marcelo Barros de Almeida <marcelobarrosalmeida@gmail.com>
2015-03-02 16:01:42 -0800

    FW-341. Add crypto_driver_board.c implementation to OpenMote-CC2538.
    Checkout c2f3d003 from https://github.com/marcelobarrosalmeida/ccms
    Fix some line endings and indent.

---
+++ bsp/boards/OpenMote-CC2538/crypto_driver_board.c
@@ -0,0 +1,28 @@
+/**
+  \brief Crypto driver initialization for custom board
+  
+  \author Marcelo Barros de Almeida <marcelobarrosalmeida@gmail.com>
+ */
+#include <stdint.h>
+#include "crypto_driver.h"
+#include "aes_ccms.h"
+#include "aes_ctr.h"
+#include "aes_ctr_board.h"
+#include "aes_cbc_mac.h"
+#include "aes_cbc_mac_board.h"
+#include "aes_ecb.h"
+#include "aes_ecb_board.h"
+#include "crypto_driver_board.h"
+
+int crypto_driver_board_init(crypto_driver_t *crypto_driver)
+{
+   crypto_driver->aes_ccms_dec = aes_ccms_dec;
+   crypto_driver->aes_ccms_enc = aes_ccms_enc;
+   crypto_driver->aes_cbc_mac_enc = aes_cbc_mac_enc;
+   crypto_driver->aes_cbc_mac_enc_raw = aes_cbc_mac_enc_raw; //aes_cbc_mac_board_enc_raw; 
+   crypto_driver->aes_ctr_enc = aes_ctr_enc;
+   crypto_driver->aes_ctr_enc_raw = aes_ctr_enc_raw; // aes_ctr_board_enc_raw;
+   crypto_driver->aes_ecb_enc = aes_ecb_enc; //aes_ecb_board_enc
+
+   return 0;
+}
