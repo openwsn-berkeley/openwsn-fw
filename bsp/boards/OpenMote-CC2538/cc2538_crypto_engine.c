6595547a737f0ebfd6d27b9f2f28138c4a673f46
Mališa Vučinić <malishav@gmail.com>
2015-03-03 16:39:08 -0800

    FW-341. Add all-firmware (placeholder) implementation of CC2538 crypto engine.

---
+++ bsp/boards/OpenMote-CC2538/cc2538_crypto_engine.c
@@ -0,0 +1,29 @@
+/**
+  \brief Crypto engine implementation for OpenMote-CC2538
+  
+  \author Marcelo Barros de Almeida <marcelobarrosalmeida@gmail.com>
+ */
+#include <stdint.h>
+#include "cc2538_crypto_engine.h"
+#include "aes_ccms.h"
+#include "aes_ctr.h"
+#include "aes_cbc_mac.h"
+#include "aes_ecb.h"
+
+static int init(void)
+{
+   return 0;
+}
+/*---------------------------------------------------------------------------*/
+const struct crypto_engine cc2538_crypto_engine = {
+   aes_ccms_enc,
+   aes_ccms_dec,
+   aes_cbc_mac_enc,
+   aes_cbc_mac_enc_raw,
+   aes_ctr_enc,
+   aes_ctr_enc_raw,
+   aes_ecb_enc,
+   init,
+};
+/*---------------------------------------------------------------------------*/
+
---
+++ bsp/boards/OpenMote-CC2538/cc2538_crypto_engine.h
@@ -0,0 +1,21 @@
+/**
+\brief Definitions for crypto engine initialization
+
+\author Marcelo Barros de Almeida <marcelobarrosalmeida@gmail.com>
+*/
+#ifndef __CC2538_CRYPTO_ENGINE_H__
+#define __CC2538_CRYPTO_ENGINE_H__
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#include "crypto_engine.h"
+
+extern const struct crypto_engine cc2538_crypto_engine;   
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif /* __CC2538_CRYPTO_ENGINE_H__ */
