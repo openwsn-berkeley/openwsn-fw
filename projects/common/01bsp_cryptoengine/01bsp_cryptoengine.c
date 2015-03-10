16a2799d92ed086a4fbca97a8350ed9fd8d9ff0e
Mališa Vučinić <malishav@gmail.com>
2015-03-10 10:57:23 -0700

    FW-348. Create main project file with some test suites for stand-alone AES.

---
+++ projects/common/01bsp_cryptoengine/01bsp_cryptoengine.c
@@ -0,0 +1,76 @@
+/**
+\brief This is a program which shows how to use the CRYPTO_ENGINE driver
+       and runs some tests in order to verify different crypto implementations.
+
+\note: You can use this project with any platform but be careful to declare the
+       CRYPTO_ENGINE implementation for the board that you want to test.
+
+Load this program on your boards. Green LED will stay on indefinitely if all
+tests passed. Red if there was an error.
+
+\author Malisa Vucinic <malishav@gmail.com>, March 2015.
+*/
+
+#include "stdint.h"
+#include "stdio.h"
+// bsp modules required
+#include "board.h"
+#include "crypto_engine.h"
+#include "leds.h"
+
+/**
+\brief The program starts executing here.
+*/
+int mote_main(void) {
+   uint8_t i;
+   uint8_t ret;
+   uint8_t ret_fails = 0;
+   uint8_t success = 0;
+ 
+#define NUM_TEST_VECTORS 4
+   uint8_t key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+   uint8_t test_vector[NUM_TEST_VECTORS][16] = {
+                                                   {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a},
+                                                   {0xae, 0x2d, 0x8a, 0x57, 0x1e, 0x03, 0xac, 0x9c, 0x9e, 0xb7, 0x6f, 0xac, 0x45, 0xaf, 0x8e, 0x51},
+                                                   {0x30, 0xc8, 0x1c, 0x46, 0xa3, 0x5c, 0xe4, 0x11, 0xe5, 0xfb, 0xc1, 0x19, 0x1a, 0x0a, 0x52, 0xef},
+                                                   {0xf6, 0x9f, 0x24, 0x45, 0xdf, 0x4f, 0x9b, 0x17, 0xad, 0x2b, 0x41, 0x7b, 0xe6, 0x6c, 0x37, 0x10},
+                                                };
+
+   uint8_t ciphertext[NUM_TEST_VECTORS][16] =   {
+                                                   {0x3a, 0xd7, 0x7b, 0xb4, 0x0d, 0x7a, 0x36, 0x60, 0xa8, 0x9e, 0xca, 0xf3, 0x24, 0x66, 0xef, 0x97},
+                                                   {0xf5, 0xd3, 0xd5, 0x85, 0x03, 0xb9, 0x69, 0x9d, 0xe7, 0x85, 0x89, 0x5a, 0x96, 0xfd, 0xba, 0xaf},
+                                                   {0x43, 0xb1, 0xcd, 0x7f, 0x59, 0x8e, 0xce, 0x23, 0x88, 0x1b, 0x00, 0xe3, 0xed, 0x03, 0x06, 0x88},
+                                                   {0x7b, 0x0c, 0x78, 0x5e, 0x27, 0xe8, 0xad, 0x3f, 0x82, 0x23, 0x20, 0x71, 0x04, 0x72, 0x5d, 0xd4},
+                                                };
+
+   board_init();
+   
+   // Init the CRYPTO_ENGINE driver
+   CRYPTO_ENGINE.init();
+
+   /* AES-ECB Test Suite */
+   for (i = 0; i < NUM_TEST_VECTORS; i++) {
+	   ret = CRYPTO_ENGINE.aes_ecb_enc(&test_vector[i], key);
+
+      if (ret == E_FAIL) {
+         ret_fails++;
+      }
+      else if (memcmp(&test_vector[i], &ciphertext[i], 16) == 0) {
+         success++;
+      }
+   }
+
+   if (success == NUM_TEST_VECTORS) {
+      do {
+         leds_radio_on();
+      } while(1);
+   }
+   else {
+      do {
+         leds_error_on();
+      } while(1);
+   }
+   
+   return 0;
+}
+
