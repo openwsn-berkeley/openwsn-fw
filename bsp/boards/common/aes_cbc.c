fc19ca9ec22603748691b5d9632577c9d7366c10
Mališa Vučinić <malishav@gmail.com>
2015-03-18 20:55:06 -0700

    FW-355. Extend the firmware implementation of CBC to use explicit IV.
    Previously, first block of the input buffer was assumed to be the IV.

--- bsp/boards/common/aes_cbc.c
+++ bsp/boards/common/aes_cbc.c
@@ -2,6 +2,7 @@
 \brief AES CBC implementation
   
 \author Marcelo Barros de Almeida <marcelobarrosalmeida@gmail.com>, March 2015.
+\author Malisa Vucinic <malishav@gmail.com>, March 2015.
 */
 #include <string.h>
 #include <stdint.h>
@@ -23,15 +24,18 @@ owerror_t aes_cbc_enc_raw(uint8_t* buffer, uint8_t len, uint8_t key[16], uint8_t
    uint8_t  k;
    uint8_t  nb;
    uint8_t* pbuf;
+   uint8_t* pxor;
 
    nb = len >> 4;
-   for (n = 0; n < (nb - 1); n++) {
+   pxor = iv;
+   for (n = 0; n < nb; n++) {
       pbuf = &buffer[16 * n];
       // may be faster if vector are aligned to 4 bytes (use long instead char in xor)
       for (k = 0; k < 16; k++) {
-         pbuf[16 + k] ^= pbuf[k];
+            pbuf[k] ^= pxor[k];
       }
-      CRYPTO_ENGINE.aes_ecb_enc(&pbuf[16],key);
+      CRYPTO_ENGINE.aes_ecb_enc(pbuf,key);
+      pxor = pbuf;
    }
    return E_SUCCESS;
 }
