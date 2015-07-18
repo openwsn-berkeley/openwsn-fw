b7743b7fbd79c207be9d08e3b52efcf4652f37ee
NicolaAccettura <nick.accettura@gmail.com>
2015-07-18 04:02:23 -0700

    FW-432. Fix.

--- openstack/02a-MAClow/IEEE802154E.h
+++ openstack/02a-MAClow/IEEE802154E.h
@@ -232,6 +232,7 @@ typedef struct {
    uint8_t                   freq;                    // frequency of the current slot
    uint8_t                   asnOffset;               // offset inside the frame
    uint8_t                   singleChannel;           // the single channel used for transmission
+   bool                      singleChannelChanged;    // detect id singleChannelChanged
    uint8_t                   chTemplate[16];          // storing the template of hopping sequence
    // template ID
    uint8_t                   tsTemplateId;            // timeslot template id
