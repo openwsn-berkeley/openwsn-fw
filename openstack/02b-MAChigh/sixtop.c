dddbeace9a8e01feac57539e6e63cc6c55317afa
changtengfei <tengfei.chang@gmail.com>
2015-07-30 16:03:11 +0800

    FW-437. Fixed.

--- openstack/02b-MAChigh/sixtop.c
+++ openstack/02b-MAChigh/sixtop.c
@@ -1377,7 +1377,7 @@ bool sixtop_candidateAddCellList(
    
    numCandCells=0;
    for(counter=0;counter<SCHEDULEIEMAXNUMCELLS;counter++){
-      i = (openrandom_get16b()&0x07)+ (openrandom_get16b()&0x03);
+      i = openrandom_get16b()%schedule_getFrameLength();
       if(schedule_isSlotOffsetAvailable(i)==TRUE){
          cellList[numCandCells].tsNum       = i;
          cellList[numCandCells].choffset    = 0;
