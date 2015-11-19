fe955ef2d4f918c59d9ca11171697f8e71d92253
changtengfei <tengfei.chang@gmail.com>
2015-11-19 17:28:11 +0100

    FW-reallocate. add cstorm and change schedule configuration

--- openstack/02b-MAChigh/schedule.h
+++ openstack/02b-MAChigh/schedule.h
@@ -20,14 +20,14 @@ The superframe repears over time and can be arbitrarly long.
 #define SLOTFRAME_LENGTH    101 //should be 101
 
 //draft-ietf-6tisch-minimal-06
-#define SCHEDULE_MINIMAL_6TISCH_ACTIVE_CELLS                      5
+#define SCHEDULE_MINIMAL_6TISCH_ACTIVE_CELLS                      4
 #define SCHEDULE_MINIMAL_6TISCH_SLOTOFFSET                        0
 #define SCHEDULE_MINIMAL_6TISCH_CHANNELOFFSET                     0
 #define SCHEDULE_MINIMAL_6TISCH_DEFAULT_SLOTFRAME_HANDLE          1 //id of slotframe
 #define SCHEDULE_MINIMAL_6TISCH_DEFAULT_SLOTFRAME_NUMBER          1 //1 slotframe by default.
 
 #define NUMSERIALRX          1
-#define NUMSLOTSOFF          20
+#define NUMSLOTSOFF          8
 
 /**
 \brief Maximum number of active slots in a superframe.
