a5551f6b5906bc546dbc52938f78b865c76b4cef
xvilajosana <xvilajosana@eecs.berkeley.edu>
2016-01-23 12:05:40 +0100

    FW-458 leds working

--- bsp/boards/openmote-ezr32wg/leds.c
+++ bsp/boards/openmote-ezr32wg/leds.c
@@ -26,7 +26,7 @@
 //=========================== public ==========================================
 
 void leds_init() {
-
+	//enable clock for this peripheral
 	CMU_ClockEnable(cmuClock_HFPER, true);
 	CMU_ClockEnable(cmuClock_GPIO, true);
 
@@ -35,66 +35,96 @@ void leds_init() {
 	GPIO_PinModeSet(gpioPortF, LEDS_PORT_DEBUG, gpioModePushPull, 0);
 	GPIO_PinModeSet(gpioPortF, LEDS_PORT_SYNC, gpioModePushPull, 0);
 	GPIO_PinModeSet(gpioPortF, LEDS_PORT_RADIO, gpioModePushPull, 0);
-
 }
 
 // red
 void leds_error_on() {
-	 GPIO_PinOutSet(gpioPortF, LEDS_PORT_ERROR);
+	GPIO_PinOutSet(gpioPortF, LEDS_PORT_ERROR);
 }
 
 void leds_error_off() {
-	 GPIO_PinOutClear(gpioPortF, LEDS_PORT_ERROR);
-
+	GPIO_PinOutClear(gpioPortF, LEDS_PORT_ERROR);
 }
 
 void leds_error_toggle() {
+	GPIO_PinOutToggle(gpioPortF, LEDS_PORT_ERROR);
 }
+
 uint8_t leds_error_isOn() {
-	return 0;
+	return (uint8_t) (1 == GPIO_PinOutGet(gpioPortF, LEDS_PORT_ERROR));
 }
 
 // orange
 void leds_sync_on() {
+	GPIO_PinOutSet(gpioPortF, LEDS_PORT_SYNC);
 }
+
 void leds_sync_off() {
+	GPIO_PinOutClear(gpioPortF, LEDS_PORT_SYNC);
 }
+
 void leds_sync_toggle() {
+	GPIO_PinOutToggle(gpioPortF, LEDS_PORT_SYNC);
 }
+
 uint8_t leds_sync_isOn() {
-	return 0;
+	return (uint8_t) (1 == GPIO_PinOutGet(gpioPortF, LEDS_PORT_SYNC));
 }
 
 // green
 void leds_radio_on() {
+	GPIO_PinOutSet(gpioPortF, LEDS_PORT_RADIO);
 }
+
 void leds_radio_off() {
+	GPIO_PinOutClear(gpioPortF, LEDS_PORT_RADIO);
 }
+
 void leds_radio_toggle() {
+	GPIO_PinOutToggle(gpioPortF, LEDS_PORT_RADIO);
 }
+
 uint8_t leds_radio_isOn() {
-	return 0;
+	return (uint8_t) (1 == GPIO_PinOutGet(gpioPortF, LEDS_PORT_RADIO));
 }
 
 // yellow
 void leds_debug_on() {
+	GPIO_PinOutSet(gpioPortF, LEDS_PORT_DEBUG);
 }
+
 void leds_debug_off() {
+	GPIO_PinOutClear(gpioPortF, LEDS_PORT_DEBUG);
 }
+
 void leds_debug_toggle() {
+	GPIO_PinOutToggle(gpioPortF, LEDS_PORT_DEBUG);
 }
+
 uint8_t leds_debug_isOn() {
-	return 0;
+	return (uint8_t) (1 == GPIO_PinOutGet(gpioPortF, LEDS_PORT_DEBUG));
 }
 
 // all
 void leds_all_on() {
+	leds_radio_on();
+	leds_sync_on();
+	leds_debug_on();
+	leds_error_on();
 }
+
 void leds_all_off() {
-
+	leds_radio_off();
+	leds_sync_off();
+	leds_debug_off();
+	leds_error_off();
 }
-void leds_all_toggle() {
 
+void leds_all_toggle() {
+	leds_radio_toggle();
+	leds_sync_toggle();
+	leds_debug_toggle();
+	leds_error_toggle();
 }
 
 void leds_error_blink() {
@@ -102,11 +132,11 @@ void leds_error_blink() {
 	volatile uint16_t delay;
 
 	// turn all LEDs off
-	//bspLedClear(BSP_LED_ALL);
+	leds_all_off();
 
 	// blink error LED for ~10s
 	for (i = 0; i < 80; i++) {
-		//bspLedToggle(BSP_LED_1);
+		leds_error_toggle();
 		for (delay = 0xffff; delay > 0; delay--)
 			;
 		for (delay = 0xffff; delay > 0; delay--)
@@ -115,11 +145,14 @@ void leds_error_blink() {
 }
 
 void leds_circular_shift() {
+    //not implemented
+	return;
 }
 
 void leds_increment() {
+	//not implemented
+	return;
 }
 
 //=========================== private =========================================
-
 
