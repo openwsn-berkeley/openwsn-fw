c886879b49f8eba409fed38f9227a60d49ec85f9
Xavier Vilajosana <xvilajosana@eecs.berkeley.edu>
2016-05-11 15:42:38 +0200

    FW-488 adding python side (WIP)

--- projects/common/03oos_bike_rx/03oos_bike_rx.py
+++ projects/common/03oos_bike_rx/03oos_bike_rx.py
@@ -4,90 +4,100 @@
 import sys
 import struct
 import socket
+import sched, time
+import calendar
+
 try:
    import serial
 except ImportError:
    pass
 
-banner  = []
-banner += [""]
-banner += [" ___                 _ _ _  ___  _ _ "]
-banner += ["| . | ___  ___ ._ _ | | | |/ __>| \ |"]
-banner += ["| | || . \/ ._>| ' || | | |\__ \|   |"]
-banner += ["`___'|  _/\___.|_|_||__/_/ <___/|_\_|"]
-banner += ["     |_|                  openwsn.org"]
-banner += [""]
-banner  = '\n'.join(banner)
-print banner
+
+class BikeCoordinator():
+
+    s = sched.scheduler(time.time, time.sleep)
+
+    def __init__(self):
+        banner  = []
+        banner += [""]
+        banner += [" ___                 _ _ _  ___  _ _ "]
+        banner += ["| . | ___  ___ ._ _ | | | |/ __>| \ |"]
+        banner += ["| | || . \/ ._>| ' || | | |\__ \|   |"]
+        banner += ["`___'|  _/\___.|_|_||__/_/ <___/|_\_|"]
+        banner += ["     |_|                  openwsn.org"]
+        banner += [""]
+        banner  = '\n'.join(banner)
+        print 'Creating Bike Coordinator\n'
+        print banner
 
 
-def mote_connect(motename=None , serialport= None, baudrate='115200'):
-    try:
-        if (motename):
-            mote = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            mote.connect((motename,20000))
-        else:
-            mote = serial.Serial(serialport, baudrate)
-        return mote
-    except Exception as err:
-        print "{0}".format(err)
-        raw_input('Press Enter to close.')
-        sys.exit(1)
-    
+    def startWorking(self):
+        mote = None
+        time_read = calendar.timegm(time.gmtime())
+        print time_read
+        #connect to the serial port
 
-#============================ configuration and connection ===================================
+        #============================ configuration and connection ===================================
 
-iotlab_serialport = False
-motename = 'wsn430-35'
-serialport = 'COM10'
-mote = None
+        mote = self.mote_connect(serialport="\dev\ttyUSB0", baudrate='115200')
 
-t = raw_input('Are you running on IoT-LAB nodes ? (Y|N): ')
-if  (not t.strip() or t.strip() in ['1','yes','y','Y']):
-    t = raw_input('Enter mote name ? (e.g. {0}): '.format(motename))
-    if t.strip():
-        motename = t.strip()
-    archi = motename.split('-')
-    assert len(archi) == 2
-    assert archi[0] in ['wsn430', 'a8', 'm3'] 
-    if (archi[0] != 'a8'):
-        iotlab_serialport = True
-        mote = mote_connect(motename=motename)
-    else:
-        mote = mote_connect(serialport='/dev/ttyA8_M3', baudrate='500000')
-    
-else:
-    t = raw_input('Enter serial port name (e.g. {0}): '.format(serialport))    
-    if t.strip():
-        serialport = t.strip()
-    mote = mote_connect(serialport=serialport)
+        #self.startPeriodicEpochTransmission(self)
+        #self.startReceiving(self, mote):
+
+    def mote_connect(self, motename=None , serialport= None, baudrate='115200'):
+        try:
+            if (motename):
+                mote = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                mote.connect((motename,20000))
+            else:
+                mote = serial.Serial(serialport, baudrate)
+            return mote
+        except Exception as err:
+            print "{0}".format(err)
+            raw_input('Press Enter to close.')
+            sys.exit(1)
 
 #============================ read ============================================
 
-rawFrame = []
-
-while True:
-    
-    if iotlab_serialport:
-        bytes = mote.recv(1024)
-        rawFrame += [ord(b) for b in bytes]
-    else:
-        byte  = mote.read(1)
-        rawFrame += [ord(byte)]
-    
-    if rawFrame[-3:]==[0xff]*3 and len(rawFrame)>=8:
-        
-        (rxpk_len,rxpk_num,rxpk_rssi,rxpk_lqi,rxpk_crc) = \
-            struct.unpack('>BBbBB', ''.join([chr(b) for b in rawFrame[-8:-3]]))
-        print 'len={0:<3} num={1:<3} rssi={2:<4} lqi={3:<3} crc={4}'.format(
-            rxpk_len,
-            rxpk_num,
-            rxpk_rssi,
-            rxpk_lqi,
-            rxpk_crc
-        )
-        
-        if rxpk_len>127:
-            print "ERROR: frame too long.\a"
-        
+    def startReceiving(self, mote):
         rawFrame = []
+
+        while True:
+            byte  = mote.read(1)
+            rawFrame += [ord(byte)]
+
+            if rawFrame[-3:]==[0xff]*3 and len(rawFrame)>=8:
+
+                (rxpk_len,rxpk_num,rxpk_rssi,rxpk_lqi,rxpk_crc) = struct.unpack('>BBbBB', ''.join([chr(b) for b in rawFrame[-8:-3]]))
+
+                print 'len={0:<3} num={1:<3} rssi={2:<4} lqi={3:<3} crc={4}'.format(
+                    rxpk_len,
+                    rxpk_num,
+                    rxpk_rssi,
+                    rxpk_lqi,
+                    rxpk_crc
+                )
+
+                if rxpk_len>127:
+                    print "ERROR: frame too long.\a"
+
+                rawFrame = []
+
+
+    def startPeriodicEpochTransmission(self):
+        self.s = sched.scheduler(time.time, time.sleep)
+        s.enter(10, 1, writeEpoch, ())
+
+
+    def writeEpoch(self):
+        time_read = calendar.timegm(time.gmtime())
+        print time_read
+
+
+
+
+#============================ main ============================================
+
+if __name__=="__main__":
+    bike = BikeCoordinator()
+    bike.startWorking()
