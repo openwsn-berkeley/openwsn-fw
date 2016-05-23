603711e72c02103c972c9a9d3e7b006e30bec126
Fabrice Theoleyre <theoleyre@unistra.fr>
2016-05-23 16:19:34 +0200

    larger densities (20 neighbors at most now)

--- openstack/02b-MAChigh/neighbors.h
+++ openstack/02b-MAChigh/neighbors.h
@@ -12,7 +12,7 @@
 
 //=========================== define ==========================================
 
-#define MAXNUMNEIGHBORS           10
+#define MAXNUMNEIGHBORS           20
 #define MAXPREFERENCE             2
 #define BADNEIGHBORMAXRSSI        -80 //dBm
 #define GOODNEIGHBORMINRSSI       -90 //dBm
