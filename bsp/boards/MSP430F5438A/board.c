7edda04c0479377e3a707913f58d2d3e63e6dac4
jmmunoz86 <jmmunoz86@gmail.com>
2016-07-12 21:20:10 +0200

    01bsp_leds and 01bsp_debugpins.

--- bsp/boards/MSP430F5438A/board.c
+++ bsp/boards/MSP430F5438A/board.c
@@ -9,7 +9,7 @@
 // bsp modules
 #include "debugpins.h"
 #include "leds.h"
-#include "uart.h"
+//#include "uart.h"
 #include "spi.h"
 #include "bsp_timer.h"
 #include "radio.h"
@@ -30,37 +30,66 @@ int main(void) {
 //=========================== public ==========================================
 
 void board_init() {
-   // disable watchdog timer
-   WDTCTL     =  WDTPW + WDTHOLD;
    
-   // setup clock speed
-   DCOCTL    |=  DCO0 | DCO1 | DCO2;             // MCLK at ~8MHz
-   BCSCTL1   |=  RSEL0 | RSEL1 | RSEL2;          // MCLK at ~8MHz
+   
+   //DCOCTL    |=  DCO0 | DCO1 | DCO2;             // MCLK at ~8MHz
+   //BCSCTL1   |=  RSEL0 | RSEL1 | RSEL2;          // MCLK at ~8MHz
                                                  // by default, ACLK from 32kHz XTAL which is running
+   // disable watchdog timer 
+   WDTCTL = WDTPW+WDTHOLD;                   // Stop WDT
+   __bis_SR_register(SCG0);                  // allows to unset the DCO flag error    
+ // __bis_SR_register(SCG1);                // this switches off the smclk
+ 
+  // Initialize LFXT1
+  P7SEL |= 0x03;                            // Select XT1
+  UCSCTL6 &= ~(XT1OFF);                     // XT1 On
+  UCSCTL6 |= XCAP_3;                        // Internal load cap
+    // Loop until XT1 fault flag is cleared
+  do
+  {
+    UCSCTL7 &= ~XT1LFOFFG;                  // Clear XT1 fault flags
+  }while (UCSCTL7&XT1LFOFFG);               // Test XT1 fault flag
+  
+  //P6DIR |= BIT1;                            // P6.1 output
+  P11DIR |= 0x07;                           // ACLK, MCLK, SMCLK set out to pins
+  P11SEL |= 0x07;                           // P11.0,1,2 for debugging purposes.
+  //P4DIR |= BIT0 | BIT1 | BIT2 | BIT3 ;
+  // setup clock speed
+  UCSCTL0 = DCO3 | DCO1;              //  ~26.5 MHz , DCO3+DCO1 and DCORSEL_7
+  UCSCTL1 = DCORSEL_7 | DISMOD;     
+  UCSCTL2 = 0;
+  UCSCTL3 = 0;
+  UCSCTL4 =  SELM_3 | SELS_3 ;
+  do
+  {
+    UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + XT1HFOFFG + DCOFFG );
+                                       // Clear XT2,XT1,DCO fault flags
+    SFRIFG1 &= ~OFIFG;                      // Clear fault flags
+  }while (SFRIFG1&OFIFG); 
    
    // initialize pins
-   P4DIR     |=  0x20;                           // [P4.5] radio VREG:  output
-   P4DIR     |=  0x40;                           // [P4.6] radio reset: output
+   //P4DIR     |=  0x20;                           // [P4.5] radio VREG:  output
+   //P4DIR     |=  0x40;                           // [P4.6] radio reset: output
    
    // initialize bsp modules
    debugpins_init();
    leds_init();
-   uart_init();
-   spi_init();
-   bsp_timer_init();
-   radio_init();
-   radiotimer_init();
+  // uart_init();
+  // spi_init();
+  // bsp_timer_init();
+  // radio_init();
+  // radiotimer_init();
    
    // enable interrupts
-   __bis_SR_register(GIE);
+   //__bis_SR_register(GIE);
 }
 
-void board_sleep() {
-   __bis_SR_register(GIE+LPM0_bits);             // sleep, but leave ACLK on
-}
+//void board_sleep() {
+  // __bis_SR_register(GIE+LPM0_bits);             // sleep, but leave ACLK on
+//}
 
 void board_reset() {
-   WDTCTL = (WDTPW+0x1200) + WDTHOLD; // writing a wrong watchdog password to causes handler to reset
+    WDTCTL = (WDTPW+0x1200) + WDTHOLD; // writing a wrong watchdog password to causes handler to reset
 }
 
 //=========================== private =========================================
@@ -71,61 +100,61 @@ void board_reset() {
 
 // PORT2_VECTOR
 
-ISR(USART1TX){
-   debugpins_isr_set();
-   if (uart_tx_isr()==KICK_SCHEDULER) {          // UART; TX
-      __bic_SR_register_on_exit(CPUOFF);
-   }
-   debugpins_isr_clr();
-}
+//ISR(USART1TX){
+//   debugpins_isr_set();
+//   if (uart_tx_isr()==KICK_SCHEDULER) {          // UART; TX
+//      __bic_SR_register_on_exit(CPUOFF);
+//   }
+//   debugpins_isr_clr();
+//}
 
-ISR(USART1RX) {
-   debugpins_isr_set();
-   if (uart_rx_isr()==KICK_SCHEDULER) {          // UART: RX
-      __bic_SR_register_on_exit(CPUOFF);
-   }
-   debugpins_isr_clr();
-}
+//ISR(USART1RX) {
+//   debugpins_isr_set();
+//   if (uart_rx_isr()==KICK_SCHEDULER) {          // UART: RX
+//      __bic_SR_register_on_exit(CPUOFF);
+//   }
+//   debugpins_isr_clr();
+//}
 
 // PORT1_VECTOR
 
 // TIMERA1_VECTOR
 
-ISR(TIMERA0) {
-   debugpins_isr_set();
-   if (bsp_timer_isr()==KICK_SCHEDULER) {        // timer: 0
-      __bic_SR_register_on_exit(CPUOFF);
-   }
-   debugpins_isr_clr();
-}
+//ISR(TIMERA0) {
+//   debugpins_isr_set();
+//   if (bsp_timer_isr()==KICK_SCHEDULER) {        // timer: 0
+//      __bic_SR_register_on_exit(CPUOFF);
+//   }
+//   debugpins_isr_clr();
+//}
 
 // ADC12_VECTOR
 
 // USART0TX_VECTOR
 
-ISR(USART0RX) {
-   debugpins_isr_set();
-   if (spi_isr()==KICK_SCHEDULER) {              // SPI
-      __bic_SR_register_on_exit(CPUOFF);
-   }
-   debugpins_isr_clr();
-}
+//ISR(USART0RX) {
+//   debugpins_isr_set();
+//   if (spi_isr()==KICK_SCHEDULER) {              // SPI
+//      __bic_SR_register_on_exit(CPUOFF);
+//   }
+//   debugpins_isr_clr();
+//}
 
 // WDT_VECTOR
 
-ISR(COMPARATORA) {
-   debugpins_isr_set();
-   __bic_SR_register_on_exit(CPUOFF);            // restart CPU
-   debugpins_isr_clr();
-}
+//ISR(COMPARATORA) {
+//   debugpins_isr_set();
+//   __bic_SR_register_on_exit(CPUOFF);            // restart CPU
+//   debugpins_isr_clr();
+//}
 
-ISR(TIMERB1) {
-   debugpins_isr_set();
-   if (radiotimer_isr()==KICK_SCHEDULER) {       // radiotimer
-      __bic_SR_register_on_exit(CPUOFF);
-   }
-   debugpins_isr_clr();
-}
+//ISR(TIMERB1) {
+//   debugpins_isr_set();
+//   if (radiotimer_isr()==KICK_SCHEDULER) {       // radiotimer
+//      __bic_SR_register_on_exit(CPUOFF);
+//   }
+//   debugpins_isr_clr();
+//}
 
 // TIMERB0_VECTOR
 
--- bsp/boards/MSP430F5438A/leds.c
+++ bsp/boards/MSP430F5438A/leds.c
@@ -16,121 +16,126 @@
 //=========================== public ==========================================
 
 void    leds_init() {
-   P5DIR     |=  0x70;                           // P5DIR = 0bx111xxxx for LEDs
-   P5OUT     |=  0x70;                           // P2OUT = 0bx111xxxx, all LEDs off
+    P4DIR |= BIT0 | BIT1 | BIT2 | BIT3 ;                          // P4DIR = 0bxxxx1111 for LEDs
+    P4OUT |= BIT0 | BIT1 | BIT2 | BIT3 ;                          // P4OUT = 0bxxxx1111, all LEDs off
+   
 }
 
-// red = LED1 = P5.4
+// red = LED1 = P4.0
 void    leds_error_on() {
-   P5OUT     &= ~0x10;
+   P4OUT     &= ~BIT0;
 }
 void    leds_error_off() {
-   P5OUT     |=  0x10;
+   P4OUT     |=  BIT0;
 }
 void    leds_error_toggle() {
-   P5OUT     ^=  0x10;
+   P4OUT     ^=  BIT0;
 }
 uint8_t leds_error_isOn() {
-   return (uint8_t)(P5OUT & 0x10)>>4;
+   return (uint8_t)(P4OUT & BIT0);
 }
 void leds_error_blink() {
    uint8_t i;
    volatile uint16_t delay;
    // turn all LEDs off
-   P5OUT     |=  0x70;
+   P4OUT     |=  BIT0 | BIT1 | BIT2 | BIT3 ;
    
    // blink error LED for ~10s
    for (i=0;i<80;i++) {
-      P5OUT     ^=  0x10;
+      P4OUT     ^=  BIT0;
       for (delay=0xffff;delay>0;delay--);
    }
 }
 
-// green = LED2 = P5.5
+// yellow = LED2 = P4.1
 void    leds_radio_on() {
-   P5OUT     &= ~0x20;
+   P4OUT     &= ~BIT1;
 }
 void    leds_radio_off() {
-   P5OUT     |=  0x20;
+   P4OUT     |=  BIT1;
 }
 void    leds_radio_toggle() {
-   P5OUT     ^=  0x20;
+   P4OUT     ^=  BIT1;
 }
 uint8_t leds_radio_isOn() {
-   return (uint8_t)(P5OUT & 0x20)>>5;
+   return (uint8_t)(P4OUT & BIT1);
 }
 
-// blue = LED3 = P5.6
+// green = LED3 = P4.2
 void    leds_sync_on() {
-   P5OUT     &= ~0x40;
+   P4OUT     &= ~BIT2;
 }
 void    leds_sync_off() {
-   P5OUT     |=  0x40;
+   P4OUT     |=  BIT2;
 }
 void    leds_sync_toggle() {
-   P5OUT     ^=  0x40;
+   P4OUT     ^=  BIT2;
 }
 uint8_t leds_sync_isOn() {
-   return (uint8_t)(P5OUT & 0x40)>>6;
+   return (uint8_t)(P4OUT & BIT2);
 }
 
+// red = LED4 = P4.3
 void    leds_debug_on() {
+  P4OUT     &= ~BIT3;
 }
 
 void    leds_debug_off() {
+  P4OUT     |=  BIT3;
 }
 
 void    leds_debug_toggle() {
+  P4OUT     ^=  BIT3;
 }
 
 uint8_t leds_debug_isOn() {
-   return 0;
+   return (uint8_t)(P4OUT & BIT3);
 }
 
 void    leds_all_on() {
-   P5OUT     &= ~0x70;
+   P4OUT     &= ~(BIT0 | BIT1 | BIT2 | BIT3);
 }
 void    leds_all_off() {
-   P5OUT     |=  0x70;
+   P4OUT     |=  BIT0 | BIT1 | BIT2 | BIT3;
 }
 void    leds_all_toggle() {
-   P5OUT     ^=  0x70;
+   P4OUT     ^=  BIT0 | BIT1 | BIT2 | BIT3;
 }
 
 void    leds_circular_shift() {
    uint8_t leds_on;
    // get LED state
-   leds_on  = (~P5OUT & 0x70) >> 4;
+   leds_on  = (~P4OUT & BIT0 | BIT1 | BIT2 | BIT3) ;
    // modify LED state
    if (leds_on==0) {                             // if no LEDs on, switch on one
-      leds_on = 0x01;
+      leds_on = BIT0;
    } else {
       leds_on <<= 1;                             // shift by one position
-      if ((leds_on & 0x08)!=0) {
-         leds_on &= ~0x08;
-         leds_on |=  0x01;                       // handle overflow
+      if ((leds_on & (BIT0 | BIT1 | BIT2 | BIT3))!=0) {
+         leds_on &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+         leds_on |=  BIT0;                       // handle overflow
       }
    }
    // apply updated LED state
    leds_on <<= 4;                                // send back to position 4
-   P5OUT |=  (~leds_on & 0x70);                  // switch on the leds marked '1' in leds_on
-   P5OUT &= ~( leds_on & 0x70);                  // switch off the leds marked '0' in leds_on
+   P4OUT |=  (~leds_on & 0x000f);                  // switch on the leds marked '1' in leds_on
+   P4OUT &= ~( leds_on & 0x000f);                  // switch off the leds marked '0' in leds_on
 }
 
 void    leds_increment() {
    uint8_t leds_on;
    // get LED state
-   leds_on  = (~P5OUT & 0x70) >> 4;
+   leds_on  = (~P4OUT & 0x000f);
    // modify LED state
    if (leds_on==0) {                             // if no LEDs on, switch on one
-      leds_on = 0x01;
+      leds_on = 0x0001;
    } else {
       leds_on += 1;
    }
    // apply updated LED state
    leds_on <<= 4;                                // send back to position 4
-   P5OUT |=  (~leds_on & 0x70);                  // switch on the leds marked '1' in leds_on
-   P5OUT &= ~( leds_on & 0x70);                  // switch off the leds marked '0' in leds_on
+   P4OUT |=  (~leds_on & (0x000f));                  // switch on the leds marked '1' in leds_on
+   P4OUT &= ~( leds_on & 0x000f);                  // switch off the leds marked '0' in leds_on
 }
 
 //=========================== private =========================================
