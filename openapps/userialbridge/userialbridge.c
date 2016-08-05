2fb945cc7c3986f220e239d268ec4597df79368a
Thomas Watteyne <twatteyne@gmail.com>
2016-08-05 13:45:58 -0700

    FW-525. Cleaned userialbridge app.

--- openapps/userialbridge/userialbridge.c
+++ openapps/userialbridge/userialbridge.c
@@ -14,8 +14,8 @@
 userialbridge_vars_t userialbridge_vars;
 
 static const uint8_t userialbridge_dst_addr[]   = {
-   0xbb, 0xbb, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01
+    0xbb, 0xbb, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01
 }; 
 
 //=========================== prototypes ======================================
@@ -37,66 +37,59 @@ void userialbridge_init() {
 }
 
 void userialbridge_sendDone(OpenQueueEntry_t* msg, owerror_t error) {
-   openqueue_freePacketBuffer(msg);
+    openqueue_freePacketBuffer(msg);
 }
 
 void userialbridge_receive(OpenQueueEntry_t* pkt) {
-   
-   openqueue_freePacketBuffer(pkt);
-   
-   openserial_printError(
-      COMPONENT_USERIALBRIDGE,
-      ERR_RCVD_ECHO_REPLY,
-      (errorparameter_t)0,
-      (errorparameter_t)0
-   );
+    openqueue_freePacketBuffer(pkt);
 }
 
 //=========================== private =========================================
 
 void userialbridge_triggerData(void) {
     
+    // store payload to send
     userialbridge_vars.txbufLen = openserial_getInputBuffer(
         &userialbridge_vars.txbuf[0],
         USERIALBRIDGE_MAXPAYLEN
     );
     
+    // push task
     scheduler_push_task(userialbridge_task_cb,TASKPRIO_COAP);
 }
 
-void userialbridge_task_cb() {
-   OpenQueueEntry_t*    pkt;
-   uint8_t              asnArray[5];
-   
-   // don't run if not synch
-   if (ieee154e_isSynch() == FALSE) return;
-   
-   // if you get here, send a packet
-   
-   // get a free packet buffer
-   pkt = openqueue_getFreePacketBuffer(COMPONENT_USERIALBRIDGE);
-   if (pkt==NULL) {
-      openserial_printError(
-         COMPONENT_USERIALBRIDGE,
-         ERR_NO_FREE_PACKET_BUFFER,
-         (errorparameter_t)0,
-         (errorparameter_t)0
-      );
-      return;
-   }
-   
-   pkt->owner                         = COMPONENT_USERIALBRIDGE;
-   pkt->creator                       = COMPONENT_USERIALBRIDGE;
-   pkt->l4_protocol                   = IANA_UDP;
-   pkt->l4_destination_port           = WKP_UDP_SERIALBRIDGE;
-   pkt->l4_sourcePortORicmpv6Type     = WKP_UDP_SERIALBRIDGE;
-   pkt->l3_destinationAdd.type        = ADDR_128B;
-   memcpy(&pkt->l3_destinationAdd.addr_128b[0],userialbridge_dst_addr,16);
-   
-   packetfunctions_reserveHeaderSize(pkt,userialbridge_vars.txbufLen);
-   memcpy(&pkt->payload[0],&userialbridge_vars.txbuf[0],userialbridge_vars.txbufLen);
-   
-   if ((openudp_send(pkt))==E_FAIL) {
-      openqueue_freePacketBuffer(pkt);
-   }
+void userialbridge_task_cb(void) {
+    OpenQueueEntry_t*    pkt;
+    
+    // don't run if not synch
+    if (ieee154e_isSynch() == FALSE) return;
+    
+    // if you get here, send a packet
+    
+    // get a free packet buffer
+    pkt = openqueue_getFreePacketBuffer(COMPONENT_USERIALBRIDGE);
+    if (pkt==NULL) {
+        openserial_printError(
+            COMPONENT_USERIALBRIDGE,
+            ERR_NO_FREE_PACKET_BUFFER,
+            (errorparameter_t)0,
+            (errorparameter_t)0
+        );
+        return;
+    }
+    
+    pkt->owner                         = COMPONENT_USERIALBRIDGE;
+    pkt->creator                       = COMPONENT_USERIALBRIDGE;
+    pkt->l4_protocol                   = IANA_UDP;
+    pkt->l4_destination_port           = WKP_UDP_SERIALBRIDGE;
+    pkt->l4_sourcePortORicmpv6Type     = WKP_UDP_SERIALBRIDGE;
+    pkt->l3_destinationAdd.type        = ADDR_128B;
+    memcpy(&pkt->l3_destinationAdd.addr_128b[0],userialbridge_dst_addr,16);
+    
+    packetfunctions_reserveHeaderSize(pkt,userialbridge_vars.txbufLen);
+    memcpy(&pkt->payload[0],&userialbridge_vars.txbuf[0],userialbridge_vars.txbufLen);
+    
+    if ((openudp_send(pkt))==E_FAIL) {
+        openqueue_freePacketBuffer(pkt);
+    }
 }
