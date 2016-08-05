ac74d89fdf277a9fa03abdc3d69ab52dde6b52cd
Thomas Watteyne <twatteyne@gmail.com>
2016-08-05 14:24:02 -0700

    [pendulum] 5 slot slotframe. 2 serialRX slots.

--- openstack/02b-MAChigh/schedule.h
+++ openstack/02b-MAChigh/schedule.h
@@ -17,7 +17,7 @@
 
 The superframe repears over time and can be arbitrarly long.
 */
-#define SLOTFRAME_LENGTH    11 //should be 101
+#define SLOTFRAME_LENGTH     5 //should be 101
 
 //draft-ietf-6tisch-minimal-06
 #define SCHEDULE_MINIMAL_6TISCH_ACTIVE_CELLS                      1
@@ -26,7 +26,7 @@ The superframe repears over time and can be arbitrarly long.
 #define SCHEDULE_MINIMAL_6TISCH_DEFAULT_SLOTFRAME_HANDLE          1 //id of slotframe
 #define SCHEDULE_MINIMAL_6TISCH_DEFAULT_SLOTFRAME_NUMBER          1 //1 slotframe by default.
 
-#define NUMSERIALRX          3
+#define NUMSERIALRX          2
 
 /*
   NUMSLOTSOFF is the max number of cells that the mote can add into schedule, 
@@ -39,7 +39,7 @@ The superframe repears over time and can be arbitrarly long.
   for seiral port to tranmit data to dagroot. 
 */
 
-#define NUMSLOTSOFF          5
+#define NUMSLOTSOFF          1
 
 /**
 \brief Maximum number of active slots in a superframe.
