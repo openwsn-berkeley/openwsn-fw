4f93728d8e212fb5c5d22f84cf83034f965adb30
changtengfei <tengfei.chang@gmail.com>
2016-08-09 18:08:14 +0200

    FW-526. update debugpins.c file.

--- bsp/boards/scum/debugpins.c
+++ bsp/boards/scum/debugpins.c
@@ -2,8 +2,6 @@
 \brief SCuM-specific definition of the "debugpins" bsp module.
 
 To be implemented after issue: SCUM-25
-(https://openwsn.atlassian.net/browse/SCUM-25) 
-is solved.
 
 \author Tengfei Chang <tengfei.chang@inria.fr>, August 2016.
 */
@@ -20,74 +18,77 @@ is solved.
 //=========================== public ==========================================
 
 void debugpins_init() {
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_frame_toggle() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
+
 void debugpins_frame_clr() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
-void debugpins_frame_set() {
 
+void debugpins_frame_set() {
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_slot_toggle() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_slot_clr() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_slot_set() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_fsm_toggle() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_fsm_clr() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_fsm_set() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_task_toggle() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_task_clr() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_task_set() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_isr_toggle() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_isr_clr() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_isr_set() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_radio_toggle() {
-
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_radio_clr() {
-    
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
 
 void debugpins_radio_set() {
-    
+    // empty for now, see https://openwsn.atlassian.net/browse/SCUM-25
 }
