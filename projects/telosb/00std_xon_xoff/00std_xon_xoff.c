773bb60b19e3abf79a040041cc2c1e7add9f77de
Thomas Watteyne <twatteyne@gmail.com>
2016-08-26 17:52:17 +0200

    FW-538. Works at 6900 baud.

--- projects/telosb/00std_xon_xoff/00std_xon_xoff.c
+++ projects/telosb/00std_xon_xoff/00std_xon_xoff.c
@@ -10,68 +10,108 @@ red LED:   successive RX'ed bytes aren't in sequence
 #define XOFF 0x13
 #define XON  0x11
 
+uint8_t fSendNextByte = 0;
+uint8_t rxByte;
+uint8_t lastRxByte    = 0xff;
+uint8_t rxIsEnabled   = 0;
+uint8_t fChangeState  = 0;
+uint8_t txByte        = 0;
+
 /**
 \brief The program starts executing here.
 */
 int main(void) {
-   uint8_t txByte;
-   // watchdog
-   WDTCTL          =  WDTPW + WDTHOLD;           // disable watchdog timer
-   
-   // clocking
-   DCOCTL          =  DCO0 | DCO1 | DCO2;        // MCLK at 8MHz
-   BCSCTL1         =  RSEL0 | RSEL1 | RSEL2;     // MCLK at 8MHz
-   
-   // LEDs
-   P5DIR          |=  0x70;                      // P5DIR = 0bx111xxxx for LEDs
-   P5OUT          |=  0x70;                      // P5OUT = 0bx111xxxx, all LEDs off
-   
-   // UART
-   P3SEL           =  0xC0;                      // P3.6,7 = UART1TX/RX
-   ME2            |=  UTXE1 + URXE1;             // enable UART1 TX/RX
-   UCTL1          |=  CHAR;                      // 8-bit character
-   UTCTL1         |=  SSEL1;                     // clocking from SMCLK
-   UBR01           =  41;                        // 4.8MHz/115200 - 41.66
-   UBR11           =  0x00;                      //
-   UMCTL1          =  0x4A;                      // modulation
-   UCTL1          &= ~SWRST;                     // clear UART1 reset bit
-   IE2            |=  URXIE1;                    // enable UART1 RX interrupt
-   
-   // TimerA
-   TACCTL0         =  CCIE;                      // capture/compare interrupt enable
-   TACCR0          =  16000;                     // 16000@32kHz ~ 500ms
-   TACTL           =  MC_1+TASSEL_1;             // up mode, using ACLK
-   
-   __bis_SR_register(LPM0_bits + GIE);           // sleep, leave interrupts and ACLK on
-   
-   /*
-   // send bytes over UART in order
-   txByte = 0;
-   while (1) {
-       U1TXBUF     =  ++txByte;
-   }
-   */
+
+    // watchdog
+    WDTCTL          =  WDTPW + WDTHOLD;          // disable watchdog timer
+
+    // clocking
+    DCOCTL          =  DCO0 | DCO1 | DCO2;       // MCLK at 8MHz
+    BCSCTL1         =  RSEL0 | RSEL1 | RSEL2;    // MCLK at 8MHz
+
+    // LEDs
+    P5DIR          |=  0x70;                     // P5DIR = 0bx111xxxx for LEDs
+    P5OUT          |=  0x70;                     // P5OUT = 0bx111xxxx, all LEDs off
+
+    // UART
+    P3SEL           =  0xc0;                     // P3.6,7 = UART1TX/RX
+
+    UCTL1           =  SWRST;                    // hold UART1 module in reset
+    UCTL1          |=  CHAR;                     // 8-bit character
+    
+    UTCTL1         |=  SSEL0;                    // clocking from SMCLK
+    UBR01           =  0x03;                     // 32768/9600 = 3.41
+    UBR11           =  0x00;                     //
+    UMCTL1          =  0x4A;                     // modulation
+    
+    /*
+    UTCTL1         |=  SSEL1;                    // clocking from SMCLK
+    UBR01           =  41;                       // 4.8MHz/115200 - 41.66
+    UBR11           =  0x00;                     //
+    UMCTL1          =  0x4A;                     // modulation
+    */
+
+    ME2            |=  UTXE1 + URXE1;            // enable UART1 TX/RX
+    UCTL1          &= ~SWRST;                    // clear UART1 reset bit
+
+    IE2            |=  UTXIE1 | URXIE1;          // enable UART1 TX/RX interrupts
+
+    // TimerA
+    TACCTL0         =  CCIE;                     // capture/compare interrupt enable
+    TACCR0          =  16000;                    // 16000@32kHz ~ 500ms
+    TACTL           =  MC_1+TASSEL_1;            // up mode, using ACLK
+
+    __bis_SR_register(GIE);                      // enable interrupts
+
+    // send bytes over UART in order
+    txByte = 0;
+    while (1) {
+        
+        fSendNextByte   = 0;  
+      
+        if (fChangeState==0x01) {
+            if (rxIsEnabled==0x01) {
+                P5OUT       &= ~0x20;            // green LED ON (clear bit)
+                U1TXBUF      = XON;              // send XON
+            } else {
+                P5OUT       |=  0x20;            // green LED OFF (set bit)
+                U1TXBUF      = XOFF;             // send XOFF
+            }
+            fChangeState=0x00;
+        } else {
+            txByte++;
+            if (txByte==XON || txByte==XOFF) {
+                txByte++;
+            }
+            U1TXBUF      =  txByte;
+        }
+        
+        while (fSendNextByte==0) {
+            // wait
+        }
+    }
 }
 
-/**
-\brief This function is called when the the UART module has received a byte.
-*/
+#pragma vector = USART1TX_VECTOR
+__interrupt void USART1TX_ISR (void) {
+    IFG2          &= ~UTXIFG1;                   // clear UART TX interrupt
+    fSendNextByte  = 1;
+}
+
 #pragma vector = USART1RX_VECTOR
-__interrupt void USART1RX_ISR (void)
-{
-   U1TXBUF    =  U1RXBUF;                        // TX -> RXed character
-   P5OUT     ^=  0x10;                           // toggle LED
+__interrupt void USART1RX_ISR (void) {
+    IFG2          &= ~URXIFG1;                   // clear UART RX interrupt
+    
+    P5OUT          ^=  0x40;                     // toggle blue LED
+    rxByte          = U1RXBUF;
+    if (rxByte!=(uint8_t)(lastRxByte+1)) {
+        P5OUT       ^=  0x10;                    // toggle red LED
+    }
+    lastRxByte      = rxByte;
 }
 
-/**
-\brief This function is called when the TimerA interrupt fires.
-*/
 #pragma vector = TIMERA0_VECTOR
 __interrupt void TIMERA0_ISR (void) {
-   P5OUT     ^=  0x20;                           // toggle LED
-   if (P5OUT & 0x20) {
-      U1TXBUF = XON;                             // enable UART RX
-   } else {
-      U1TXBUF = XOFF;                            // disable UART RX
-   }
+    rxIsEnabled    ^= 0x01;
+    fChangeState    = 0x01;
 }
