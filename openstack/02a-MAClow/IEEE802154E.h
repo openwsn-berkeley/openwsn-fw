6cfc21b36545bfca1bcae476baafbdcf5e9ef674
changtengfei <tengfei.chang@gmail.com>
2016-09-18 23:59:58 +0200

    FW-548. speed up sending EB and extend the DESYNCTIMEOUT.

--- openstack/02a-MAClow/IEEE802154E.h
+++ openstack/02a-MAClow/IEEE802154E.h
@@ -27,9 +27,9 @@ static const uint8_t chTemplate_default[] = {
 #define TX_POWER                    31 // 1=-25dBm, 31=0dBm (max value)
 #define RESYNCHRONIZATIONGUARD       5 // in 32kHz ticks. min distance to the end of the slot to successfully synchronize
 #define US_PER_TICK                 30 // number of us per 32kHz clock tick
-#define EBPERIOD                     5 // in seconds: sending EB every 30 seconds
+#define EBPERIOD                     3 // in seconds: sending EB every 30 seconds
 #define MAXKAPERIOD               2000 // in slots: @15ms per slot -> ~30 seconds. Max value used by adaptive synchronization.
-#define DESYNCTIMEOUT             2333 // in slots: @15ms per slot -> ~35 seconds. A larger DESYNCTIMEOUT is needed if using a larger KATIMEOUT.
+#define DESYNCTIMEOUT             4000 // in slots: @15ms per slot -> ~35 seconds. A larger DESYNCTIMEOUT is needed if using a larger KATIMEOUT.
 #define LIMITLARGETIMECORRECTION     5 // threshold number of ticks to declare a timeCorrection "large"
 #define LENGTH_IEEE154_MAX         128 // max length of a valid radio packet  
 #define DUTY_CYCLE_WINDOW_LIMIT    (0xFFFFFFFF>>1) // limit of the dutycycle window
