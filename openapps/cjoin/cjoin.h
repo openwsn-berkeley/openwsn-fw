daec167d8ad5d371cf7f48f78a1a948bc07be9c1
Mališa Vučinić <malishav@gmail.com>
2017-04-19 14:35:29 +0200

    FW-651. Add initial implementation of CBOR parser for minimal security.

--- openapps/cjoin/cjoin.h
+++ openapps/cjoin/cjoin.h
@@ -15,6 +15,41 @@
 #define CJOIN_MAX_NUM_KEYS          2
 // max supported number of bytes in key id field of COSE_Key
 #define CJOIN_MAX_KEYID_LENGTH      2
+
+
+// CBOR additional info mask
+#define CBOR_ADDINFO_MASK              0x1F
+
+// max number of pairs in COSE symmetric key struct
+#define COSE_SYMKEY_MAXNUMPAIRS        6    
+
+// CBOR major types
+typedef enum {
+    CBOR_MAJORTYPE_UINT                 = 0,
+    CBOR_MAJORTYPE_NINT                 = 1,
+    CBOR_MAJORTYPE_BSTR                 = 2,
+    CBOR_MAJORTYPE_TSTR                 = 3,
+    CBOR_MAJORTYPE_ARRAY                = 4,
+    CBOR_MAJORTYPE_MAP                  = 5,
+} cbor_majortype_t;
+
+// COSE key map labels
+typedef enum {
+    COSE_KEY_LABEL_KTY                  = 1,
+    COSE_KEY_LABEL_KID                  = 2,
+    COSE_KEY_LABEL_ALG                  = 3,
+    COSE_KEY_LABEL_KEYOPS               = 4,
+    COSE_KEY_LABEL_BASEIV               = 5,
+    COSE_KEY_LABEL_K                    = 32, // -1
+} cose_key_label_t;
+
+// COSE key type values
+typedef enum {
+    COSE_KEY_VALUE_OKP                  = 1,
+    COSE_KEY_VALUE_EC2                  = 2,
+    COSE_KEY_VALUE_SYMMETRIC            = 4,
+} cose_key_value_t;
+
 //=========================== typedef =========================================
 
 typedef struct {
@@ -23,7 +58,7 @@ typedef struct {
 } short_address_t;
 
 typedef struct {
-    uint8_t                 kty;
+    cose_key_value_t        kty;
     uint8_t*                kid;
     uint8_t                 kid_len;
     uint8_t*                k;
@@ -35,10 +70,8 @@ typedef struct {
 } COSE_keyset_t;
 
 typedef struct {
-    uint8_t*                COSE_keyset;
-    uint8_t                 COSE_keyset_len;
-    uint8_t*                short_address;
-    uint8_t                 short_address_len;
+    COSE_keyset_t           keyset;
+    short_address_t         short_address;
 } join_response_t;
 
 typedef struct {
