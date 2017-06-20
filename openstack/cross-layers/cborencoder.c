d331ec687b7e53d28474d4659a604c4aca45d612
Mališa Vučinić <malishav@gmail.com>
2017-06-20 13:56:38 +0200

    FW-651. Rename opt-cbor.[ch] to cborencoder.[ch] and integrate with OpenWSN.

--- openstack/cross-layers/cborencoder.c
+++ openstack/cross-layers/cborencoder.c
@@ -1,20 +1,13 @@
+/**
+\brief CBOR encoder library.
 
-#include "opt-cbor.h"
-#include <string.h>
-
-#define DEBUG 0
-#if DEBUG
-#include <stdio.h>
-#define PRINTF(...) printf(__VA_ARGS__)
-#define PRINTF_HEX(data, len) 	oscoap_printf_hex(data, len)
-#else
-#define PRINTF(...)
-#define PRINTF_HEX(data, len)
-#endif
+\author Martin Gunnarsson <martin.gunnarsson@ri.se>
+*/
 
 
+#include "cborencoder.h"
 
-uint8_t OPT_CBOR_put_text(uint8_t **buffer, char *text, uint8_t text_len){
+uint8_t cborencoder_put_text(uint8_t **buffer, char *text, uint8_t text_len){
 	uint8_t ret = 0;
 
 	if(text_len > 23 ){
@@ -35,9 +28,8 @@ uint8_t OPT_CBOR_put_text(uint8_t **buffer, char *text, uint8_t text_len){
 	return ret;
 }
 
-uint8_t OPT_CBOR_put_array(uint8_t **buffer,uint8_t elements){
+uint8_t cborencoder_put_array(uint8_t **buffer,uint8_t elements){
 	if(elements > 15){
-		PRINTF("ERROR! in put array\n");
 		return 0;
 	}
 
@@ -46,7 +38,7 @@ uint8_t OPT_CBOR_put_array(uint8_t **buffer,uint8_t elements){
 	return 1;
 }
 
-uint8_t OPT_CBOR_put_bytes(uint8_t **buffer, uint8_t bytes_len, uint8_t *bytes){
+uint8_t cborencoder_put_bytes(uint8_t **buffer, uint8_t bytes_len, uint8_t *bytes){
 	uint8_t ret = 0;
 	if(bytes_len > 23){
 		**buffer = 0x58;
@@ -64,9 +56,8 @@ uint8_t OPT_CBOR_put_bytes(uint8_t **buffer, uint8_t bytes_len, uint8_t *bytes){
 	ret += bytes_len;
 	return ret;
 }
-uint8_t OPT_CBOR_put_map(uint8_t **buffer, uint8_t elements){
+uint8_t cborencoder_put_map(uint8_t **buffer, uint8_t elements){
 	if(elements > 15){
-		PRINTF("ERROR in put map\n");
 		return 0;
 	}
 	**buffer = (0xa0 | elements);
@@ -75,7 +66,7 @@ uint8_t OPT_CBOR_put_map(uint8_t **buffer, uint8_t elements){
 	return 1;
 }
 
-uint8_t OPT_CBOR_put_unsigned(uint8_t **buffer, uint8_t value){
+uint8_t cborencoder_put_unsigned(uint8_t **buffer, uint8_t value){
 	if(value > 0x17 ){
 			(**buffer) = (0x18);
 			(*buffer)++;
