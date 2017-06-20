a6c39b102073b4c997fc7d168efbb468a47d5652
changtengfei <tengfei.chang@gmail.com>
2017-06-20 18:24:35 +0200

    FW-655. fixed.

--- openapps/openapps.c
+++ openapps/openapps.c
@@ -8,16 +8,6 @@
 
 // CoAP
 #include "opencoap.h"
-#include "c6t.h"
-#include "cinfo.h"
-#include "cleds.h"
-#include "cexample.h"
-#include "cstorm.h"
-#include "cwellknown.h"
-#include "rrt.h"
-// UDP
-#include "uecho.h"
-#include "uinject.h"
 
 //=========================== variables =======================================
 
@@ -30,13 +20,4 @@
 void openapps_init(void) {
    //-- 04-TRAN
    opencoap_init();     // initialize before any of the CoAP applications
-
-   // CoAP
-   c6t_init();
-   cinfo_init();
-   //cexample_init();
-   cleds__init();
-   cstorm_init();
-   cwellknown_init();
-   rrt_init();
 }
