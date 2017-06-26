81bf7154ccf4b900582f5578d98f939f345645f0
Fabrice Theoleyre <theoleyre@unistra.fr>
2017-06-26 14:24:57 +0200

    no warning then inserting a task in the scheduler

--- kernel/openos/scheduler.c
+++ kernel/openos/scheduler.c
@@ -47,14 +47,6 @@ void scheduler_start() {
          // execute the current task
          pThisTask->cb();
 
-         openserial_printInfo(
-                COMPONENT_OPENWSN,
-                           ERR_SCHEDULER,
-                           (errorparameter_t)scheduler_dbg.numTasksCur,
-                           (errorparameter_t)pThisTask->prio
-                     );
-
-
          // free up this task container
          pThisTask->cb            = NULL;
          pThisTask->prio          = TASKPRIO_NONE;
