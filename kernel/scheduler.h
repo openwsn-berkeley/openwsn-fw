07a2160845d3046bd01d846e8d8bd7941202993c
Diogo Guerra <dy090.guerra@gmail.com>
2017-08-26 23:41:19 +0100

    [FIX] ADD task_prio_t definitions used by freertos

--- kernel/scheduler.h
+++ kernel/scheduler.h
@@ -22,7 +22,7 @@ typedef enum {
    TASKPRIO_RPL                   = 0x04,
    TASKPRIO_TCP_TIMEOUT           = 0x05,
    TASKPRIO_COAP                  = 0x06,
-   TASKPRIO_ADAPTIVE_SYNC         = 0x07, 
+   TASKPRIO_ADAPTIVE_SYNC         = 0x07,
    TASKPRIO_SF0                   = 0x08,
    // tasks trigger by other interrupts
    TASKPRIO_BUTTON                = 0x09,
@@ -30,6 +30,27 @@ typedef enum {
    TASKPRIO_SNIFFER               = 0x0b,
    TASKPRIO_MAX                   = 0x0c,
 } task_prio_t;
+
+
+// THIS DEFINES PRIORTIES USED WITH FREERTOS
+
+// tasks trigger by the stack rx
+#define TASKPRIO_STACK_LOWMAC (task_prio_t) 0x01
+#define TASKPRIO_STACK_HIGHMAC (task_prio_t) 0x02
+#define TASKPRIO_STACK_6TOP (task_prio_t) 0x03
+#define TASKPRIO_STACK_IP (task_prio_t) 0x04
+#define TASKPRIO_STACK_ROUTING (task_prio_t) 0x05
+#define TASKPRIO_STACK_TRANSPORT (task_prio_t) 0x06
+// tasks going up the stack - sendone and timers
+#define TASKPRIO_SENDDONE_TIMERS_MAC (task_prio_t) 0x07
+#define TASKPRIO_SENDDONE_TIMERS_6TOP (task_prio_t) 0x08
+#define TASKPRIO_SENDDONE_TIMERS_IP (task_prio_t) 0x09
+#define TASKPRIO_SENDDONE_TIMERS_ROUTING (task_prio_t) 0x0A
+#define TASKPRIO_SENDDONE_TIMERS_TRANSPORT (task_prio_t) 0x0
+//app tasks - down the stack
+#define TASKPRIO_APP_HIGH (task_prio_t) 0x0C
+#define TASKPRIO_APP_MED (task_prio_t) 0x0D
+#define TASKPRIO_APP_LOW (task_prio_t) 0x0E
 
 
 #define TASK_LIST_DEPTH           10
