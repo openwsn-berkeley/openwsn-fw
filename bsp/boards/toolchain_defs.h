86fc9e4bcbebf76ecaaca63437aade375f91b7cd
Diogo Guerra <dy090.guerra@gmail.com>
2017-08-27 03:10:02 +0100

    ...

--- bsp/boards/toolchain_defs.h
+++ bsp/boards/toolchain_defs.h
@@ -29,7 +29,7 @@
       #if defined(__GNUC__) && (__GNUC__==4)  && (__GNUC_MINOR__<=5) && defined(__MSP430__)
          // mspgcc <4.5.x
          #define              ISR(v) interrupt (v ## _VECTOR) v ## _ISR(void)
-      #elif
+      #else
 
 	#define	__PRAGMA__(x) _Pragma(#x)
 	#define	ISR(v) __PRAGMA__(vector=v ##_VECTOR) __interrupt void v ##_ISR(void)
