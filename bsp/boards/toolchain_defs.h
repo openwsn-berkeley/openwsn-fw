2bed92c9bafda6b27ec2df203b2f3a55d32631ae
Diogo Guerra <dy090.guerra@gmail.com>
2017-08-27 02:28:37 +0100

    [FIX] duplicated ISR declaration

--- bsp/boards/toolchain_defs.h
+++ bsp/boards/toolchain_defs.h
@@ -25,8 +25,8 @@
     #define __MSPGCC_MAYBE_C16__
       #if defined(__GNUC__) && (__GNUC__==4)  && (__GNUC_MINOR__<=5) && defined(__MSP430__)
          // mspgcc <4.5.x
-         #include <signal.h>
-         #define              ISR(v) interrupt (v ## _VECTOR) v ## _ISR(void)
+         #include <legacymsp430.h>
+         //#define              ISR(v) interrupt (v ## _VECTOR) v ## _ISR(void)
       #endif
    #endif /* -mc20 */
    #define        ISR(v)   void __attribute__((__interrupt__ (a##_VECTOR))) __MSPGCC_MAYBE_C16__ (v##_ISR)(void)
