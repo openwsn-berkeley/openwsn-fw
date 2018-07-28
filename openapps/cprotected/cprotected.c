463ebf04fbd2ca14c5b91b3951bff286790706c9
Mališa Vučinić <malishav@gmail.com>
2018-07-29 01:59:54 +0200

    FW-767. ACE error handling: returning AS info
    When a protected resource is accessed over unprotected transport (i.e.
    without OSCORE), RS returns 4.01 Unauthorized error with payload
    containing the AS Info CBOR object. This object encodes the URI of the
    AS, where access token can be requested.

--- openapps/cprotected/cprotected.c
+++ openapps/cprotected/cprotected.c
@@ -11,6 +11,8 @@
 #include "openrandom.h"
 #include "board.h"
 #include "idmanager.h"
+#include "icmpv6rpl.h"
+#include "cborencoder.h"
 
 //=========================== defines =========================================
 
@@ -33,6 +35,8 @@ void cprotected_sendDone(
         OpenQueueEntry_t* msg,
         owerror_t error
 );
+
+uint8_t cprotected_get_as_info(uint8_t* buf);
 
 //=========================== public ==========================================
 
@@ -77,30 +81,74 @@ owerror_t cprotected_receive(
         coap_option_iht*  coap_outgoingOptions,
         uint8_t*          coap_outgoingOptionsLen
 ) {
-   owerror_t outcome;
-   const uint8_t text[] = " __ _  ______\n(_ |_)/ \\|(_\n__)|  \\_/|__)";
+    owerror_t outcome;
+    uint8_t i;
+    bool found;
+    uint8_t asInfo[60];    // buffer holding CBOR serialization of AS info object
+    uint8_t asInfoLen;
+    const uint8_t text[] = " __ _  ______\n(_ |_)/ \\|(_\n__)|  \\_/|__)";
 
-   switch (coap_header->Code) {
-      case COAP_CODE_REQ_GET:
-         //=== reset packet payload (we will reuse this packetBuffer)
-         msg->payload                     = &(msg->packet[127]);
-         msg->length                      = 0;
+    i = 0;
+    asInfoLen = 0;
+    found = FALSE;
 
-        // copy payload
-         packetfunctions_reserveHeaderSize(msg,sizeof(text)-1);
-         memcpy(&msg->payload[0],&text,sizeof(text)-1);
+    while (coap_incomingOptions[i].type != COAP_OPTION_NONE) {
+        if (coap_incomingOptions[i].type == COAP_OPTION_NUM_OBJECTSECURITY) {
+            found = TRUE;
+        }
+        i++;
+    }
 
-         //=== prepare  CoAP response
+    // if received over unprotected channel (no OSCORE), return unauthorized
+    if (!found) {
+        //=== reset packet payload (we will reuse this packetBuffer)
+        msg->payload                     = &(msg->packet[127]);
+        msg->length                      = 0;
 
-         // set the CoAP header
-         coap_header->Code                = COAP_CODE_RESP_CONTENT;
+        //=== prepare  CoAP response
+        asInfoLen = cprotected_get_as_info(asInfo);
 
-         outcome                          = E_SUCCESS;
-         break;
-      default:
-         // return an error message
-         outcome = E_FAIL;
-   }
+        if (asInfoLen) {
+            packetfunctions_reserveHeaderSize(msg, asInfoLen);
+            memcpy(&msg->payload[0],asInfo, asInfoLen);
+
+            cprotected_vars.medType = COAP_MEDTYPE_APPCBOR;
+            coap_outgoingOptions[0].type = COAP_OPTION_NUM_CONTENTFORMAT;
+            coap_outgoingOptions[0].length = 1;
+            coap_outgoingOptions[0].pValue = &cprotected_vars.medType;
+
+            *coap_outgoingOptionsLen = 1;
+        }
+
+        // set the CoAP header
+        coap_header->Code                = COAP_CODE_RESP_UNAUTHORIZED;
+
+        return E_SUCCESS;
+    }
+
+    // else: message was received over an OSCORE channel
+
+    switch (coap_header->Code) {
+        case COAP_CODE_REQ_GET:
+            //=== reset packet payload (we will reuse this packetBuffer)
+            msg->payload                     = &(msg->packet[127]);
+            msg->length                      = 0;
+
+            // copy payload
+            packetfunctions_reserveHeaderSize(msg,sizeof(text)-1);
+            memcpy(&msg->payload[0],&text,sizeof(text)-1);
+
+            //=== prepare  CoAP response
+
+            // set the CoAP header
+            coap_header->Code                = COAP_CODE_RESP_CONTENT;
+
+            outcome                          = E_SUCCESS;
+            break;
+        default:
+            // return an error message
+            outcome = E_FAIL;
+    }
 
    return outcome;
 }
@@ -114,3 +162,66 @@ owerror_t cprotected_receive(
 void cprotected_sendDone(OpenQueueEntry_t* msg, owerror_t error) {
    openqueue_freePacketBuffer(msg);
 }
+
+uint8_t cprotected_get_as_info(uint8_t* buf) {
+    static char hex [] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ,'a', 'b', 'c', 'd', 'e', 'f' };
+    open_addr_t JRCaddress;
+    uint8_t i;
+    uint8_t len;
+    uint16_t dig16b;
+    uint8_t uri[39];  // worst case string encoding of IPv6 address
+    uint8_t uriLen;
+    const uint8_t scheme[] = "coap://";
+    const uint8_t token[] = "/token";
+
+    i = 0;
+    len = 0;
+    uriLen = 0;
+
+    // add JRC's IPv6 address to payload for the Client to obtain an access token
+    JRCaddress.type = ADDR_128B;
+    if (icmpv6rpl_getRPLDODAGid(JRCaddress.addr_128b) == E_SUCCESS) {
+
+        // create an absolute URI string
+
+        // Step 1. Add scheme "coap://
+        memcpy(&uri[uriLen], scheme, sizeof(scheme)-1);
+        uriLen += sizeof(scheme)-1;
+
+        // Step 2. Add IPv6 address within []
+        uri[uriLen++] = '[';
+        // convert IPv6 byte string to hex string
+        for (i = 0; i < 16; i = i+2) {
+
+            dig16b = packetfunctions_ntohs(&JRCaddress.addr_128b[i]);
+
+            if (i != 0) {
+                uri[uriLen++] = ':';
+            }
+
+            if (dig16b) {
+                uri[uriLen++] = hex[(0xf0 & JRCaddress.addr_128b[i]) >> 4];
+                uri[uriLen++] = hex[(0x0f & JRCaddress.addr_128b[i]) >> 0];
+                uri[uriLen++] = hex[(0xf0 & JRCaddress.addr_128b[i+1]) >> 4];
+                uri[uriLen++] = hex[(0x0f & JRCaddress.addr_128b[i+1]) >> 0];
+            } else {
+                uri[uriLen++] = '0';
+            }
+        }
+        uri[uriLen++] = ']';
+
+        // Step 3. Add URI-Path of the /token resource at AS
+        memcpy(&uri[uriLen], token, sizeof(token)-1);
+        uriLen += sizeof(token)-1;
+
+        // Now, create CBOR AS Info object
+        len += cborencoder_put_map(&buf[len], 1);
+        len += cborencoder_put_unsigned(&buf[len], (uint8_t) AS_INFO_LABEL_AS);
+        len += cborencoder_put_text(&buf[len], (char *) uri, uriLen);
+
+        return len;
+    }
+
+    return 0;
+}
+
--- openapps/cprotected/cprotected.h
+++ openapps/cprotected/cprotected.h
@@ -14,10 +14,16 @@
 
 //=========================== typedef =========================================
 
+typedef enum {
+   AS_INFO_LABEL_AS      = 0,
+   AS_INFO_LABEL_NONCE   = 5,
+} as_info_label_t;
+
 //=========================== variables =======================================
 
 typedef struct {
    coap_resource_desc_t desc;
+   uint8_t medType;
 } cprotected_vars_t;
 
 //=========================== prototypes ======================================
