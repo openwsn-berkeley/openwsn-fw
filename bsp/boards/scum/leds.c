6747c7e2972079cd41e53d9d75561010ca3066b8
changtengfei <tengfei.chang@gmail.com>
2018-08-06 14:18:55 -0700

    FW-770. use gpio_4-7 as leds since gpio 1 and 2 are used as radio data_in and data_clk_in pin.

--- bsp/boards/scum/leds.c
+++ bsp/boards/scum/leds.c
@@ -17,108 +17,108 @@
 //=========================== public ==========================================
 
 void leds_init(void) {
-    GPIO_REG__OUTPUT    &= ~0x0F;    // GPIO_REG__OUTPUT = 0bxxxx0000, all LEDs off
+    GPIO_REG__OUTPUT    &= ~0xF0;    // GPIO_REG__OUTPUT = 0bxxxx0000, all LEDs off
 }
 
 // 0 <H17>
 void    leds_error_on(void) {
-    GPIO_REG__OUTPUT    |=  0x01;
+    GPIO_REG__OUTPUT    |=  0x10;
 }
 void    leds_error_off(void) {
-    GPIO_REG__OUTPUT    &= ~0x01;
+    GPIO_REG__OUTPUT    &= ~0x10;
 }
 void    leds_error_toggle(void) {
-    GPIO_REG__OUTPUT    ^=  0x01;
+    GPIO_REG__OUTPUT    ^=  0x10;
 }
 uint8_t leds_error_isOn(void) {
-    return (uint8_t)(GPIO_REG__OUTPUT & 0x01);
+    return (uint8_t)(GPIO_REG__OUTPUT & 0x10);
 }
 
 // 1 <K15>
 void    leds_radio_on(void) {
-    GPIO_REG__OUTPUT    |=  0x02;
+    GPIO_REG__OUTPUT    |=  0x20;
 }
 void    leds_radio_off(void) {
-    GPIO_REG__OUTPUT    &= ~0x02;
+    GPIO_REG__OUTPUT    &= ~0x20;
 }
 void    leds_radio_toggle(void) {
-    GPIO_REG__OUTPUT    ^=  0x02;
+    GPIO_REG__OUTPUT    ^=  0x20;
 }
 uint8_t leds_radio_isOn(void) {
-    return (uint8_t)(GPIO_REG__OUTPUT & 0x02)>>1;
+    return (uint8_t)(GPIO_REG__OUTPUT & 0x20)>>1;
 }
 
 // 2 <J13>
 void    leds_sync_on(void) {
-    GPIO_REG__OUTPUT    |=  0x04;
+    GPIO_REG__OUTPUT    |=  0x40;
 }
 void    leds_sync_off(void) {
-    GPIO_REG__OUTPUT    &= ~0x04;
+    GPIO_REG__OUTPUT    &= ~0x40;
 }
 void    leds_sync_toggle(void) {
-    GPIO_REG__OUTPUT    ^=  0x04;
+    GPIO_REG__OUTPUT    ^=  0x40;
 }
 uint8_t leds_sync_isOn(void) {
-    return (uint8_t)(GPIO_REG__OUTPUT & 0x04)>>2;
+    return (uint8_t)(GPIO_REG__OUTPUT & 0x40)>>2;
 }
 
 // 3 <N14>
 void    leds_debug_on(void) {
-    GPIO_REG__OUTPUT    |=  0x08;
+    GPIO_REG__OUTPUT    |=  0x80;
 }
 void    leds_debug_off(void) {
-    GPIO_REG__OUTPUT    &= ~0x08;
+    GPIO_REG__OUTPUT    &= ~0x80;
 }
 void    leds_debug_toggle(void) {
-    GPIO_REG__OUTPUT    ^=  0x08;
+    GPIO_REG__OUTPUT    ^=  0x80;
 }
 uint8_t leds_debug_isOn(void) {
-    return (uint8_t)(GPIO_REG__OUTPUT & 0x08)>>3;
+    return (uint8_t)(GPIO_REG__OUTPUT & 0x80)>>3;
 }
 
 void leds_all_on(void) {
-    GPIO_REG__OUTPUT    |=  0x0F;
+    GPIO_REG__OUTPUT    |=  0xF0;
 }
 void leds_all_off(void) {
-    GPIO_REG__OUTPUT    &= ~0x0F;
+    GPIO_REG__OUTPUT    &= ~0xF0;
 }
 void leds_all_toggle(void) {
-    GPIO_REG__OUTPUT    ^=  0x0F;
+    GPIO_REG__OUTPUT    ^=  0xF0;
 }
 
 void leds_error_blink(void) {
     uint8_t i;
     volatile uint16_t delay;
     // turn all LEDs off
-    GPIO_REG__OUTPUT    &= ~0x0F;
+    GPIO_REG__OUTPUT    &= ~0xF0;
      
     // blink error LED for ~10s
     for (i=0;i<80;i++) {
-        GPIO_REG__OUTPUT    ^=  0x01;
+        GPIO_REG__OUTPUT    ^=  0x10;
         for (delay=0xffff;delay>0;delay--);
     }
 }
 
 void leds_circular_shift(void) {
     uint8_t temp_leds;
-    if ((GPIO_REG__OUTPUT & 0x0F)==0) {         // if no LEDs on, switch on first one
-        GPIO_REG__OUTPUT    |= 0x01;
+    if ((GPIO_REG__OUTPUT & 0xF0)==0) {         // if no LEDs on, switch on first one
+        GPIO_REG__OUTPUT    |= 0x10;
         return;
     }
-    temp_leds = GPIO_REG__OUTPUT & 0x0F;        // retrieve current status of LEDs
+    temp_leds = GPIO_REG__OUTPUT & 0xF0;        // retrieve current status of LEDs
     temp_leds <<= 1;                            // shift by one position
     if ((temp_leds & 0x10)!=0) {
         temp_leds++;                            // handle overflow
     }
     GPIO_REG__OUTPUT    |= temp_leds;              // switch on the leds marked '1' in temp_leds
-    GPIO_REG__OUTPUT    &= ~(~temp_leds & 0x0F);   // switch off the leds marked '0' in temp_leds
+    GPIO_REG__OUTPUT    &= ~(~temp_leds & 0xF0);   // switch off the leds marked '0' in temp_leds
 }
 
 void leds_increment(void) {
    uint8_t led_counter;
-   led_counter           = ((GPIO_REG__OUTPUT & 0x0f)+1);
-   GPIO_REG__OUTPUT     &= ~0x0f;                   //all LEDs off
-   GPIO_REG__OUTPUT     |=  led_counter & 0x0f;     //LEDs on again
+   led_counter           = ((GPIO_REG__OUTPUT & 0xf0)+0x10);
+   GPIO_REG__OUTPUT     &= ~0xf0;                   //all LEDs off
+   GPIO_REG__OUTPUT     |=  led_counter & 0xf0;     //LEDs on again
 }
 
 //=========================== private =========================================
