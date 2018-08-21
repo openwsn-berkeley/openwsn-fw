4515cf109a5aafbc81eebd33f732671998d0f1bd
changtengfei <tengfei.chang@gmail.com>
2018-08-21 02:26:17 +0200

    SCUM-30. update startup and linker file according to ARM_Cortex_M0_DesignStart_Eval examples.

--- bsp/boards/scum/scum_startup.s
+++ bsp/boards/scum/scum_startup.s
@@ -1,38 +1,3 @@
-/* File: startup_ARMCM0.S
- * Purpose: startup file for Cortex-M0 devices. Should use with
- *   GCC for ARM Embedded Processors
- * Version: V2.01
- * Date: 12 June 2014
- *
- */
-/* Copyright (c) 2011 - 2014 ARM LIMITED
-
-   All rights reserved.
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are met:
-   - Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-   - Redistributions in binary form must reproduce the above copyright
-     notice, this list of conditions and the following disclaimer in the
-     documentation and/or other materials provided with the distribution.
-   - Neither the name of ARM nor the names of its contributors may be used
-     to endorse or promote products derived from this software without
-     specific prior written permission.
-   *
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-   ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS AND CONTRIBUTORS BE
-   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-   POSSIBILITY OF SUCH DAMAGE.
-   ---------------------------------------------------------------------------*/
-
-
     .syntax    unified
     .arch    armv6-m
 
@@ -69,7 +34,7 @@ __HeapLimit:
     .size    __HeapLimit, . - __HeapLimit
 
     .section .vectors
-    .align 2
+    .align 4
     .globl    __Vectors
 __Vectors:
     .long    __StackTop            /* Top of Stack */
@@ -109,10 +74,11 @@ __Vectors:
 
     .size    __Vectors, . - __Vectors
 
+/* Reset Handler */
     .text
     .thumb
     .thumb_func
-    .align    1
+    .align    2
     .globl    Reset_Handler
     .type    Reset_Handler, %function
 Reset_Handler:
@@ -123,30 +89,29 @@ Reset_Handler:
     ldr     r0, =0xc1
     str     r0, [r1]
 
-    .global main
-    b       main
+    ldr     r0,=main
+    blx     r0
 
-    .pool
     .size    Reset_Handler, . - Reset_Handler
-
-    .align    1
-    .thumb_func
-    .weak    Default_Handler
-    .type    Default_Handler, %function
-Default_Handler:
-    b    .
-    .size    Default_Handler, . - Default_Handler
-
+    
 /*    Macro to define default handlers. Default handler
  *    will be weak symbol and just dead loops. They can be
  *    overwritten by other handlers */
-    .macro    def_irq_handler    handler_name
+    .macro    def_default_handler    handler_name
+    .align 1
+    .thumb_func
     .weak    \handler_name
-    .set    \handler_name, Default_Handler
+    .type    \handler_name, %function
+\handler_name :
+    b    .
+    .size    \handler_name, . - \handler_name
     .endm
-
-    def_irq_handler    UART_Handler
-    def_irq_handler    RF_Handler
-    def_irq_handler    RFTIMER_Handler
+    
+/* System Exception Handlers */
+    
+/* IRQ Handlers */
+    def_default_handler    UART_Handler
+    def_default_handler    RF_Handler
+    def_default_handler    RFTIMER_Handler
     
     .end
