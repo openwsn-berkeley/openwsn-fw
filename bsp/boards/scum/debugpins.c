b2f16c8420fec18d71cda210907b690ca8230d33
changtengfei <tengfei.chang@gmail.com>
2018-08-27 18:26:18 +0200

    SCUM-30. update scum bsp driver according to the latest design.

--- bsp/boards/scum/debugpins.c
+++ bsp/boards/scum/debugpins.c
@@ -18,78 +18,82 @@ To be implemented after issue: SCUM-25
 //=========================== public ==========================================
 
 void debugpins_init(void) {
-    // GPIO pin 8 to 13 are used as debugpins, 
-    GPIO_REG__OUTPUT    &= ~0x3F00; // all PINS low at initial
+    // GPIO pin 13 to 16 are used as debugpins, 
+    GPIO_REG__OUTPUT    &= ~0xF000; // all PINS low at initial
 }
 
 void debugpins_frame_toggle(void) {
-    GPIO_REG__OUTPUT    ^=  0x0100;
-}
-
-void debugpins_frame_clr(void) {
-    GPIO_REG__OUTPUT    &= ~0x0100;
-}
-
-void debugpins_frame_set(void) {
-    GPIO_REG__OUTPUT    |=  0x0100;
-}
-
-void debugpins_slot_toggle(void) {
-    GPIO_REG__OUTPUT    ^=  0x0200;
-}
-
-void debugpins_slot_clr(void) {
-    GPIO_REG__OUTPUT    &= ~0x0200;
-}
-
-void debugpins_slot_set(void) {
-    GPIO_REG__OUTPUT    |=  0x0200;
-}
-
-void debugpins_fsm_toggle(void) {
-    GPIO_REG__OUTPUT    ^=  0x0400;
-}
-
-void debugpins_fsm_clr(void) {
-    GPIO_REG__OUTPUT    &= ~0x0400;
-}
-
-void debugpins_fsm_set(void) {
-    GPIO_REG__OUTPUT    |=  0x0400;
-}
-
-void debugpins_task_toggle(void) {
-    GPIO_REG__OUTPUT    ^=  0x0800;
-}
-
-void debugpins_task_clr(void) {
-    GPIO_REG__OUTPUT    &= ~0x0800;
-}
-
-void debugpins_task_set(void) {
-    GPIO_REG__OUTPUT    |=  0x0800;
-}
-
-void debugpins_isr_toggle(void) {
     GPIO_REG__OUTPUT    ^=  0x1000;
 }
 
-void debugpins_isr_clr(void) {
+void debugpins_frame_clr(void) {
     GPIO_REG__OUTPUT    &= ~0x1000;
 }
 
-void debugpins_isr_set(void) {
+void debugpins_frame_set(void) {
     GPIO_REG__OUTPUT    |=  0x1000;
 }
 
-void debugpins_radio_toggle(void) {
+void debugpins_slot_toggle(void) {
     GPIO_REG__OUTPUT    ^=  0x2000;
 }
 
-void debugpins_radio_clr(void) {
+void debugpins_slot_clr(void) {
     GPIO_REG__OUTPUT    &= ~0x2000;
 }
 
-void debugpins_radio_set(void) {
+void debugpins_slot_set(void) {
     GPIO_REG__OUTPUT    |=  0x2000;
+}
+
+void debugpins_fsm_toggle(void) {
+    GPIO_REG__OUTPUT    ^=  0x4000;
+}
+
+void debugpins_fsm_clr(void) {
+    GPIO_REG__OUTPUT    &= ~0x4000;
+}
+
+void debugpins_fsm_set(void) {
+    GPIO_REG__OUTPUT    |=  0x4000;
+}
+
+// no enough pin for debugpins task
+
+void debugpins_task_toggle(void) {
+    // GPIO_REG__OUTPUT    ^=  0x8000;
+}
+
+void debugpins_task_clr(void) {
+    // GPIO_REG__OUTPUT    &= ~0x8000;
+}
+
+void debugpins_task_set(void) {
+    // GPIO_REG__OUTPUT    |=  0x8000;
+}
+
+void debugpins_isr_toggle(void) {
+    GPIO_REG__OUTPUT    ^=  0x8000;
+}
+
+void debugpins_isr_clr(void) {
+    GPIO_REG__OUTPUT    &= ~0x8000;
+}
+
+void debugpins_isr_set(void) {
+    GPIO_REG__OUTPUT    |=  0x8000;
+}
+
+// no enough pin for debugpins radio
+
+void debugpins_radio_toggle(void) {
+    // GPIO_REG__OUTPUT    ^=  0x2000;
+}
+
+void debugpins_radio_clr(void) {
+    // GPIO_REG__OUTPUT    &= ~0x2000;
+}
+
+void debugpins_radio_set(void) {
+    // GPIO_REG__OUTPUT    |=  0x2000;
 }
