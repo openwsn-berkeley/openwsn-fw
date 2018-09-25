7de2f8121bfc306d38fe6ff22703ea17159bd89b
changtengfei <tengfei.chang@gmail.com>
2018-09-25 11:41:08 +0200

    FW-776. update radio driver.

--- bsp/chips/at86rf215/at86rf215.c
+++ bsp/chips/at86rf215/at86rf215.c
@@ -2,65 +2,65 @@
 \brief AT86RF215-specific library.
 
 \author Jonathan Munoz <jonathan.munoz@inria.fr>, August 2016.
+\author Xavi Vilajosana <xvilajosana@eecs.berkeley.edu>, December 2017.
+
 */
 
 #include "board.h"
-#include "radio.h"
 #include "at86rf215.h"
 #include "spi.h"
-#include "radiotimer.h"
 #include "debugpins.h"
 #include "leds.h"
+#include "radio.h"
 
 
 void at86rf215_spiStrobe(uint8_t strobe) {
-   uint8_t  spi_tx_buffer[3];
-   
-   spi_tx_buffer[0]     = ((FLAG_WRITE) | (uint8_t)(RG_RF09_CMD/256));
-   spi_tx_buffer[1]     = (uint8_t)(RG_RF09_CMD%256);
-   spi_tx_buffer[2]     = strobe;
+    uint8_t  spi_tx_buffer[3];
 
-   spi_txrx(
-      spi_tx_buffer,              // bufTx
-      sizeof(spi_tx_buffer),      // lenbufTx
-      SPI_FIRSTBYTE,              // returnType
-      NULL,                       // bufRx
-      1,                          // maxLenBufRx
-      SPI_FIRST,                  // isFirst
-      SPI_LAST                    // isLast
-   );
+    spi_tx_buffer[0]     = ((FLAG_WRITE) | (uint8_t)(RG_RF09_CMD/256));
+    spi_tx_buffer[1]     = (uint8_t)(RG_RF09_CMD%256);
+    spi_tx_buffer[2]     = strobe;
+
+    spi_txrx(
+        spi_tx_buffer,              // bufTx
+        sizeof(spi_tx_buffer),      // lenbufTx
+        SPI_FIRSTBYTE,              // returnType
+        NULL,                       // bufRx
+        1,                          // maxLenBufRx
+        SPI_FIRST,                  // isFirst
+        SPI_LAST                    // isLast
+    );
 }
 
 void at86rf215_spiWriteReg(uint16_t reg, uint8_t regValueToWrite) {
-     
-   uint8_t spi_rx_buffer[3];
-   uint8_t spi_tx_buffer[3];
-        
-   spi_tx_buffer[0]     = (FLAG_WRITE | (uint8_t)((reg)/256));
-   spi_tx_buffer[1]     = (uint8_t)((reg)%256);
-   spi_tx_buffer[2]     = regValueToWrite;
-      
-   spi_txrx(
-       spi_tx_buffer,              // bufTx
-       3,                          // lenbufTx
-       SPI_FIRSTBYTE,              // returnType
-       spi_rx_buffer,              // bufRx
-       3,                          // maxLenBufRx
-       SPI_FIRST,                  // isFirst
-       SPI_LAST                    // isLast
-   );
+
+    uint8_t spi_rx_buffer[3];
+    uint8_t spi_tx_buffer[3];
+
+    spi_tx_buffer[0]     = (FLAG_WRITE | (uint8_t)((reg)/256));
+    spi_tx_buffer[1]     = (uint8_t)((reg)%256);
+    spi_tx_buffer[2]     = regValueToWrite;
+
+    spi_txrx(
+        spi_tx_buffer,              // bufTx
+        3,                          // lenbufTx
+        SPI_FIRSTBYTE,              // returnType
+        spi_rx_buffer,              // bufRx
+        3,                          // maxLenBufRx
+        SPI_FIRST,                  // isFirst
+        SPI_LAST                    // isLast
+    );
 }
 
 uint8_t at86rf215_spiReadReg(uint16_t regAddr16) {
-  
+
     uint8_t              spi_tx_buffer[3];
     uint8_t              spi_rx_buffer[3];
-   
+
     spi_tx_buffer[0] = (FLAG_READ | (uint8_t)(regAddr16 >> 8));
     spi_tx_buffer[1] = (uint8_t)(regAddr16 & 0xFF);
-    spi_tx_buffer[2] = 0x00;   
+    spi_tx_buffer[2] = 0x00;
 
-   
     spi_txrx(
         spi_tx_buffer,              // bufTx
         sizeof(spi_tx_buffer),      // lenbufTx
@@ -70,18 +70,18 @@ uint8_t at86rf215_spiReadReg(uint16_t regAddr16) {
         SPI_FIRST,                  // isFirst
         SPI_LAST                    // isLast
     );
-    return spi_rx_buffer[2]; 
+    return spi_rx_buffer[2];
 }
 
 void at86rf215_spiWriteFifo(uint8_t* bufToWrite, uint16_t len) {
     uint8_t              spi_tx_buffer[4];
-   
-    // step 1. send packet length. 
+
+    // step 1. send packet length.
     spi_tx_buffer[0]     = (FLAG_WRITE | (uint8_t)(RG_BBC0_TXFLL >> 8));
     spi_tx_buffer[1]     = (uint8_t)(RG_BBC0_TXFLL & 0xFF);
     spi_tx_buffer[2]     = (uint8_t)(len & 0xFF);       // low byte of packet-length -> TXFLL
-    spi_tx_buffer[3]     = (uint8_t)(len >> 8) & 0x07;  // high byte of packet-length -> TXFLH   
-  
+    spi_tx_buffer[3]     = (uint8_t)(len >> 8) & 0x07;  // high byte of packet-length -> TXFLH
+
     spi_txrx(
         spi_tx_buffer,              // bufTx
         sizeof(spi_tx_buffer),      // lenbufTx
@@ -94,7 +94,7 @@ void at86rf215_spiWriteFifo(uint8_t* bufToWrite, uint16_t len) {
 
     spi_tx_buffer[0]     = (FLAG_WRITE | (uint8_t)(RG_BBC0_FBTXS >> 8));
     spi_tx_buffer[1]     = (uint8_t)(RG_BBC0_FBTXS& 0xFF);
-   
+
     // step 2. send the address to the Tx buffer.
     spi_txrx(
         spi_tx_buffer,             // bufTx
@@ -109,14 +109,14 @@ void at86rf215_spiWriteFifo(uint8_t* bufToWrite, uint16_t len) {
     // step 3. send the payload.
     spi_txrx(
         bufToWrite,                 // bufTx
-        len - LENGTH_CRC,           // lenbufTx
+        len - LENGTH_CRC,    // lenbufTx
         SPI_LASTBYTE,               // returnType
         NULL,                       // bufRx
         1,                          // maxLenBufRx
         SPI_NOTFIRST,               // isFirst
         SPI_LAST                    // isLast
     );
-    
+
 }
 
 void at86rf215_spiReadRxFifo( uint8_t* pBufRead, uint16_t* lenRead) {
@@ -124,12 +124,12 @@ void at86rf215_spiReadRxFifo( uint8_t* pBufRead, uint16_t* lenRead) {
     uint8_t spi_tx_buffer[4];
     uint8_t spi_rx_buffer[4];
     uint16_t length;
-     
+
     // step 1 - read packet length from RG_BBC0_RXFLL/_RXFLH:
     spi_tx_buffer[0] = (FLAG_READ | (uint8_t)(RG_BBC0_RXFLL >> 8));
     spi_tx_buffer[1] = (uint8_t)(RG_BBC0_RXFLL & 0xFF);
     spi_tx_buffer[2] = 0x00;
-    spi_tx_buffer[3] = 0x00;  
+    spi_tx_buffer[3] = 0x00;
 
     //read FIFO length
     spi_txrx(
@@ -143,10 +143,10 @@ void at86rf215_spiReadRxFifo( uint8_t* pBufRead, uint16_t* lenRead) {
     );
     length = (uint16_t)spi_rx_buffer[2]             // RXFLL
                     | ((uint16_t)(spi_rx_buffer[3] & 0x07) << 8);  // RXFLH
-        
+
     spi_tx_buffer[0]    = (FLAG_READ | (uint8_t)(BASE_ADDR_BBC0_FB0/256));
-    spi_tx_buffer[1]    = (uint8_t)(BASE_ADDR_BBC0_FB0%256);      
-    //read FIFO 
+    spi_tx_buffer[1]    = (uint8_t)(BASE_ADDR_BBC0_FB0%256);
+    //read FIFO
     spi_txrx(
         spi_tx_buffer,              // bufTx
         2,                          // size of the address
@@ -156,7 +156,7 @@ void at86rf215_spiReadRxFifo( uint8_t* pBufRead, uint16_t* lenRead) {
         SPI_FIRST,                  // isFirst
         SPI_NOTLAST                 // isLast
     );
-    
+
         spi_txrx(
         NULL,                        // bufTx
         length,                     // lenbufTx
@@ -165,14 +165,14 @@ void at86rf215_spiReadRxFifo( uint8_t* pBufRead, uint16_t* lenRead) {
         length,                     // maxLenBufRx
         SPI_NOTFIRST,               // isFirst
         SPI_LAST                    // isLast
-    );   
+    );
     *(lenRead) = length;
 }
 
 uint8_t at86rf215_status (void){
     uint8_t spi_tx_buffer[3];
     uint8_t spi_rx_buffer[3];
-    
+
     spi_tx_buffer[0] = (FLAG_READ | (uint8_t)(RG_RF09_STATE/256));
     spi_tx_buffer[1] = (uint8_t)(RG_RF09_STATE%256);
     spi_tx_buffer[2] = 0x00;
@@ -186,19 +186,19 @@ uint8_t at86rf215_status (void){
         SPI_FIRST,                  // isFirst
         SPI_LAST                    // isLast
     );
-    
+
     return spi_rx_buffer[2];
 }
 
 void at86rf215_read_isr (uint8_t* rf09_isr){
     uint8_t spi_tx[6];
     uint8_t spi_rx[6];
-    
+
     memset(&spi_tx[0],0,sizeof(spi_tx));
     memset(&spi_rx[0],0,sizeof(spi_rx));
     spi_tx[0] = (FLAG_READ | (uint8_t)(RG_RF09_IRQS >> 8));
-    spi_tx[1] = (uint8_t)(RG_RF09_IRQS & 0xFF);             
-    
+    spi_tx[1] = (uint8_t)(RG_RF09_IRQS & 0xFF);
+
     spi_txrx(
         spi_tx,                     // bufTx
         6,                          // lenbufTx
@@ -211,8 +211,10 @@ void at86rf215_read_isr (uint8_t* rf09_isr){
     *(rf09_isr)     = spi_rx[2];
     *(rf09_isr+1)   = spi_rx[3];
     *(rf09_isr+2)   = spi_rx[4];
-    *(rf09_isr+3)   = spi_rx[5];  
+    *(rf09_isr+3)   = spi_rx[5];
 }
+
+
 
 void at86rf215_readBurst(uint16_t reg, uint8_t* regValueRead, uint16_t size){
     uint8_t spi_tx[2049];
@@ -221,7 +223,7 @@ void at86rf215_readBurst(uint16_t reg, uint8_t* regValueRead, uint16_t size){
 
     spi_tx[0] = (uint8_t)(reg/256);
     spi_tx[1] = (uint8_t)(reg%256);
-    
+
     spi_txrx(
         spi_tx,                     // bufTx
         sizeof(spi_tx),             // lenbufTx
@@ -231,6 +233,6 @@ void at86rf215_readBurst(uint16_t reg, uint8_t* regValueRead, uint16_t size){
         SPI_FIRST,                  // isFirst
         SPI_LAST                    // isLast
     );
-    
+
 }
 
--- bsp/chips/at86rf215/spi.c
+++ bsp/chips/at86rf215/spi.c
@@ -1,15 +1,30 @@
 /**
-\brief msp430f5438a-specific definition of the "spi" bsp module.
+\brief definition of the "spi" bsp module.
 
-\author Jonathan Munoz <jonathan.munoz@inria.fr>, August 2016.
+\author Xavier Vilajosana <xvilajosana@eecs.berkeley.edu>, September 2017.
 */
-
-#include "msp430f5438a.h"
+#include "board.h"
+#include "board_info.h"
+#include "ssi.h"
 #include "spi.h"
-#include "leds.h"
-#include "bsp.h"
+#include "gpio.h"
+#include "ioc.h"
+#include "sys_ctrl.h"
+
+#include <headers/hw_ints.h>
+#include <headers/hw_ioc.h>
+#include <headers/hw_memmap.h>
+#include <headers/hw_ssi.h>
+#include <headers/hw_sys_ctrl.h>
+#include <headers/hw_types.h>
+
 
 //=========================== defines =========================================
+#define SPI_PIN_SSI_CLK             GPIO_PIN_2      //    CLK
+#define SPI_PIN_SSI_FSS             GPIO_PIN_3      //    CSn
+#define SPI_PIN_SSI_RX              GPIO_PIN_4      //    MISO
+#define SPI_PIN_SSI_TX              GPIO_PIN_5      //    MOSI
+#define SPI_GPIO_SSI_BASE           GPIO_A_BASE
 
 //=========================== variables =======================================
 
@@ -34,59 +49,44 @@ typedef struct {
 spi_vars_t spi_vars;
 
 //=========================== prototypes ======================================
-
+static void disableInterrupts(void);
+static void enableInterrupts(void);
 //=========================== public ==========================================
 
-void spi_init(void) {
+void spi_init() {
     // clear variables
-    memset(&spi_vars,0,sizeof(spi_vars_t));  
-   
-    UCB0CTL1   |=  UCSWRST ;                       // reset state
-   
-    // RF SPI0 CS as GPIO output high
-    //
-    P3SEL &= ~TRXEM_SPI_SC_N_PIN;
-    P3OUT |=  TRXEM_SPI_SC_N_PIN;
-    P3DIR |=  TRXEM_SPI_SC_N_PIN;
-    // Configuration
-    // -  8-bit
-    // -  Master Mode
-    //-  3-pin
-    // -  synchronous mode
-    // -  MSB first
-    // -  Clock phase select = captured on first edge
-    // -  Inactive state is low
-    // -  SMCLK as clock source
-    // -  Spi clk is adjusted corresponding to systemClock as the highest rate
-    //    supported by the supported radios: this could be optimized and done
-    //    after chip detect.
- 
-    UCB0CTL0  =  0x00+UCMST + UCSYNC + UCMODE_0 + UCMSB + UCCKPH;
-    UCB0CTL1 |=  UCSSEL_2;
-    UCB0BR1   =  0x00;
-    UCB0BR0   =  0x06;
-    // Configure port and pins
-    // - MISO/MOSI/SCLK GPIO controlled by peripheral
-    // - CS_n GPIO controlled manually, set to 1
- 
-    P3SEL    |=  TRXEM_SPI_MOSI_PIN | TRXEM_SPI_MISO_PIN | TRXEM_SPI_SCLK_PIN ;
-    P3SEL    &= ~TRXEM_SPI_SC_N_PIN ;
-    P3OUT    |=  TRXEM_SPI_SC_N_PIN | TRXEM_SPI_MISO_PIN ;/* Pullup on MISO */
-   
-    P3DIR    |=  TRXEM_SPI_SC_N_PIN;
-    // In case not automatically set 
-    P3DIR    |= TRXEM_SPI_MOSI_PIN | TRXEM_SPI_SCLK_PIN ;
-    P3DIR    &= ~TRXEM_SPI_MISO_PIN ;
-   
-    UCB0CTL1     &= ~UCSWRST;
-   
-    // enable interrupts via the IEx SFRs
-#ifdef SPI_IN_INTERRUPT_MODE
-    UCB0IE       |=  UCRXIE;                      // we only enable the SPI RX interrupt
-                                                  // since TX and RX happen concurrently,
-                                                  // i.e. an RX completion necessarily
-                                                  // implies a TX completion.
-#endif
+    memset(&spi_vars,0,sizeof(spi_vars_t));
+
+    // set the clk miso and cs pins as output
+    GPIOPinTypeGPIOOutput(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_CLK);
+    GPIOPinTypeGPIOOutput(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_TX);
+    GPIOPinTypeGPIOOutput(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_FSS);
+
+    //set cs to high
+    GPIOPinWrite(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_FSS, SPI_PIN_SSI_FSS);
+    //set pins to low
+    GPIOPinWrite(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_TX, 0);
+    GPIOPinWrite(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_CLK, 0);
+
+    SysCtrlPeripheralEnable(SYS_CTRL_PERIPH_SSI0);
+    SysCtrlPeripheralSleepEnable(SYS_CTRL_PERIPH_SSI0);
+    SysCtrlPeripheralDeepSleepDisable(SYS_CTRL_PERIPH_SSI0);
+
+    SSIDisable(SSI0_BASE);
+    SSIClockSourceSet(SSI0_BASE, SSI_CLOCK_PIOSC);
+
+    IOCPinConfigPeriphOutput(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_CLK, IOC_MUX_OUT_SEL_SSI0_CLKOUT);
+    IOCPinConfigPeriphOutput(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_TX, IOC_MUX_OUT_SEL_SSI0_TXD);
+    IOCPinConfigPeriphInput(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_RX, IOC_SSIRXD_SSI0);
+
+    GPIOPinTypeSSI(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_CLK );
+    GPIOPinTypeSSI(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_RX );
+    GPIOPinTypeSSI(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_TX );
+
+    SSIConfigSetExpClk(SSI0_BASE, SysCtrlIOClockGet(), SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, /*SysCtrlIOClockGet()/2*/16000000, 8);
+
+    // Enable the SSI0 module.
+    SSIEnable(SSI0_BASE);
 }
 
 #ifdef SPI_IN_INTERRUPT_MODE
@@ -95,19 +95,16 @@ void spi_setCb(spi_cbt cb) {
 }
 #endif
 
-void spi_txrx(uint8_t*     bufTx,
-              uint16_t     lenbufTx,
-              spi_return_t returnType,
-              uint8_t*     bufRx,
-              uint16_t     maxLenBufRx,
-              spi_first_t  isFirst,
-              spi_last_t   isLast) {
+void    spi_txrx(uint8_t*     bufTx,
+                 uint16_t     lenbufTx,
+                 spi_return_t returnType,
+                 uint8_t*     bufRx,
+                 uint16_t     maxLenBufRx,
+                 spi_first_t  isFirst,
+                 spi_last_t   isLast) {
 
-#ifdef SPI_IN_INTERRUPT_MODE
-    // disable interrupts
-    __disable_interrupt();
-#endif
-   
+    uint32_t data,i;
+    GPIOPinWrite(GPIO_B_BASE, GPIO_PIN_1, GPIO_PIN_1);
     // register spi frame to send
     spi_vars.pNextTxByte      =  bufTx;
     spi_vars.numTxedBytes     =  0;
@@ -117,120 +114,98 @@ void spi_txrx(uint8_t*     bufTx,
     spi_vars.maxRxBytes       =  maxLenBufRx;
     spi_vars.isFirst          =  isFirst;
     spi_vars.isLast           =  isLast;
-   
+
     // SPI is now busy
     spi_vars.busy             =  1;
-   
+
     // lower CS signal to have slave listening
-    /* Pull CS_N low and wait for SO to go low before communication starts */
     if (spi_vars.isFirst==SPI_FIRST) {
-        TRXEM_SPI_BEGIN();
-        while(P3IN & TRXEM_SPI_MISO_PIN);
+       GPIOPinWrite(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_FSS, 0);
     }
-   
-#ifdef SPI_IN_INTERRUPT_MODE
-    // implementation 1. use a callback function when transaction finishes
-   
-    // write first byte to TX buffer
-    UCB0TXBUF                   = *spi_vars.pNextTxByte;
-   
-    // re-enable interrupts
-    __enable_interrupt();
-#else
-   
-    // send all bytes
-    while (spi_vars.txBytesLeft>0) {
-      
-        TRXEM_SPI_TX(*spi_vars.pNextTxByte);
-        // busy wait on the interrupt flag
-        while ((UCB0IFG & UCRXIFG)==0);
-        // save the byte just received in the RX buffer
-        switch (spi_vars.returnType) {
-            case SPI_FIRSTBYTE:
-                if (spi_vars.numTxedBytes==0) {
-                    *spi_vars.pNextRxByte   = UCB0RXBUF;
-                }
-                break;
-            case SPI_BUFFER:
-                *spi_vars.pNextRxByte      = UCB0RXBUF;
-                spi_vars.pNextRxByte++;
-                break;
-            case SPI_LASTBYTE:
-                *spi_vars.pNextRxByte      = UCB0RXBUF;
-                break;
-        }
+
+    for ( i =  0; i < lenbufTx; i++)
+    {
+        // Push a byte
+        SSIDataPut(SSI0_BASE, spi_vars.pNextTxByte[i]);
+
+        // Wait until it is complete
+        while(SSIBusy(SSI0_BASE));
+
+        // Read a byte
+        SSIDataGet(SSI0_BASE, &data);
+
+        // Store the result
+        spi_vars.pNextRxByte[i] = (uint8_t)(data & 0xFF);
         // one byte less to go
-        spi_vars.pNextTxByte++;
-        spi_vars.numTxedBytes++;
-        spi_vars.txBytesLeft--;
-    }
-   
-    // put CS signal high to signal end of transmission to slave
-    if (spi_vars.isLast==SPI_LAST) {
-        TRXEM_SPI_END();
-    }
-   
+     }
+
+     if (spi_vars.isLast==SPI_LAST) {
+        GPIOPinWrite(SPI_GPIO_SSI_BASE, SPI_PIN_SSI_FSS, SPI_PIN_SSI_FSS);
+     }
+
     // SPI is not busy anymore
     spi_vars.busy             =  0;
-#endif
+    GPIOPinWrite(GPIO_B_BASE, GPIO_PIN_1, 0);
 }
 
 //=========================== private =========================================
 
+port_INLINE void enableInterrupts(void)
+{
+    // Enable the SPI interrupt
+    SSIIntEnable(SSI0_BASE, (SSI_TXFF | SSI_RXFF | SSI_RXTO | SSI_RXOR));
+
+    // Enable the SPI interrupt
+    IntEnable(INT_SSI0);
+}
+
+port_INLINE void disableInterrupts(void)
+{
+    // Disable the SPI interrupt
+    SSIIntDisable(SSI0_BASE, (SSI_TXFF | SSI_RXFF | SSI_RXTO | SSI_RXOR));
+
+    // Disable the SPI interrupt
+    IntDisable(INT_SSI0);
+}
+
 //=========================== interrupt handlers ==============================
 
-kick_scheduler_t spi_isr(void) {
-#ifdef SPI_IN_INTERRUPT_MODE   
+kick_scheduler_t spi_isr() {
+#ifdef SPI_IN_INTERRUPT_MODE
+    uint32_t data;
     // save the byte just received in the RX buffer
-    switch (spi_vars.returnType) {
-        case SPI_FIRSTBYTE:
-            if (spi_vars.numTxedBytes==0) {
-                *spi_vars.pNextRxByte   = UCB0RXBUF;
-            }
-            break;
-        case SPI_BUFFER:
-            *spi_vars.pNextRxByte      = UCB0RXBUF;
-            spi_vars.pNextRxByte++;
-            break;
-        case SPI_LASTBYTE:
-            *spi_vars.pNextRxByte      = UCB0RXBUF;
-            break;
-    }
-   
+    status = SSIIntStatus(SSI0_BASE, true);
+
+    // Clear SPI interrupt in the NVIC
+    IntPendClear(INT_SSI0);
+
+    SSIDataGet(SSI0_BASE, &data);
+
+    // Store the result
+    spi_vars.pNextRxByte = (uint8_t)(data & 0xFF);
+
     // one byte less to go
     spi_vars.pNextTxByte++;
-    spi_vars.numTxedBytes++;
+    spi_vars.pNextRxByte++;
     spi_vars.txBytesLeft--;
-   
+
     if (spi_vars.txBytesLeft>0) {
         // write next byte to TX buffer
-        UCB0TXBUF                = *spi_vars.pNextTxByte;
+        SSIDataPut(SSI0_BASE, *spi_vars.pNextTxByte);
+
     } else {
-        // put CS signal high to signal end of transmission to slave
-        if (spi_vars.isLast==SPI_LAST) {
-            P3OUT                 |=  0x01;
-        }
         // SPI is not busy anymore
         spi_vars.busy             =  0;
-      
+
         // SPI is done!
         if (spi_vars.callback!=NULL) {
-        // call the callback
+           // call the callback
             spi_vars.spi_cb();
             // kick the OS
             return KICK_SCHEDULER;
         }
     }
-    return DO_NOT_KICK_SCHEDULER;
-#else
-    // this should never happpen!
-   
-    // we can not print from within the BSP. Instead:
-    // blink the error LED
-    leds_error_blink();
-    // reset the board
-    board_reset();
-   
-    return DO_NOT_KICK_SCHEDULER; // we will not get here, statement to please compiler
+
 #endif
+    return DO_NOT_KICK_SCHEDULER;
 }
