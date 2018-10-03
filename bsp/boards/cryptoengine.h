83dcf8f85e182a209eecfb113b067df91310bf23
Timothy Claeys <timothy.claeys@gmail.com>
2018-10-03 18:23:02 +0200

    Implementation of the ecdsa verify driver

--- bsp/boards/cryptoengine.h
+++ bsp/boards/cryptoengine.h
@@ -3,16 +3,44 @@
 
 \author Malisa Vucinic <malishav@gmail.com>, March 2015.
 \author Marcelo Barros de Almeida <marcelobarrosalmeida@gmail.com>, March 2015.
+\author Timothy Claeys <timothy.claeys@gmail.com>, October 2018.
 */
 #ifndef __CRYPTO_ENGINE_H__
 #define __CRYPTO_ENGINE_H__
 
 #include "opendefs.h"
+#include "source/pka.h"
 
 //=========================== define ==========================================
+
 #define CBC_MAX_MAC_SIZE  16
+#define ECC_MAX_MUL_SIZE  24
+
+
+/* Elliptic Curve groups supported */
+
+enum EC_GROUPS {
+    SECP256R1    = 1,
+};
 
 //=========================== module variables ================================
+
+/* [NIST P-256, X9.62 prime256v1] */
+static const char curve_name[11] = "NIST P-256";
+static const uint32_t nist_p_256_p[8] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0x00000000,
+                                          0x00000000, 0x00000000, 0x00000001, 0xFFFFFFFF };
+static const uint32_t nist_p_256_n[8] = { 0xFC632551, 0xF3B9CAC2, 0xA7179E84, 0xBCE6FAAD,
+                                          0xFFFFFFFF, 0xFFFFFFFF, 0x00000000, 0xFFFFFFFF };
+static const uint32_t nist_p_256_a[8] = { 0xFFFFFFFC, 0xFFFFFFFF, 0xFFFFFFFF, 0x00000000,
+                                          0x00000000, 0x00000000, 0x00000001, 0xFFFFFFFF };
+static const uint32_t nist_p_256_b[8] = { 0x27D2604B, 0x3BCE3C3E, 0xCC53B0F6, 0x651D06B0,
+                                          0x769886BC, 0xB3EBBD55, 0xAA3A93E7, 0x5AC635D8 };
+static const uint32_t nist_p_256_x[8] = { 0xD898C296, 0xF4A13945, 0x2DEB33A0, 0x77037D81,
+                                          0x63A440F2, 0xF8BCE6E5, 0xE12C4247, 0x6B17D1F2 };
+static const uint32_t nist_p_256_y[8] = { 0x37BF51F5, 0xCBB64068, 0x6B315ECE, 0x2BCE3357,
+                                          0x7C0F9E16, 0x8EE7EB4A, 0xFE1A7F9B, 0x4FE342E2 };
+
+//============================ module methods =================================
 
 /**
 \brief CCM* forward transformation (i.e. encryption + authentication).
@@ -61,6 +89,37 @@ owerror_t cryptoengine_aes_ccms_dec(uint8_t* a,
       uint8_t key[16],
       uint8_t len_mac);  
 
+
+
+/**
+\brief ECDSA signature verification routine.
+\param[in] grp Pointer to a struct containing information about the used eliptic curve group.
+\param[in] Q Pointer to the public key used for verification of the signature.
+\param[in] s Pointer to the s value of the signature.
+\param[in] s_length number of uint32_t blocks used for the s value.
+\param[in] r Pointer to the r value of the signature.
+\param[in] s_length number of uint32_t blocks used for the r value.
+\param[in] hash the hash of the message that was signed.
+\param[in] h_length number of uint32_t blocks used for the hash.
+ */
+owerror_t cryptoengine_ecdsa_verify(
+      tECCCurveInfo* grp,
+      tECPt* Q,
+      uint32_t* s,
+      uint8_t s_length,
+      uint32_t* r,
+      uint8_t r_length,
+      uint32_t* hash,
+      uint8_t h_lenght);
+
+
+/**
+\brief Loads the information on the requested group
+\param[in] group Integer denoting the requested group
+\param[in, out] grp Pointer to a struct of type tECCCurveInfo,
+    struct gets initialized with the correct parameters.
+*/
+owerror_t cryptoengine_load_group(uint8_t group, tECCCurveInfo *grp);
 /**
 \brief Basic AES encryption of a single 16-octet block.
 \param[in,out] buffer Single block plaintext (16 octets). Will be overwritten by ciphertext.
--- bsp/boards/openmote-cc2538/cryptoengine.c
+++ bsp/boards/openmote-cc2538/cryptoengine.c
@@ -2,6 +2,7 @@
 \brief Crypto engine implementation for OpenMote-CC2538
   
 \author Malisa Vucinic <malishav@gmail.com>, March 2015.
+\author Timothy Claeys <timothy.claeys@gmail.com>, October 2018.
 */
 #include <stdint.h>
 
@@ -10,6 +11,7 @@
 #include <source/sys_ctrl.h>
 #include <source/aes.h>
 #include <source/ccm.h>
+#include <source/pka.h>
 
 #include "cryptoengine.h"
 
@@ -20,7 +22,6 @@
 
 static owerror_t load_key(uint8_t key[16], uint8_t* /* out */ key_location);
 
-
 //=========================== public ==========================================
 
 owerror_t cryptoengine_init(void) {
@@ -28,7 +29,9 @@ owerror_t cryptoengine_init(void) {
    // Enable AES peripheral
    //
    SysCtrlPeripheralReset(SYS_CTRL_PERIPH_AES);
+   SysCtrlPeripheralReset(SYS_CTRL_PERIPH_PKA);
    SysCtrlPeripheralEnable(SYS_CTRL_PERIPH_AES);
+   SysCtrlPeripheralEnable(SYS_CTRL_PERIPH_PKA);
    return E_SUCCESS;
 }
 
@@ -138,6 +141,135 @@ owerror_t cryptoengine_aes_ecb_enc(uint8_t* buffer, uint8_t* key) {
    return E_FAIL;
 }
 
+owerror_t cryptoengine_load_group(uint8_t group, tECCCurveInfo* grp) {
+    uint8_t ret;
+
+    switch( group )
+    {
+        case SECP256R1:
+            grp->name          = curve_name;
+            grp->ui8Size       = 8;
+            grp->pui32Prime    = nist_p_256_p;
+            grp->pui32N        = nist_p_256_n;
+            grp->pui32A        = nist_p_256_a;
+            grp->pui32B        = nist_p_256_b;
+            grp->pui32Gx       = nist_p_256_x;
+            grp->pui32Gy       = nist_p_256_y;
+            ret = E_SUCCESS;
+            break;
+        default:
+            ret = E_FAIL;
+    }
+
+    return ( ret );
+}
+
+
+owerror_t cryptoengine_ecdsa_verify(
+    tECCCurveInfo* grp,
+    tECPt* Q,
+    uint32_t* s,
+    uint8_t s_length,
+    uint32_t* r,
+    uint8_t r_length,
+    uint32_t * hash,
+    uint8_t h_length)
+{
+    uint8_t result;
+    uint32_t s_inv[grp->ui8Size];
+    uint32_t resultLocation;
+    uint32_t u1[ECC_MAX_MUL_SIZE];
+    uint32_t u2[ECC_MAX_MUL_SIZE];
+    uint32_t u1_length = ECC_MAX_MUL_SIZE, u2_length = ECC_MAX_MUL_SIZE;
+
+    tECPt P1;
+    uint32_t p1_x[grp->ui8Size];
+    uint32_t p1_y[grp->ui8Size];
+    P1.pui32X = p1_x;
+    P1.pui32Y = p1_y;
+
+    tECPt P2;
+    uint32_t p2_x[grp->ui8Size];
+    uint32_t p2_y[grp->ui8Size];
+    P2.pui32X = p2_x;
+    P2.pui32Y = p2_y;
+
+    tECPt G;
+    G.pui32X = grp->pui32Gx;
+    G.pui32Y = grp->pui32Gy;
+
+    /* Invert s mod n */
+    PKABigNumInvModStart( s, s_length, grp->pui32N, grp->ui8Size, &resultLocation );
+
+    do{
+        ASM_NOP;
+    } while (PKABigNumInvModGetResult( s_inv, grp->ui8Size, resultLocation ) == PKA_STATUS_OPERATION_INPRG);
+
+    /* u1 = s_inv * hash mod ord */
+    PKABigNumMultiplyStart( s_inv, grp->ui8Size, hash, h_length, &resultLocation );
+
+    //u1_length gets the appropriate length after the operation completes
+    do{
+        ASM_NOP;
+    } while (PKABigNumMultGetResult( u1, &u1_length, resultLocation ) == PKA_STATUS_OPERATION_INPRG);
+
+    PKABigNumModStart( u1, (uint8_t)u1_length, grp->pui32N, grp->ui8Size, &resultLocation );
+
+    do{
+        ASM_NOP;
+    } while (PKABigNumModGetResult( u1, grp->ui8Size, resultLocation ) == PKA_STATUS_OPERATION_INPRG);
+
+    /* u2 = s_inv * r mod ord */
+    PKABigNumMultiplyStart( s_inv, grp->ui8Size, r, r_length, &resultLocation );
+
+    //u2_length gets the appropriate length after the operation completes
+    do{
+        ASM_NOP;
+    } while (PKABigNumMultGetResult( u2, &u2_length, resultLocation ) == PKA_STATUS_OPERATION_INPRG);
+
+    PKABigNumModStart( u2, (uint8_t)u2_length, grp->pui32N, grp->ui8Size, &resultLocation );
+
+    do{
+        ASM_NOP;
+    } while (PKABigNumModGetResult( u2, grp->ui8Size, resultLocation ) == PKA_STATUS_OPERATION_INPRG);
+
+    /* p1 = u1 * G */
+    PKAECCMultiplyStart( u1, &G, grp, &resultLocation );
+
+    do{
+        ASM_NOP;
+    } while( PKAECCMultiplyGetResult( &P1, resultLocation ) == PKA_STATUS_OPERATION_INPRG );
+
+    PKAECCMultiplyStart( u2, Q, grp, &resultLocation );
+
+    do{
+        ASM_NOP;
+    } while( PKAECCMultiplyGetResult( &P2, resultLocation ) == PKA_STATUS_OPERATION_INPRG );
+
+    /* P = p1 + p2 */
+    PKAECCAddStart( &P1, &P2, grp, &resultLocation );
+
+    do{
+        ASM_NOP;
+    } while( PKAECCAddGetResult( &P1, resultLocation ) == PKA_STATUS_OPERATION_INPRG );
+
+    /* verify the signature */
+    PKABigNumCmpStart( r, P1.pui32X, r_length );
+
+    do{
+        ASM_NOP;
+    } while( PKABigNumCmpGetResult() == PKA_STATUS_OPERATION_INPRG );
+
+    if ( PKABigNumCmpGetResult() == PKA_STATUS_SUCCESS ){
+        result = E_SUCCESS;
+    }
+    else{
+        result = E_FAIL;
+    }
+    return ( result );
+}
+
+
 //=========================== private ==========================================
 
 /**
@@ -157,3 +289,4 @@ static owerror_t load_key(uint8_t key[16], uint8_t* /* out */ key_location) {
    *key_location = DEFAULT_KEY_AREA;
    return E_SUCCESS;
 }
+
