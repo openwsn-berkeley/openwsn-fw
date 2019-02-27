b3b6061dfc24ca8404efc9aa76268f86960372e3
Mališa Vučinić <malishav@gmail.com>
2019-02-27 16:23:56 +0100

    FW-808. Implement burst handling.

--- openapps/cbenchmark/cbenchmark.c
+++ openapps/cbenchmark/cbenchmark.c
@@ -83,72 +83,79 @@ void cbenchmark_sendPacket(open_addr_t *dest, bool con, uint8_t numPackets, uint
     coap_option_iht              options[1];
     owerror_t                    outcome;
     uint8_t                      numOptions;
+    uint8_t                      i;
 
     numOptions = 0;
+    i = 0;
 
-    // create a CoAP packet
-    pkt = openqueue_getFreePacketBuffer(COMPONENT_CBENCHMARK);
-    if (pkt==NULL) {
-        openserial_printError(
-            COMPONENT_CBENCHMARK,
-            ERR_NO_FREE_PACKET_BUFFER,
-            (errorparameter_t)0,
-            (errorparameter_t)0
-        );
-        // TODO log error
-    }
+    for (i = 0; i < numPackets; i++) {
+        // create a CoAP packet
+        pkt = openqueue_getFreePacketBuffer(COMPONENT_CBENCHMARK);
+        if (pkt==NULL) {
+            openserial_printError(
+                COMPONENT_CBENCHMARK,
+                ERR_NO_FREE_PACKET_BUFFER,
+                (errorparameter_t)0,
+                (errorparameter_t)0
+            );
+            // TODO log error
+        }
 
-    // take ownership over that packet
-    pkt->creator                   = COMPONENT_CBENCHMARK;
-    pkt->owner                     = COMPONENT_CBENCHMARK;
+        // take ownership over that packet
+        pkt->creator                   = COMPONENT_CBENCHMARK;
+        pkt->owner                     = COMPONENT_CBENCHMARK;
 
-    // location-path option
-    options[numOptions].type = COAP_OPTION_NUM_URIPATH;
-    options[numOptions].length = sizeof(cbenchmark_path0)-1;
-    options[numOptions].pValue = (uint8_t *)cbenchmark_path0;
-    numOptions++;
-
-    // metadata
-    pkt->l4_destination_port       = WKP_UDP_COAP;
-    // construct destination address
-    packetfunctions_mac64bToIp128b(idmanager_getMyID(ADDR_PREFIX), dest, &(pkt->l3_destinationAdd));
-
-    // copy the token
-    packetfunctions_reserveHeaderSize(pkt, tokenLen);
-    memcpy(pkt->payload, token, tokenLen);
-
-    // zero-out the payload after the token
-    packetfunctions_reserveHeaderSize(pkt, payloadLen);
-    memset(pkt->payload, 0x00, payloadLen);
-
-    // confirmable semantics of openbenchmark denotes whether there is an application-layer
-    // acknowledgment going back. we implement this behavior by sending or not the CoAP response
-    if (!con) {
-        // to avoid the server generating a response, we use the No Response option specified
-        // in RFC7967
-        options[numOptions].type = COAP_OPTION_NUM_NORESPONSE;
-        options[numOptions].length = 1;
-        options[numOptions].pValue = &cbenchmark_vars.noResponse;
+        // location-path option
+        options[numOptions].type = COAP_OPTION_NUM_URIPATH;
+        options[numOptions].length = sizeof(cbenchmark_path0)-1;
+        options[numOptions].pValue = (uint8_t *)cbenchmark_path0;
         numOptions++;
+
+        // confirmable semantics of openbenchmark denotes whether there is an application-layer
+        // acknowledgment going back. we implement this behavior by sending or not the CoAP response
+        if (!con) {
+            // to avoid the server generating a response, we use the No Response option specified
+            // in RFC7967
+            options[numOptions].type = COAP_OPTION_NUM_NORESPONSE;
+            options[numOptions].length = 1;
+            options[numOptions].pValue = &cbenchmark_vars.noResponse;
+            numOptions++;
+        }
+
+        // metadata
+        pkt->l4_destination_port       = WKP_UDP_COAP;
+        // construct destination address
+        packetfunctions_mac64bToIp128b(idmanager_getMyID(ADDR_PREFIX), dest, &(pkt->l3_destinationAdd));
+
+        // packet counter
+        packetfunctions_reserveHeaderSize(pkt, 1);
+        pkt->payload[0] = i;
+
+        // copy the token
+        packetfunctions_reserveHeaderSize(pkt, tokenLen);
+        memcpy(pkt->payload, token, tokenLen);
+
+        // zero-out the payload after the token
+        packetfunctions_reserveHeaderSize(pkt, payloadLen);
+        memset(pkt->payload, 0x00, payloadLen);
+
+        // send
+        outcome = opencoap_send(
+            pkt,
+            COAP_TYPE_NON,
+            COAP_CODE_REQ_POST,
+            1, // token len
+            options,
+            numOptions,
+            &cbenchmark_vars.desc
+        );
+
+        // avoid overflowing the queue if fails
+        if (outcome==E_FAIL) {
+            openqueue_freePacketBuffer(pkt);
+            // TODO log error
+        }
+        // TODO log packet sent event
     }
-
-    // send
-    outcome = opencoap_send(
-        pkt,
-        COAP_TYPE_NON,
-        COAP_CODE_REQ_POST,
-        1, // token len
-        options,
-        numOptions,
-        &cbenchmark_vars.desc
-    );
-
-    // avoid overflowing the queue if fails
-    if (outcome==E_FAIL) {
-        openqueue_freePacketBuffer(pkt);
-        // TODO log error
-    }
-
-    // TODO log packet sent event
 }
 
