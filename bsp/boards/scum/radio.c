bfbc52f07a9b51415db2a068bea58fddcbf03269
changtengfei <tengfei.chang@gmail.com>
2019-04-24 19:51:08 +0200

    FW-830. format the calibration code.

--- bsp/boards/scum/radio.c
+++ bsp/boards/scum/radio.c
@@ -25,7 +25,10 @@
 
 // ==== for calibration
 
-#define  FREQ_UPDATE_TIMEOUT  15
+#define  IF_FREQ_UPDATE_TIMEOUT  10
+#define  LO_FREQ_UPDATE_TIMEOUT  10
+#define  FILTER_WINDOWS_LEN      11
+#define  FIR_COEFF_SCALE         512 // determined by FIR_coeff
 
 //=========================== variables =======================================
 
@@ -46,10 +49,11 @@ typedef struct {
 
 radio_vars_t radio_vars;
 
-unsigned char   FIR_coeff[11] = {4,16,37,64,87,96,87,64,37,16,4};
-unsigned int    IF_estimate_history[11] = {500,500,500,500,500,500,500,500,500,500};
-signed short    cdr_tau_history[11] = {0};
-unsigned short  frequency_update_cooldown_timer = 0;
+unsigned char   FIR_coeff[FILTER_WINDOWS_LEN]           = {4,   16, 37, 64, 87, 96, 87, 64, 37, 16,  4};
+unsigned int    IF_estimate_history[FILTER_WINDOWS_LEN] = {500,500,500,500,500,500,500,500,500,500,500};
+signed short    cdr_tau_history[FILTER_WINDOWS_LEN]     = {0,    0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+unsigned short  IF_freq_freshness_timeout               =  0;
+unsigned short  LO_freq_freshness_timeout               =  0;
 signed short    cdr_tau_value;
 unsigned int    LQI_chip_errors;
 unsigned int    IF_estimate;
@@ -355,119 +359,133 @@ void radio_getReceivedFrame(uint8_t* pBufRead,
     *pLenRead       = radio_vars.radio_rx_buffer[0];
     
     //===== packet 
-    memcpy(pBufRead,&(radio_vars.radio_rx_buffer[1]),*pLenRead);
+    if (*pLenRead<=maxBufLen) {
+        memcpy(pBufRead,&(radio_vars.radio_rx_buffer[1]),*pLenRead);
+    }
 }
 
 //=========================== private =========================================
 
 void radio_calibration(void) {
 
-    signed int sum = 0;
-    int jj;
-    unsigned int IF_est_filtered;
-    signed int chip_rate_error_ppm, chip_rate_error_ppm_filtered;
-    unsigned short packet_len;
-    signed int timing_correction;
+    int8_t   i;
+    uint8_t  packet_len;
+    uint32_t IF_est_filtered;
+    int32_t  chip_rate_error_ppm;
+    int32_t  chip_rate_error_ppm_filtered;
+    int32_t  sum;
     
     packet_len = radio_vars.radio_rx_buffer[0];
     
-    // When updating LO and IF clock frequncies, must wait long enough for the changes to propagate before changing again
-    // Need to receive as many packets as there are taps in the FIR filter
-    frequency_update_cooldown_timer++;
+    if( packet_len==0x0a){
     
-    // FIR filter for cdr tau slope
-    sum = 0;
-    
-    // A tau value of 0 indicates there is no rate mistmatch between the TX and RX chip clocks
-    // The cdr_tau_value corresponds to the number of samples that were added or dropped by the CDR
-    // Each sample point is 1/16MHz = 62.5ns
-    // Need to estimate ppm error for each packet, then FIR those values to make tuning decisions
-    // error_in_ppm = 1e6 * (#adjustments * 62.5ns) / (packet length (bytes) * 64 chips/byte * 500ns/chip)
-    // Which can be simplified to (#adjustments * 15625) / (packet length * 8)
-                
-    chip_rate_error_ppm = (cdr_tau_value * 15625) / (packet_len * 8);
-    
-    // Shift old samples
-    for (jj=9; jj>=0; jj--){
-        cdr_tau_history[jj+1] = cdr_tau_history[jj];        
-    }
-    
-    // New sample
-    cdr_tau_history[0] = chip_rate_error_ppm;
-    
-    // Do FIR convolution
-    for (jj=0; jj<=10; jj++){
-        sum = sum + cdr_tau_history[jj] * FIR_coeff[jj];        
-    }
-    
-    // Divide by 512 (sum of the coefficients) to scale output
-    chip_rate_error_ppm_filtered = sum / 512;
-    
-    // The IF clock frequency steps are about 2000ppm, so make an adjustment only if the error is larger than 1000ppm
-    // Must wait long enough between changes for FIR to settle (at least 10 packets)
-    // Need to add some handling here in case the IF_fine code will rollover with this change (0 <= IF_fine <= 31)
-    if(frequency_update_cooldown_timer == FREQ_UPDATE_TIMEOUT){
-        if(chip_rate_error_ppm_filtered > 1000) {
-            set_IF_clock_frequency(IF_coarse, IF_fine++, 0);
-        }
-        if(chip_rate_error_ppm_filtered < -1000) {
-            set_IF_clock_frequency(IF_coarse, IF_fine--, 0);
-        }
-    }
-    
-    // FIR filter for IF estimate
-    sum = 0;
-    
-    // The IF estimate reports how many zero crossings (both pos and neg) there were in a 100us period
-    // The IF should on average be 2.5 MHz, which means the IF estimate will return ~500 when there is no IF error
-    // Each tick is roughly 5 kHz of error
-    
-    // Only make adjustments when the chip error rate is <10% (this value was picked as an arbitrary choice)
-    // While packets can be received at higher chip error rates, the average IF estimate tends to be less accurate
-    // Estimated chip_error_rate = LQI_chip_errors/256 (assuming the packet length was at least 8 Bytes)
-    if(LQI_chip_errors < 25){
-    
+        // When updating LO and IF clock frequncies, must wait long enough for the changes to propagate before changing again
+        // Need to receive as many packets as there are taps in the FIR filter
+        IF_freq_freshness_timeout++;
+        LO_freq_freshness_timeout++;
+        
+        printf("IF_estimate=%d, cdr_tau_value=%d  LQI_chip_errors=%d\r\n",IF_estimate,cdr_tau_value,LQI_chip_errors);
+        
+        // ==== FIR filter for cdr tau slope ====
+        
+        // the IF clock is for receiver chip clock
+        
+        sum = 0;
+        
+        // A tau value of 0 indicates there is no rate mistmatch between the TX and RX chip clocks
+        // The cdr_tau_value corresponds to the number of samples that were added or dropped by the CDR
+        // Each sample point is 1/16MHz = 62.5ns
+        // Need to estimate ppm error for each packet, then FIR those values to make tuning decisions
+        // error_in_ppm = 1e6 * (#adjustments * 62.5ns) / (packet length (bytes) * 64 chips/byte * 500ns/chip)
+        // Which can be simplified to (#adjustments * 15625) / (packet length * 8)
+        
+        chip_rate_error_ppm = (cdr_tau_value * 15625) / (packet_len * 8);
+        
         // Shift old samples
-        for (jj=9; jj>=0; jj--){
-            IF_estimate_history[jj+1] = IF_estimate_history[jj];        
+        for (i=FILTER_WINDOWS_LEN-2; i>=0; i--){
+            cdr_tau_history[i+1] = cdr_tau_history[i];        
         }
         
         // New sample
-        IF_estimate_history[0] = IF_estimate;
-
+        cdr_tau_history[0] = chip_rate_error_ppm;
+        
         // Do FIR convolution
-        for (jj=0; jj<=10; jj++){
-            sum = sum + IF_estimate_history[jj] * FIR_coeff[jj];        
+        for (i=0; i<FILTER_WINDOWS_LEN; i++){
+            sum = sum + cdr_tau_history[i] * FIR_coeff[i];
         }
         
         // Divide by 512 (sum of the coefficients) to scale output
-        IF_est_filtered = sum / 512;
+        chip_rate_error_ppm_filtered = sum / FIR_COEFF_SCALE;
         
-        //printf("%d - %d, %d\n",IF_estimate,IF_est_filtered,LQI_chip_errors);
-        
-        // The LO frequency steps are about ~80-100 kHz, so make an adjustment only if the error is larger than that
-        // These hysteresis bounds (+/- X) have not been optimized
-        // Must wait long enough between changes for FIR to settle (at least as many packets as there are taps in the FIR)
-        // For now, assume that TX/RX should both be updated, even though the IF information is only from the RX code
-        if(frequency_update_cooldown_timer == FREQ_UPDATE_TIMEOUT){
-            if(IF_est_filtered > 520){
-                RX_channel_codes[radio_vars.current_frequency - 11]++; 
-                TX_channel_codes[radio_vars.current_frequency - 11]++; 
+        // The IF clock frequency steps are about 2000ppm, so make an adjustment only if the error is larger than 1000ppm
+        // Must wait long enough between changes for FIR to settle (at least 10 packets)
+        // Need to add some handling here in case the IF_fine code will rollover with this change (0 <= IF_fine <= 31)
+        if(IF_freq_freshness_timeout >= IF_FREQ_UPDATE_TIMEOUT){
+            if(chip_rate_error_ppm_filtered > 1000) {
+                set_IF_clock_frequency(IF_coarse, IF_fine++, 0);
             }
-            if(IF_est_filtered < 480){
-                RX_channel_codes[radio_vars.current_frequency - 11]--; 
-                TX_channel_codes[radio_vars.current_frequency - 11]--; 
+            if(chip_rate_error_ppm_filtered < -1000) {
+                set_IF_clock_frequency(IF_coarse, IF_fine--, 0);
             }
-
-            frequency_update_cooldown_timer = 0;
+            IF_freq_freshness_timeout = 0;
+            
+            printf("chip_rate_error_ppm = %d, chip_rate_error_ppm_filtered = %d \r\n", chip_rate_error_ppm,   chip_rate_error_ppm_filtered);
         }
+            
+        // ==== FIR filter for IF estimate ====
+        
+        sum = 0;
+        
+        // The IF estimate reports how many zero crossings (both pos and neg) there were in a 100us period
+        // The IF should on average be 2.5 MHz, which means the IF estimate will return ~500 when there is no IF error
+        // Each tick is roughly 5 kHz of error
+        
+        // Only make adjustments when the chip error rate is <10% (this value was picked as an arbitrary choice)
+        // While packets can be received at higher chip error rates, the average IF estimate tends to be less accurate
+        // Estimated chip_error_rate = LQI_chip_errors/256 (assuming the packet length was at least 8 symbols)
+//        if(LQI_chip_errors < 25){
+        
+            // Shift old samples
+            for (i=FILTER_WINDOWS_LEN-2; i>=0; i--){
+                IF_estimate_history[i+1] = IF_estimate_history[i];        
+            }
+            
+            // New sample
+            IF_estimate_history[0] = IF_estimate;
+
+            // Do FIR convolution
+            for (i=0; i<FILTER_WINDOWS_LEN; i++){
+                sum = sum + IF_estimate_history[i] * FIR_coeff[i];        
+            }
+            IF_est_filtered = sum / FIR_COEFF_SCALE;
+            
+            printf("IF_estimate = %d, IF_est_filtered = %d \r\n", IF_estimate, IF_est_filtered);
+
+            // The LO frequency steps are about ~80-100 kHz, so make an adjustment only if the error is larger than that
+            // These hysteresis bounds (+/- X) have not been optimized
+            // Must wait long enough between changes for FIR to settle (at least as many packets as there are taps in the FIR)
+            // For now, assume that TX/RX should both be updated, even though the IF information is only from the RX code
+            if(LO_freq_freshness_timeout >= LO_FREQ_UPDATE_TIMEOUT){
+                if(IF_est_filtered > 520){
+                    RX_channel_codes[radio_vars.current_frequency - 11]++; 
+                    TX_channel_codes[radio_vars.current_frequency - 11]++; 
+                }
+                if(IF_est_filtered < 480){
+                    RX_channel_codes[radio_vars.current_frequency - 11]--; 
+                    TX_channel_codes[radio_vars.current_frequency - 11]--; 
+                }
+
+                LO_freq_freshness_timeout = 0;
+                
+                printf("IF_estimate         = %d, IF_est_filtered              = %d \r\n", IF_estimate,           IF_est_filtered);
+            }
+//        }
+        
+        // Write and load analog scan chain
+        
+        analog_scan_chain_write_3B_fromFPGA(&ASC[0]);
+        analog_scan_chain_load_3B_fromFPGA();
     }
-    
-    // Write and load analog scan chain
-    analog_scan_chain_write_3B_fromFPGA(&ASC[0]);
-    analog_scan_chain_load_3B_fromFPGA();
-    
-//    printf("IF=%d, LQI=%d, CDR=%d, len=%d, LC=%d\r\n",IF_estimate,LQI_chip_errors,cdr_tau_value,packet_len,LC_code);
 }
 
 //=========================== callbacks =======================================
@@ -536,16 +554,12 @@ kick_scheduler_t radio_isr(void) {
 
             // Only record IF estimate, LQI, and CDR tau for valid packets
             IF_estimate = read_IF_estimate();
-            LQI_chip_errors    = ANALOG_CFG_REG__21 & 0xFF; //read_LQI();    
+            LQI_chip_errors    = ANALOG_CFG_REG__21 & 0xFF; //read_LQI();
         
             // Read the value of tau debug at end of packet
             // Do this later in the ISR to make sure this register has settled before trying to read it
             // (the register is on the adc clock domain)
             cdr_tau_value = ANALOG_CFG_REG__25;
-            
-            radio_calibration();
-            
-            radio_rxEnable();
         }
         // the packet transmission or reception is done,
         // update the radio state
@@ -553,6 +567,12 @@ kick_scheduler_t radio_isr(void) {
         if (radio_vars.endFrame_cb!=NULL) {
             // call the callback
             radio_vars.endFrame_cb(capturedTime);
+            
+            if (irq_status & RX_DONE_INT) {
+                radio_calibration();
+                radio_rxEnable();
+            }
+            
             debugpins_isr_clr();
             // kick the OS
             return KICK_SCHEDULER;
