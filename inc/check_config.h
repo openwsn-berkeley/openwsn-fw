551d800a619caf86fc5c61a996d3b95301734245
Timothy Claeys <timothy.claeys@gmail.com>
2020-03-06 14:52:30 +0100

    FW-867. Add config checker (config_check.h)

---
+++ inc/check_config.h
@@ -0,0 +1,37 @@
+#ifndef __CHECK_CONFIG_H
+#define __CHECK_CONFIG_H
+
+
+#if !defined(OPENWSN_COAP_C) && (\
+    defined(OPENWSN_C6T_C) || \
+    defined(OPENWN_CEXAMPLE_C) || \
+    defined(OPENWSN_CINFO_C) || \
+    defined(OPENWSN_CINFRARED_C) || \
+    defined(OPENWSN_CLED_C) || \
+    defined(OPENWSN_CSENSORS_C) || \
+    defined(OPENWSN_CSTORM_C) || \
+    defined(OPENWSN_CWELLKNOWN) || \
+    defined(OPENWSN_RRT_C))
+
+#error "A CoAP dependent application is defined, but CoAP is not included in the build."
+#endif
+
+#if !defined(OPENWSN_OPENUDP_C) && (\
+    defined(OPENWSN_USERIALBRIDGE_C) || \
+    defined(OPENWN_UECHO_C) || \
+    defined(OPENWSN_UINJECT_C) || \
+    defined(OPENWSN_USERIALBRIDGE_C) || \
+    defined(OPENWSN_UEXPIRATION_C) || \
+    defined(OPENWSN_UEXP_MONITOR))
+
+#error "A UDP dependent application is defined, but UDP is not included in the build."
+#endif
+
+#if !defined(OPENWSN_6LO_FRAGMENTATION_C) && (\
+    defined(OPENWSN_MAX_PKTSIZE_SUPPORTED) || \
+    defined(OPENWN_MAX_NUM_BIGPKTS))
+
+#error "6LoWPAN fragmentation options specified, but 6LoWPAN fragmentation is not included in the build."
+#endif
+
+#endif /* __CHECK_CONFIG_H */
