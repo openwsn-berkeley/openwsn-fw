0ac461718174cd22221ef6e1762ce78b03786c1d
changtengfei <tengfei.chang@gmail.com>
2020-04-01 15:09:28 +0200

    FW-751. update uart implementation.

--- bsp/boards/nrf52840/uart.c
+++ bsp/boards/nrf52840/uart.c
@@ -40,6 +40,8 @@ typedef struct
 {
    uart_tx_cbt txCb;
    uart_rx_cbt rxCb;
+   bool        fXonXoffEscaping;
+   uint8_t     xonXoffEscapedByte;
 } uart_vars_t;
 
 uart_vars_t uart_vars;
@@ -52,45 +54,42 @@ static void uart_event_handler(app_uart_evt_t * p_event);
 
 static app_uart_comm_params_t const app_config =
 {
-  .rx_pin_no= RX_PIN_NUMBER,
-  .tx_pin_no= TX_PIN_NUMBER,
-  .rts_pin_no= RTS_PIN_NUMBER,                // defaults to UART_PIN_DISCONNECTED
-  .cts_pin_no= CTS_PIN_NUMBER,                // defaults to UART_PIN_DISCONNECTED
-  .baud_rate= UART_DEFAULT_CONFIG_BAUDRATE,
-  .use_parity= (UART_DEFAULT_CONFIG_PARITY != 0) ? (true) : (false),
-  .flow_control= UART_DEFAULT_CONFIG_HWFC     // defaults to false
+    .rx_pin_no= RX_PIN_NUMBER,
+    .tx_pin_no= TX_PIN_NUMBER,
+    .rts_pin_no= RTS_PIN_NUMBER,                // defaults to UART_PIN_DISCONNECTED
+    .cts_pin_no= CTS_PIN_NUMBER,                // defaults to UART_PIN_DISCONNECTED
+    .baud_rate= UART_DEFAULT_CONFIG_BAUDRATE,
+    .use_parity= (UART_DEFAULT_CONFIG_PARITY != 0) ? (true) : (false),
+    .flow_control= UART_DEFAULT_CONFIG_HWFC     // defaults to false
 };
 
-static void uart_reinit(void)
-{ 
-  // for the case that the UART has previously been initialized, uninitialize it first  
-  app_uart_close();
+static void uart_reinit(void) { 
+    // for the case that the UART has previously been initialized, uninitialize it first  
+    app_uart_close();
 
-  #define APP_UART_BUF_SIZE 128
+    #define APP_UART_BUF_SIZE 128
 
-  static uint8_t rx_buf[APP_UART_BUF_SIZE];
-  static uint8_t tx_buf[APP_UART_BUF_SIZE];
-  static app_uart_buffers_t app_uart_buffers=
-  {
-    .rx_buf= rx_buf,
-    .tx_buf= tx_buf,
-    .rx_buf_size= sizeof(rx_buf),
-    .tx_buf_size= sizeof(tx_buf)
-  };
+    static uint8_t rx_buf[APP_UART_BUF_SIZE];
+    static uint8_t tx_buf[APP_UART_BUF_SIZE];
+    static app_uart_buffers_t app_uart_buffers=
+    {
+        .rx_buf= rx_buf,
+        .tx_buf= tx_buf,
+        .rx_buf_size= sizeof(rx_buf),
+        .tx_buf_size= sizeof(tx_buf)
+    };
 
-  memset(rx_buf, 0, sizeof(rx_buf));
-  memset(tx_buf, 0, sizeof(tx_buf));
+    memset(rx_buf, 0, sizeof(rx_buf));
+    memset(tx_buf, 0, sizeof(tx_buf));
 
   // if UART cannot be initialized, blink error LED for 10s, and then reset
-  if (NRF_SUCCESS != app_uart_init(&app_config, &app_uart_buffers, uart_event_handler, (app_irq_priority_t) NRFX_UART_DEFAULT_CONFIG_IRQ_PRIORITY))
-  {
-    leds_error_blink();
-    board_reset();
-  }
+    if (NRF_SUCCESS != app_uart_init(&app_config, &app_uart_buffers, uart_event_handler, (app_irq_priority_t) NRFX_UART_DEFAULT_CONFIG_IRQ_PRIORITY)) {
+        leds_error_blink();
+        board_reset();
+    }
 }
 
-void uart_init(void)
-{ 
+void uart_init(void) { 
   // reset local variables
   memset(&uart_vars,0,sizeof(uart_vars_t));
 
@@ -107,60 +106,56 @@ void uart_init(void)
   uart_reinit();  
 }
 
-void uart_setCallbacks(uart_tx_cbt txCb, uart_rx_cbt rxCb)
-{
+void uart_setCallbacks(uart_tx_cbt txCb, uart_rx_cbt rxCb) {
   uart_vars.txCb = txCb;
   uart_vars.rxCb = rxCb;
 }
 
-void uart_enableInterrupts(void)
-{
-  // nrf_uart_int_enable(..., NRF_UART_INT_MASK_TXDRDY | NRF_UART_INT_MASK_RXDRDY | NRF_UART_INT_MASK_RXTO | NRF_UART_INT_MASK_ERROR);
+void uart_enableInterrupts(void) {
+    nrf_uart_int_enable(NRF_UART0, NRF_UART_INT_MASK_TXDRDY | NRF_UART_INT_MASK_RXDRDY | NRF_UART_INT_MASK_RXTO | NRF_UART_INT_MASK_ERROR);
 }
 
-void uart_disableInterrupts(void)
-{
-  // nrf_uart_int_disable(..., NRF_UART_INT_MASK_TXDRDY | NRF_UART_INT_MASK_RXDRDY | NRF_UART_INT_MASK_RXTO | NRF_UART_INT_MASK_ERROR);
+void uart_disableInterrupts(void) {
+    nrf_uart_int_disable(NRF_UART0, NRF_UART_INT_MASK_TXDRDY | NRF_UART_INT_MASK_RXDRDY | NRF_UART_INT_MASK_RXTO | NRF_UART_INT_MASK_ERROR);
 }
 
-void uart_clearRxInterrupts(void)
-{
+void uart_clearRxInterrupts(void) {
   // is handled within app_uart
 }
 
-void uart_clearTxInterrupts(void)
-{
+void uart_clearTxInterrupts(void) {
   // is handled within app_uart
 }
 
-void uart_setCTS(bool state)
-{
-  if (state==0x01)
-  {
-    app_uart_put(XON);
-  }
-  else
-  {
-    app_uart_put(XOFF);
-  }
+void uart_setCTS(bool state) {
+    if (state==0x01) {
+     app_uart_put(XON);
+    } else {
+      app_uart_put(XOFF);
+    }
 }
 
-void uart_writeByte(uint8_t byteToWrite)
-{
-  app_uart_put(byteToWrite);
+void uart_writeByte(uint8_t byteToWrite) {
+    app_uart_put(byteToWrite);
+
+    if (byteToWrite==XON || byteToWrite==XOFF || byteToWrite==XONXOFF_ESCAPE) {
+        uart_vars.fXonXoffEscaping     = 0x01;
+        uart_vars.xonXoffEscapedByte   = byteToWrite;
+        app_uart_put(XONXOFF_ESCAPE);
+    } else {
+        app_uart_put(byteToWrite);
+    }
 }
 
-uint8_t uart_readByte(void)
-{
-  uint8_t newChar;
-  app_uart_get(&newChar);
-  return (newChar);
+uint8_t uart_readByte(void) {
+    uint8_t newChar;
+    app_uart_get(&newChar);
+    return (newChar);
 }
 
 //=========================== interrupt handlers ==============================
 
-void uart_event_handler(app_uart_evt_t * p_event)
-{
+void uart_event_handler(app_uart_evt_t * p_event) {
 // debugpins_isr_set();
 
   if ((p_event->evt_type == APP_UART_COMMUNICATION_ERROR) || (p_event->evt_type == APP_UART_FIFO_ERROR))
