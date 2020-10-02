dedd48156e1ccfb8769e280d32349abb7cddb3c6
changtengfei <tengfei.chang@gmail.com>
2020-10-02 17:56:36 +0200

    df. calculate the angle.

--- projects/nrf5340_net/data/is_sample_parser.py
+++ projects/nrf5340_net/data/is_sample_parser.py
@@ -2,7 +2,12 @@ import struct
 import numpy as np
 import matplotlib.pyplot as plt
 
+import math
+
 NUM_SAMPLES  = 160
+C            = 299792458 # speed of ligh 
+ANT_D        = 0.035     # meter
+CENTER_FREQ  = (2404*1e6)# channel 0
 
 nrf5340_port = {
     'red'  : 'COM9',
@@ -41,8 +46,111 @@ time_data = time
 sample_file = 'samples_{0}.txt'.format(key)
 # sample_file = 'samples_black_scum.txt'
 
+def aoa_angle_calculation(phase_data, num_pkt):
+    
+    # find a complete sweep from -201 to 201
+    
+    # 64 
+    index_1 = 0
+    index_2 = 0
+    for index in range(64):
+        if phase_data[index+1] < phase_data[index]:
+            if  index_1 == 0:
+                index_1 = index+1
+            else:
+                index_2 = index+1
+                break   
+    
+    if index_1 != 0 and index_2 != 0:
+        
+        IF = 1 / ((index_2 - index_1) * t_unit * 1e-6)
+        freq = CENTER_FREQ + IF
+        
+        wave_length = (1/freq) * C  # in meters
+        reference_phase_data = phase_data[index_1:index_2]
+    else:
+        return None
+    
+    # generate phase data for ANT used reference at each sampling point
+    
+    target_phase_data = phase_data[:index_2]
+    
+    # total number of samples is 160, 8 is the samples missed during first antenna switching slot
+    while len(target_phase_data) < 160 + 8: 
+        target_phase_data += reference_phase_data
+    
+    target_phase_data = target_phase_data[:64] + target_phase_data[64+8:160+8]
+    
+    # compare to the phase data between generated and sampled
+    
+    figure, temp = plt.subplots()
+    
+    i = 8
+    
+    data_1 = phase_data[8*i:8*(i+1)]        + phase_data[8*(i+4):8*(i+5)]           + phase_data[8*(i+8):8*(i+9)]
+    data_2 = target_phase_data[8*i:8*(i+1)] + target_phase_data[8*(i+4):8*(i+5)]    + target_phase_data[8*(i+8):8*(i+9)]
+    
+    phase_diff_ant_1_2 = []
+    for index in range(len(data_1)):
+        diff = data_1[index]-data_2[index]
+        if diff <= -201:
+            diff += 402
+        if diff >= 201:
+            diff -= 402
+        phase_diff_ant_1_2.append(diff) 
+        
+    temp.plot(phase_diff_ant_1_2, 'o-', label='ant1.1 and ant1.2')
+    
+    i += 2
+    
+    data_1 = phase_data[8*i:8*(i+1)]        + phase_data[8*(i+4):8*(i+5)]           + phase_data[8*(i+8):8*(i+9)]
+    data_2 = target_phase_data[8*i:8*(i+1)] + target_phase_data[8*(i+4):8*(i+5)]    + target_phase_data[8*(i+8):8*(i+9)]
+    
+    phase_diff_ant_1_3 = []
+    for index in range(len(data_1)):
+        diff = data_1[index]-data_2[index]
+        if diff <= -201:
+            diff += 402
+        if diff >= 201:
+            diff -= 402
+        phase_diff_ant_1_3.append(diff) 
+        
+    temp.plot(phase_diff_ant_1_3, 'o-', label='ant1.1 and ant1.3')
+    temp.legend()
+    figure.savefig('sampe_pkt_phase_diff_{0}'.format(num_pkt))
+    
+    # calculate the phase degree
+    
+    print "{0} {1} {2} {3}".format(
+        (sum(phase_diff_ant_1_2)/len(phase_diff_ant_1_2)/402 * wave_length) / ANT_D, 
+        (sum(phase_diff_ant_1_2)/len(phase_diff_ant_1_2))/402.0, 
+        wave_length, 
+        ANT_D
+    )
+    theta_1 = math.acos( 
+        (sum(phase_diff_ant_1_2)/len(phase_diff_ant_1_2)/402.0 * wave_length) / ANT_D
+    )
+
+    print "{0} {1} {2} {3}".format(
+        (sum(phase_diff_ant_1_3)/len(phase_diff_ant_1_3)/402 * wave_length) / ANT_D, 
+        (sum(phase_diff_ant_1_3)/len(phase_diff_ant_1_3))/402.0, 
+        wave_length, 
+        ANT_D
+    )
+    theta_2 = math.acos( 
+        (sum(phase_diff_ant_1_3)/len(phase_diff_ant_1_3)/402.0 * wave_length) / ANT_D
+    )
+    
+    print "angle_1 {0}  angle_2 {1}".format(
+        theta_1, 
+        theta_2
+    )
+    
+    return target_phase_data
+    
+
 with open(sample_file, 'r') as f:
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10,8))
     num_pkt = 0
     for line in f:
         one_entry_samples = eval(line)
@@ -55,27 +163,58 @@ with open(sample_file, 'r') as f:
         # ax.plot(time_data, magPhase['mag_data'], '^-', label='mag_data')
         # ax.plot(time_data, magPhase['phase_data'], '*-', label='phase_data')
         
+        # determine angles
+        result = aoa_angle_calculation(magPhase['phase_data'], num_pkt)
+        
+        if result:
+            ax.plot(time_data, result, '--', label='reference phase data')
+        
+        # only plots samples in sample slots
         i = 8
-        ax.plot(time_data[:8*i], magPhase['mag_data'][:8*i], '^-', label='mag_data_ant1.1')
+        ax.plot(time_data[:8*i], magPhase['mag_data'][:8*i], 'k^', label='mag_data_ant1.1')
         ax.plot(time_data[:8*i], magPhase['phase_data'][:8*i], '*-', label='phase_data_ant1.1')
 
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.2')
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.2')
+        ax.plot(
+            time_data[8*i:8*(i+1)]              + time_data[8*(i+4):8*(i+5)]                + time_data[8*(i+8):8*(i+9)], 
+            magPhase['mag_data'][8*i:8*(i+1)]   + magPhase['mag_data'][8*(i+4):8*(i+5)]     + magPhase['mag_data'][8*(i+8):8*(i+9)], 
+            'k^',
+            label='mag_data_antt1.2'
+        )
+        ax.plot(
+            time_data[8*i:8*(i+1)]              + time_data[8*(i+4):8*(i+5)]                + time_data[8*(i+8):8*(i+9)], 
+            magPhase['phase_data'][8*i:8*(i+1)] + magPhase['phase_data'][8*(i+4):8*(i+5)]   + magPhase['phase_data'][8*(i+8):8*(i+9)], 
+            '*',
+            label='phase_data_antt1.2'
+        )
         i += 2
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.3')
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.3')
-        i += 2
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.2')
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.2')
-        i += 2
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.3')
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.3')
-        i += 2
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.2')
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.2')
-        i += 2
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.2')
-        ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.3')
+        
+        ax.plot(
+            time_data[8*i:8*(i+1)]              + time_data[8*(i+4):8*(i+5)]                + time_data[8*(i+8):8*(i+9)], 
+            magPhase['mag_data'][8*i:8*(i+1)]   + magPhase['mag_data'][8*(i+4):8*(i+5)]     + magPhase['mag_data'][8*(i+8):8*(i+9)], 
+            'k^',
+            label='mag_data_antt1.3'
+        )
+        ax.plot(
+            time_data[8*i:8*(i+1)]              + time_data[8*(i+4):8*(i+5)]                + time_data[8*(i+8):8*(i+9)], 
+            magPhase['phase_data'][8*i:8*(i+1)] + magPhase['phase_data'][8*(i+4):8*(i+5)]   + magPhase['phase_data'][8*(i+8):8*(i+9)], 
+            '*',
+            label='phase_data_antt1.3'
+        )
+        # i += 2
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.3')
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.3')
+        # i += 2
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.2')
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.2')
+        # i += 2
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.3')
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.3')
+        # i += 2
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.2')
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.2')
+        # i += 2
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['mag_data'][8*i:8*(i+1)], '^-', label='phase_data_antt1.2')
+        # ax.plot(time_data[8*i:8*(i+1)], magPhase['phase_data'][8*i:8*(i+1)], '*-', label='phase_data_antt1.3')
         
         ax.set_ylim(-400, 400)
         ax.set_xlim(0,    25)
