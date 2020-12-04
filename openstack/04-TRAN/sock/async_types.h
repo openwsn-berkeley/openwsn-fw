d5eaba1d74a7842a3d33073020754308e68ba19d
Francisco Molina <femolina@uc.cl>
2020-12-04 17:30:37 +0100

    openstack/04-TRAN: move sock to own directory

--- openstack/04-TRAN/sock/async_types.h
+++ openstack/04-TRAN/sock/async_types.h
@@ -15,7 +15,20 @@ typedef enum {
 
 typedef struct sock_udp sock_udp_t;   /**< forward declare for async */
 
-// definitions asynchronous callback for socket events
+/**
+ * @brief   Event callback for @ref sock_udp_t
+ *
+ * @pre `(sock != NULL)`
+ *
+ * @param[in] sock  The sock the event happened on
+ * @param[in] flags The event flags. Expected values are
+ *                  - @ref SOCK_ASYNC_MSG_RECV,
+ *                  - @ref SOCK_ASYNC_MSG_SENT,
+ *                  - @ref SOCK_ASYNC_PATH_PROP, or
+ *                  - a combination of them.
+ * @param[in] arg   Argument provided when setting the callback using
+ *                  @ref sock_udp_set_cb(). May be NULL.
+ */
 typedef void (* sock_udp_cb_t)(sock_udp_t* sock, sock_async_flags_t type, void* arg);
 
 #endif /* OPENWSN_ASYNC_TYPES_H */
