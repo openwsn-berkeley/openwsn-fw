daac560c73cdefeb51855c56dc6efc290592c221
Timothy Claeys <timothy.claeys@gmail.com>
2021-03-17 11:45:09 +0100

    Update flash scripts

--- cmake/toolchains/m3.cmake
+++ cmake/toolchains/m3.cmake
@@ -4,6 +4,7 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 set(SIZE_TOOL arm-none-eabi-size)
+set(OBJCOPY arm-none-eabi-objcopy)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
