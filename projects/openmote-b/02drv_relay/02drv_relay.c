e468bef4c7bacfc0a5fe89ab001d75e21f904b72
Mina Rady <minarady@gmail.com>
2021-10-11 18:22:59 +0200

    FW-533 payload is transferred to packet and periodic transmission starts

--- projects/openmote-b/02drv_relay/02drv_relay.c
+++ projects/openmote-b/02drv_relay/02drv_relay.c
@@ -1,15 +1,15 @@
 /**
 \brief This is a program which shows how to use the bsp modules for the board
-       and UART.
+and UART.
 
 \note: Since the bsp modules for different platforms have the same declaration,
-       you can use this project with any platform.
+you can use this project with any platform.
 
 Load this program on your board. Open a serial terminal client (e.g. PuTTY or
 TeraTerm):
 - You will read "Hello World!" printed over and over on your terminal client.
 - when you enter a character on the client, the board echoes it back (i.e. you
-  see the character on the terminal client) and the "ERROR" led blinks.
+see the character on the terminal client) and the "ERROR" led blinks.
 
 \author Thomas Watteyne <watteyne@eecs.berkeley.edu>, February 2012
 */
@@ -22,15 +22,29 @@ TeraTerm):
 #include "uart.h"
 #include "sctimer.h"
 #include "leds.h"
+#include "radio.h"
 
 //=========================== defines =========================================
+#define CPU_CLOCK_FREQ             32768  // 32kHz = 1s
 
-#define SCTIMER_PERIOD     0xffff // 0xffff@32kHz = 2s
+// Serial input
+
+#define SCTIMER_PERIOD     CPU_CLOCK_FREQ*5 
 #define MAX_STRING_SIZE     100
+
+// Protocol stack
+
+#define LENGTH_PACKET   125+LENGTH_CRC // maximum length is 127 bytes
+#define CHANNEL         11             // 24ghz: 11 = 2.405GHz, subghz: 11 = 865.325 in  FSK operating mode #1
+#define TIMER_PERIOD    (32768>>1)     // (32768>>1) = 500ms @ 32kHz
+#define NETID           38000          // Grenoble postal code
+#define NETID_SIZE      2              // 2 bytes
 
 uint8_t stringReceived[MAX_STRING_SIZE];
 uint8_t writeIndex;
+bool IAmTransmitter;
 
+uint16_t net_id;
 uint8_t payloadString[MAX_STRING_SIZE];
 uint8_t payloadSize;
 
@@ -38,18 +52,44 @@ uint8_t payloadSize;
 //=========================== variables =======================================
 
 typedef struct {
-              uint8_t uart_lastTxByteIndex;
-   volatile   uint8_t uartDone;
-   volatile   uint8_t uartSendNow;
+  
+  // UART TX related
+  uint8_t uart_lastTxByteIndex;
+  volatile   uint8_t uartDone;
+  volatile   uint8_t uartSendNow;
+  
+  // MAC related
+  
+  uint8_t              txpk_txNow;
+  uint8_t              txpk_buf[LENGTH_PACKET];
+  uint8_t              txpk_len;
+  uint8_t              txpk_num;
 } app_vars_t;
 
 app_vars_t app_vars;
 
+
+typedef struct {
+  uint8_t              num_scTimerCompare;
+  uint8_t              num_startFrame;
+  uint8_t              num_endFrame;
+  uint8_t              counter;
+} app_dbg_t;
+
+app_dbg_t app_dbg;
+
 //=========================== prototypes ======================================
 
+// timer callback
 void cb_compare(void);
+
+// UART callbacks
 void cb_uartTxDone(void);
 uint8_t cb_uartRxCb(void);
+
+// radio callbacks
+void cb_startFrame(PORT_TIMER_WIDTH timestamp);
+void cb_endFrame(PORT_TIMER_WIDTH timestamp);
 
 //=========================== main ============================================
 
@@ -57,82 +97,168 @@ uint8_t cb_uartRxCb(void);
 \brief The program starts executing here.
 */
 int mote_main(void) {
-   
-   // clear local variable
-   memset(&app_vars,0,sizeof(app_vars_t));
-   memset(&stringReceived,0,MAX_STRING_SIZE);
-   memset(&payloadString,0,MAX_STRING_SIZE);
-   writeIndex= 0;
-   
-   app_vars.uartSendNow = 1;
-   
-   // initialize the board
-   board_init();
-   
-   // setup UART
-   uart_setCallbacks(cb_uartTxDone,cb_uartRxCb);
-   uart_enableInterrupts();
-   
-   // setup sctimer
-   sctimer_set_callback(cb_compare);
-   sctimer_setCompare(sctimer_readCounter()+SCTIMER_PERIOD);
-   
-   
-   while(1) {
+  
+  // clear local variable
+  memset(&app_vars,0,sizeof(app_vars_t));
+  memset(&stringReceived,0,MAX_STRING_SIZE);
+  memset(&payloadString,0,MAX_STRING_SIZE);
+  writeIndex= 0;
+  IAmTransmitter = false;
+  app_dbg.counter = 0;
+  
+  app_vars.uartSendNow = 1;
+  
+  // initialize the board
+  board_init();
+  
+  // setup UART
+  uart_setCallbacks(cb_uartTxDone,cb_uartRxCb);
+  uart_enableInterrupts();
+  
+  // setup sctimer
+  sctimer_set_callback(cb_compare);
+  sctimer_setCompare(sctimer_readCounter()+SCTIMER_PERIOD);
+  
+  // setup the radios
+  
+
+  
+  // cc2538 callbacks 
+  radio_setConfig (RADIOSETTING_24GHZ);
+  radio_setStartFrameCb(cb_startFrame);
+  radio_setEndFrameCb(cb_startFrame);
+  
+    // atmel callbacks
+  radio_setConfig (RADIOSETTING_FSK_OPTION1_FEC);
+  radio_setStartFrameCb(cb_startFrame);
+  radio_setEndFrameCb(cb_endFrame);
+  
+  // radio frequency
+  radio_setFrequency(CHANNEL, FREQ_TX);
+  
+  while(1) {
+    
+    // if I am a designated transmitter, transmit. 
+    while (IAmTransmitter == true){
       
-      // wait for timer to elapse
-      while (app_vars.uartSendNow==0);
-      app_vars.uartSendNow = 0;
+      uint8_t i = 0;
       
-      // send string over UART
-      //app_vars.uartDone              = 0;
-      //app_vars.uart_lastTxByteIndex  = 0;
-      //uart_writeByte(payloadString[app_vars.uart_lastTxByteIndex]);
-      //while(app_vars.uartDone==0);
-   }
+      // prepare packet
+      
+      /*
+        1B packet number
+        1B hop number
+        2B NET ID
+        1B payload size
+      */          
+      app_vars.txpk_num++;
+      app_vars.txpk_len             = sizeof(app_vars.txpk_buf);
+      app_vars.txpk_buf[i++]        = app_vars.txpk_num;
+      app_vars.txpk_buf[i++]        = 0;     // hop number
+      app_vars.txpk_buf[i++]        = NETID >>8;
+      app_vars.txpk_buf[i++]        = NETID & 0xFF;
+      app_vars.txpk_buf[i++]        = payloadSize; 
+      
+      
+      for (uint8_t j = 0; j< payloadSize; j++)
+      {
+        app_vars.txpk_buf[i++] = payloadString[j];
+      }
+      
+      // now packet is ready
+      
+      while (app_vars.txpk_txNow==0) {
+        board_sleep();
+      }
+      app_vars.txpk_txNow = 0;
+      app_dbg.counter++;
+      
+      // transmit
+      radio_loadPacket(app_vars.txpk_buf,app_vars.txpk_len);
+      radio_txEnable();
+      radio_txNow();
+      
+    }
+    
+    // otherwise, listen and relay
+    
+    
+    //while (app_vars.uartSendNow==0);
+    //app_vars.uartSendNow = 0;
+    
+    // send string over UART
+    // app_vars.uartDone              = 0;
+    // app_vars.uart_lastTxByteIndex  = 0;
+    // uart_writeByte(payloadString[app_vars.uart_lastTxByteIndex]);
+    // while(app_vars.uartDone==0);
+  }
 }
 
 //=========================== callbacks =======================================
 
 void cb_compare(void) {
-   
-   // have main "task" send over UART
-   app_vars.uartSendNow = 1;
-   
-   // schedule again
-   sctimer_setCompare(sctimer_readCounter()+SCTIMER_PERIOD);
+  
+  // send packet
+  app_vars.txpk_txNow = 1;
+  
+  // app_vars.uartSendNow = 1;
+  
+  // schedule again
+  sctimer_setCompare(sctimer_readCounter()+SCTIMER_PERIOD);
 }
 
 void cb_uartTxDone(void) {
-   app_vars.uart_lastTxByteIndex++;
-   if (app_vars.uart_lastTxByteIndex<sizeof(payloadString)) {
-      uart_writeByte(payloadString[app_vars.uart_lastTxByteIndex]);
-   } else {
-      app_vars.uartDone = 1;
-   }
+  app_vars.uart_lastTxByteIndex++;
+  if (app_vars.uart_lastTxByteIndex<sizeof(payloadString)) {
+    uart_writeByte(payloadString[app_vars.uart_lastTxByteIndex]);
+  } else {
+    app_vars.uartDone = 1;
+  }
 }
 
 uint8_t cb_uartRxCb(void) {
-   uint8_t byte;
-   
-   
-   // toggle LED
-   leds_error_toggle();
-   
-   // read received byte
-   
-   byte = uart_readByte();
-   
-   // store the received byte
-   if (writeIndex < MAX_STRING_SIZE && byte !='\r')
-   {
-     stringReceived [writeIndex++] = byte;
-   }else{
-      // string complete
-      payloadSize = writeIndex;
-      writeIndex = 0;
-      memcpy(payloadString, stringReceived,sizeof(stringReceived)+1);
-      memset(&stringReceived,0,sizeof(stringReceived));
-   }
-   return 0;
+  uint8_t byte;
+  
+  
+  // toggle LED
+  leds_error_toggle();
+  
+  // read received byte
+  byte = uart_readByte();
+  
+  // echo it back to the terminal
+  uart_writeByte (byte);
+  
+  // store the received byte
+  if (writeIndex < MAX_STRING_SIZE && byte !='\r')
+  {
+    stringReceived [writeIndex++] = byte;
+  }else{
+    // string complete
+    payloadSize = writeIndex;
+    writeIndex = 0;
+    memcpy(payloadString, stringReceived,sizeof(stringReceived)+1);
+    memset(&stringReceived,0,sizeof(stringReceived));
+    
+    IAmTransmitter = true;
+  }
+  return 0;
+}
+
+void cb_startFrame(PORT_TIMER_WIDTH timestamp) {
+
+   // update debug vals
+   app_dbg.num_startFrame++;
+
+   // led
+   leds_sync_on();
+
+}
+
+void cb_endFrame(PORT_TIMER_WIDTH timestamp) {
+   // update debug vals
+   app_dbg.num_endFrame++;
+
+   // led
+   leds_sync_off();
 }
