925e8b89159b073c4eda89c5ed733683e0e4382b
Mina Rady <minarady@gmail.com>
2021-10-11 15:21:48 +0200

    FW-533 String retrieved over serial and transfered to a payload variable when enter is pressed (\r)

--- projects/openmote-b/02drv_relay/02drv_relay.c
+++ projects/openmote-b/02drv_relay/02drv_relay.c
@@ -1,120 +1,53 @@
 /**
-\brief This program shows the use of the "radio" bsp module.
+\brief This is a program which shows how to use the bsp modules for the board
+       and UART.
 
-Since the bsp modules for different platforms have the same declaration, you
-can use this project with any platform.
+\note: Since the bsp modules for different platforms have the same declaration,
+       you can use this project with any platform.
 
-This application places the mote in receive mode, and prints, over the serial
-port, all information about the received packet. The frame printed over the
-serial port for each received packet is formatted as follows:
-- [1B] the length of the packet, an unsigned integer
-- [1B] the first byte of the packet, an unsigned integer
-- [1B] the receive signal strength of tehe packet, an signed integer
-- [1B] the link quality indicator, an unsigned integer
-- [1B] whether the receive packet passed CRC (1) or not (0)
-- [3B] closing flags, each of value 0xff
+Load this program on your board. Open a serial terminal client (e.g. PuTTY or
+TeraTerm):
+- You will read "Hello World!" printed over and over on your terminal client.
+- when you enter a character on the client, the board echoes it back (i.e. you
+  see the character on the terminal client) and the "ERROR" led blinks.
 
-You can run the 01bsp_radio_rx.py script to listen to your mote and parse those
-serial frames. The application can connect directly to the mote's serial port,
-or to its TCP port when running on the IoT-LAB platform.
-
-Example when running locally:
-----------------------------
-
- ___                 _ _ _  ___  _ _
-| . | ___  ___ ._ _ | | | |/ __>| \ |
-| | || . \/ ._>| ' || | | |\__ \|   |
-`___'|  _/\___.|_|_||__/_/ <___/|_\_|
-     |_|                  openwsn.org
-
-running IoT-lAB? (Y|N): N
-name of serial port (e.g. COM10): COM25
-len=127 num=176 rssi=-43  lqi=107 crc=1
-len=127 num=177 rssi=-43  lqi=107 crc=1
-len=127 num=178 rssi=-43  lqi=106 crc=1
-len=127 num=179 rssi=-43  lqi=107 crc=1
-len=127 num=180 rssi=-43  lqi=108 crc=1
-len=127 num=181 rssi=-43  lqi=107 crc=1
-len=127 num=182 rssi=-43  lqi=107 crc=1
-len=127 num=183 rssi=-43  lqi=107 crc=1
-
-
-Example when running on the IoT-LAB platform:
---------------------------------------------
-
- ___                 _ _ _  ___  _ _
-| . | ___  ___ ._ _ | | | |/ __>| \ |
-| | || . \/ ._>| ' || | | |\__ \|   |
-`___'|  _/\___.|_|_||__/_/ <___/|_\_|
-     |_|                  openwsn.org
-
-running IoT-lAB? (Y|N): Y
-motename? (e.g. wsn430-35): wsn430-35
-len=17  num=84  rssi=-80  lqi=107 crc=1
-len=17  num=84  rssi=-81  lqi=107 crc=1
-len=17  num=84  rssi=-80  lqi=107 crc=1
-len=17  num=84  rssi=-81  lqi=105 crc=1
-len=17  num=84  rssi=-80  lqi=108 crc=1
-len=17  num=84  rssi=-81  lqi=108 crc=1
-
-
-\author Xavi Vilajosana xvilajosana@eecs.berkeley.edu>, June 2012.
-\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, August 2014.
+\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, February 2012
 */
 
 #include "stdint.h"
+#include "stdio.h"
 #include "string.h"
+// bsp modules required
 #include "board.h"
-#include "radio.h"
-#include "leds.h"
-#include "debugpins.h"
 #include "uart.h"
-#include "eui64.h"
-//#include "sctimer.h"
+#include "sctimer.h"
+#include "leds.h"
 
 //=========================== defines =========================================
 
-#define LENGTH_PACKET        125+LENGTH_CRC // maximum length is 127 bytes
-#define CHANNEL              11             // 24ghz: 11 = 2.405GHz, subghz: 11 = 865.325 in  FSK operating mode #1
-#define LENGTH_SERIAL_FRAME  10              // length of the serial frame
-#
+#define SCTIMER_PERIOD     0xffff // 0xffff@32kHz = 2s
+#define MAX_STRING_SIZE     100
+
+uint8_t stringReceived[MAX_STRING_SIZE];
+uint8_t writeIndex;
+
+uint8_t payloadString[MAX_STRING_SIZE];
+uint8_t payloadSize;
+
+
 //=========================== variables =======================================
 
 typedef struct {
-    uint8_t    num_radioTimerCompare;
-    uint8_t    num_startFrame;
-    uint8_t    num_endFrame;
-} app_dbg_t;
-
-app_dbg_t app_dbg;
-
-typedef struct {
-    // rx packet
-    volatile    uint8_t    rxpk_done;
-                uint8_t    rxpk_buf[LENGTH_PACKET];
-                uint16_t    rxpk_len;
-                uint8_t    rxpk_num;
-                uint8_t    rxpk_src;
-                int8_t     rxpk_rssi;
-                uint8_t    rxpk_lqi;
-                bool       rxpk_crc;
-                uint8_t    rxpk_freq_offset;
-    // uart
-                uint8_t    uart_txFrame[LENGTH_SERIAL_FRAME];
-                uint8_t    uart_lastTxByte;
-    volatile    uint8_t    uart_done;
+              uint8_t uart_lastTxByteIndex;
+   volatile   uint8_t uartDone;
+   volatile   uint8_t uartSendNow;
 } app_vars_t;
 
 app_vars_t app_vars;
-uint8_t tx_started;
+
 //=========================== prototypes ======================================
 
-// radiotimer
-void cb_radioTimerOverflows(void);
-// radio
-void cb_startFrame(PORT_TIMER_WIDTH timestamp);
-void cb_endFrame(PORT_TIMER_WIDTH timestamp);
-// uart
+void cb_compare(void);
 void cb_uartTxDone(void);
 uint8_t cb_uartRxCb(void);
 
@@ -124,190 +57,82 @@ uint8_t cb_uartRxCb(void);
 \brief The program starts executing here.
 */
 int mote_main(void) {
-
-    uint8_t i;
-
-    // clear local variables
-    memset(&app_vars,0,sizeof(app_vars_t));
-
-    // initialize board
-    board_init();
-
-
-
-    // setup UART
-    uart_setCallbacks(cb_uartTxDone,cb_uartRxCb);
-
-    // prepare radio
-    radio_setConfig (RADIOSETTING_FSK_OPTION1_FEC);
-    radio_rfOn();
-    // freq type only effects on scum port
-    radio_setFrequency(CHANNEL, FREQ_RX);
-    // add callback functions radio
-    radio_setStartFrameCb(cb_startFrame);
-    radio_setEndFrameCb(cb_endFrame);
-    // switch in RX
-    radio_rxEnable();
-    radio_rxNow();
-    debugpins_radio_clr();
-    while (1) {
-
-        // sleep while waiting for at least one of the rxpk_done to be set
-
-        app_vars.rxpk_done = 0;
-        while (app_vars.rxpk_done==0) {
-            board_sleep();
-        }
-
-        // if I get here, I just received a packet
-
-        //===== send notification over serial port
-
-        // led
-        leds_error_on();
-
-        // format frame to send over serial port
-        i = 0;
-        app_vars.uart_txFrame[i++] = app_vars.rxpk_len;  // packet length
-        app_vars.uart_txFrame[i++] = app_vars.rxpk_num;  // packet number
-        app_vars.uart_txFrame[i++] = app_vars.rxpk_src;  // packet source EUI
-        app_vars.uart_txFrame[i++] = app_vars.rxpk_rssi; // RSSI
-        app_vars.uart_txFrame[i++] = app_vars.rxpk_lqi;  // LQI
-        app_vars.uart_txFrame[i++] = app_vars.rxpk_crc;  // CRC
-        app_vars.uart_txFrame[i++] = app_vars.rxpk_freq_offset; // freq_offset
-        app_vars.uart_txFrame[i++] = 0xff;               // closing flag
-        app_vars.uart_txFrame[i++] = 0xff;               // closing flag
-        app_vars.uart_txFrame[i++] = 0xff;               // closing flag
-
-        app_vars.uart_done          = 0;
-        app_vars.uart_lastTxByte    = 0;
-
-        // send app_vars.uart_txFrame over UART
-        uart_clearTxInterrupts();
-        uart_clearRxInterrupts();
-        uart_enableInterrupts();
-        uart_writeByte(app_vars.uart_txFrame[app_vars.uart_lastTxByte]);
-        while (app_vars.uart_done==0); // busy wait to finish
-        uart_disableInterrupts();
-
-        // led
-        leds_error_off();
-
-        
-
-
-    }
+   
+   // clear local variable
+   memset(&app_vars,0,sizeof(app_vars_t));
+   memset(&stringReceived,0,MAX_STRING_SIZE);
+   memset(&payloadString,0,MAX_STRING_SIZE);
+   writeIndex= 0;
+   
+   app_vars.uartSendNow = 1;
+   
+   // initialize the board
+   board_init();
+   
+   // setup UART
+   uart_setCallbacks(cb_uartTxDone,cb_uartRxCb);
+   uart_enableInterrupts();
+   
+   // setup sctimer
+   sctimer_set_callback(cb_compare);
+   sctimer_setCompare(sctimer_readCounter()+SCTIMER_PERIOD);
+   
+   
+   while(1) {
+      
+      // wait for timer to elapse
+      while (app_vars.uartSendNow==0);
+      app_vars.uartSendNow = 0;
+      
+      // send string over UART
+      //app_vars.uartDone              = 0;
+      //app_vars.uart_lastTxByteIndex  = 0;
+      //uart_writeByte(payloadString[app_vars.uart_lastTxByteIndex]);
+      //while(app_vars.uartDone==0);
+   }
 }
 
 //=========================== callbacks =======================================
 
-//===== radio
-
-void cb_startFrame(PORT_TIMER_WIDTH timestamp) {
-    debugpins_radio_toggle();
-    debugpins_fsm_set();
-    leds_sync_on();
-    // update debug stats
-    app_dbg.num_startFrame++;
+void cb_compare(void) {
+   
+   // have main "task" send over UART
+   app_vars.uartSendNow = 1;
+   
+   // schedule again
+   sctimer_setCompare(sctimer_readCounter()+SCTIMER_PERIOD);
 }
-
-void cb_endFrame(PORT_TIMER_WIDTH timestamp) {
-    debugpins_radio_toggle();
-    debugpins_fsm_clr();
-    uint8_t  i;
-    bool     expectedFrame;
-    leds_debug_toggle();
-    // update debug stats
-    app_dbg.num_endFrame++;
-
-    memset(&app_vars.rxpk_buf[0],0,LENGTH_PACKET);
-
-    app_vars.rxpk_freq_offset = radio_getFrequencyOffset();
-    uint8_t rxpk_len = (uint8_t) app_vars.rxpk_len;
-    // get packet from radio
-    radio_getReceivedFrame(
-        app_vars.rxpk_buf,
-        &rxpk_len,
-        sizeof(app_vars.rxpk_buf),
-        &app_vars.rxpk_rssi,
-        &app_vars.rxpk_lqi,
-        &app_vars.rxpk_crc
-    );
-
-    // check the frame is sent by radio_tx project
-    expectedFrame = TRUE;
-
-    if (app_vars.rxpk_len>LENGTH_PACKET){
-        expectedFrame = FALSE;
-    } else {
-        for(i=2;i<10;i++){
-            if(app_vars.rxpk_buf[i]!=i){
-                expectedFrame = FALSE;
-                break;
-            }
-        }
-    }
-
-
-    
-    // if I am at neither ends of the network, just keep relaying 
-        
-    uint8_t eui[8];
-    memset(eui,0,sizeof(eui));
-    eui64_get(eui);
-
-    
-
-    // read the packet number
-    app_vars.rxpk_num = app_vars.rxpk_buf[0];
-    
-    // toggle led if the frame is expected
-    if (expectedFrame){
-        // indicate I just received a packet from bsp_radio_tx mote
-        app_vars.rxpk_done = 1;
-
-        //leds_debug_toggle();
-    }
-
-    // relay 
-    if (tx_started ==0){
-        tx_started = 1;
-        radio_rfOff();
-        radio_loadPacket(app_vars.rxpk_buf,app_vars.rxpk_len);
-        radio_txEnable();
-        radio_txNow();
-    }else{
-        // I am just done transmitting, switch to rx mode
-        tx_started = 0;
-        radio_rfOff();
-        radio_rxEnable();
-        radio_rxNow(); 
-    }
-
-
-    // led
-    leds_sync_off();
-}
-
-//===== uart
 
 void cb_uartTxDone(void) {
-
-    uart_clearTxInterrupts();
-
-    // prepare to send the next byte
-    app_vars.uart_lastTxByte++;
-
-    if (app_vars.uart_lastTxByte<sizeof(app_vars.uart_txFrame)) {
-        uart_writeByte(app_vars.uart_txFrame[app_vars.uart_lastTxByte]);
-    } else {
-        app_vars.uart_done=1;
-    }
+   app_vars.uart_lastTxByteIndex++;
+   if (app_vars.uart_lastTxByteIndex<sizeof(payloadString)) {
+      uart_writeByte(payloadString[app_vars.uart_lastTxByteIndex]);
+   } else {
+      app_vars.uartDone = 1;
+   }
 }
 
 uint8_t cb_uartRxCb(void) {
-
-    //  uint8_t byte;
-    uart_clearRxInterrupts();
-    return 1;
+   uint8_t byte;
+   
+   
+   // toggle LED
+   leds_error_toggle();
+   
+   // read received byte
+   
+   byte = uart_readByte();
+   
+   // store the received byte
+   if (writeIndex < MAX_STRING_SIZE && byte !='\r')
+   {
+     stringReceived [writeIndex++] = byte;
+   }else{
+      // string complete
+      payloadSize = writeIndex;
+      writeIndex = 0;
+      memcpy(payloadString, stringReceived,sizeof(stringReceived)+1);
+      memset(&stringReceived,0,sizeof(stringReceived));
+   }
+   return 0;
 }
