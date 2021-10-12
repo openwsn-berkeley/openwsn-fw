8d3d678219f6ec0de58ab14fe2c0b40d6b31f8ab
Mina Rady <minarady@gmail.com>
2021-10-12 15:11:05 +0200

    Update 02drv_relay.c

--- projects/openmote-b/02drv_relay/02drv_relay.c
+++ projects/openmote-b/02drv_relay/02drv_relay.c
@@ -1,17 +1,7 @@
 /**
-\brief This is a program which shows how to use the bsp modules for the board
-and UART.
+\brief This is a program which runs a simple relay. If it recives an input from UART, it turns the mote into a periodic transmitter for that payload. 
 
-\note: Since the bsp modules for different platforms have the same declaration,
-you can use this project with any platform.
-
-Load this program on your board. Open a serial terminal client (e.g. PuTTY or
-TeraTerm):
-- You will read "Hello World!" printed over and over on your terminal client.
-- when you enter a character on the client, the board echoes it back (i.e. you
-see the character on the terminal client) and the "ERROR" led blinks.
-
-\author Thomas Watteyne <watteyne@eecs.berkeley.edu>, February 2012
+\author Mina Rady <mina.rady@orange.com>, October 2021
 */
 
 #include "stdint.h"
